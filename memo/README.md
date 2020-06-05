c9a4f9632efd4af186d33a8c9ed9987ad2c00c50
Tyera Eulberg <tyera@solana.com>
2020-06-05 13:42:10 -0600

    Initial memo commit

---
+++ memo/README.md
@@ -0,0 +1,4 @@
+# Memo Program
+
+A simple program that accepts a string of encoded characters and verifies that
+it parses. Currently handles UTF-8.
---
+++ memo/src/processor.rs
@@ -0,0 +1,48 @@
+use solana_sdk::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, program_error::ProgramError,
+    pubkey::Pubkey,
+};
+use std::str::from_utf8;
+
+entrypoint!(process_instruction);
+fn process_instruction<'a>(
+    _program_id: &Pubkey,
+    _accounts: &'a [AccountInfo<'a>],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    from_utf8(instruction_data)
+        .map(|_| ())
+        .map_err(|_| ProgramError::InvalidInstructionData)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use solana_sdk::{program_error::ProgramError, pubkey::Pubkey};
+
+    #[test]
+    fn test_utf8_memo() {
+        let program_id = Pubkey::new(&[0; 32]);
+
+        let string = b"letters and such";
+        assert_eq!(
+            Ok(()),
+            process_instruction(&program_id, &mut vec![], string)
+        );
+
+        let emoji = "🐆".as_bytes();
+        let bytes = [0xF0, 0x9F, 0x90, 0x86];
+        assert_eq!(emoji, bytes);
+        assert_eq!(
+            Ok(()),
+            process_instruction(&program_id, &mut vec![], &emoji)
+        );
+
+        let mut bad_utf8 = bytes;
+        bad_utf8[3] = 0xFF; // Invalid UTF-8 byte
+        assert_eq!(
+            Err(ProgramError::InvalidInstructionData),
+            process_instruction(&program_id, &mut vec![], &bad_utf8)
+        );
+    }
+}
