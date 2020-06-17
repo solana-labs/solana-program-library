74898cd200e2a3a5cfc5350e3213f5eaba99834d
Tyera Eulberg <tyera@solana.com>
2020-06-17 00:32:02 -0600

    Add rust program id to memo

--- memo/src/lib.rs
+++ memo/src/lib.rs
@@ -3,3 +3,6 @@
 //! A simple program that accepts a string of encoded characters and verifies that it parses. Currently handles UTF-8.
 
 pub mod processor;
+
+/// The spl-memo program's on-chain program id
+pub const PROGRAM_ID: &str = "Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo";
