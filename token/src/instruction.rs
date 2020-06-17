03ef85a3202e579767f46f5c684c641f18ea2fa7
Jack May <jack@solana.com>
2020-06-17 15:49:58 -0700

    No account required is initial supply is zero

--- token/src/instruction.rs
+++ token/src/instruction.rs
@@ -18,19 +18,21 @@ pub enum Instruction {
     ///
     /// # Accounts expected by this instruction:
     ///
-    ///   0. [writable, signer] New token to create.
-    ///   1. [writable] Account to hold all the newly minted tokens.
-    ///   2. Optional: [] Owner of the token, if present allows further minting of tokens.
+    ///   0. `[writable, signer]` New token to create.
+    ///   1.
+    ///      * If supply is non-zero: `[writable]` Account to hold all the newly minted tokens.
+    ///      * If supply is zero: `[]` Owner of the token.
+    ///   2. Optional: `[]` Owner of the token if supply is non-zero, if present then the token allows further minting of tokens.
     NewToken(TokenInfo),
     /// Creates a new account.  The new account can either hold tokens or be a delegate
     /// for another account.
     ///
     /// # Accounts expected by this instruction:
     ///
-    ///   0. [writable, signer]  New account being created.
-    ///   1. [] Owner of the new account.
-    ///   2. [] Token this account will be associated with.
-    ///   3. Optional: [] Source account that this account will be a delegate for.
+    ///   0. `[writable, signer]`  New account being created.
+    ///   1. `[]` Owner of the new account.
+    ///   2. `[]` Token this account will be associated with.
+    ///   3. Optional: `[]` Source account that this account will be a delegate for.
     NewAccount,
     /// Transfers tokens from one account to another either directly or via a delegate.
     ///
@@ -46,32 +48,32 @@ pub enum Instruction {
     ///
     /// # Accounts expected by this instruction:
     ///
-    ///   0. [signer] Owner of the source account.
-    ///   1. [] Source account.
-    ///   2. [writable] Delegate account.
+    ///   0. `[signer]` Owner of the source account.
+    ///   1. `[]` Source account.
+    ///   2. `[writable]` Delegate account.
     Approve(u64),
     /// Sets a new owner of an account.
     ///
     /// # Accounts expected by this instruction:
     ///
-    ///   0. [signer] Current owner of the account.
-    ///   1. [writable] account to change the owner of.
-    ///   2. [] New owner of the account.
+    ///   0. `[signer]` Current owner of the account.
+    ///   1. `[writable]` account to change the owner of.
+    ///   2. `[]` New owner of the account.
     SetOwner,
     /// Mints new tokens to an account.
     ///
     /// # Accounts expected by this instruction:
     ///
-    ///   0. [signer] Owner of the token.
-    ///   1. [writable] Token to mint.
-    ///   2. [writable] Account to mint tokens to.
+    ///   0. `[signer]` Owner of the token.
+    ///   1. `[writable]` Token to mint.
+    ///   2. `[writable]` Account to mint tokens to.
     MintTo(u64),
     /// Burns tokens by removing them from an account and the total supply.
     ///
     /// # Accounts expected by this instruction:
     ///
-    ///   0. [signer] Owner of the account to burn from.
-    ///   1. [writable] Account to burn from.
-    ///   2. [writable] Token being burned.
+    ///   0. `[signer]` Owner of the account to burn from.
+    ///   1. `[writable]` Account to burn from.
+    ///   2. `[writable]` Token being burned.
     Burn(u64),
 }
