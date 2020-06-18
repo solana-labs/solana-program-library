437d2d1c8bb8bbe9bb1dd7fe8e46147fe7c0f667
Jack May <jack@solana.com>
2020-06-18 11:02:42 -0700

    Bump to Solana v1.2.3 and Solana-web3.js v0.60.0 (#46)

--- bpf-sdk-install.sh
+++ bpf-sdk-install.sh
@@ -2,7 +2,7 @@
 set -e
 
 installDir=$1
-channel=edge
+channel=v1.2.3
 
 if [[ -n $2 ]]; then
   channel=$2
--- token/js/client/util/send-and-confirm-transaction.js
+++ token/js/client/util/send-and-confirm-transaction.js
@@ -4,8 +4,6 @@ import {sendAndConfirmTransaction as realSendAndConfirmTransaction} from '@solan
 import type {Account, Connection, Transaction} from '@solana/web3.js';
 import YAML from 'json-to-pretty-yaml';
 
-import {newSystemAccountWithAirdrop} from './new-system-account-with-airdrop';
-
 type TransactionNotification = (string, string) => void;
 
 let notify: TransactionNotification = () => undefined;
@@ -14,7 +12,6 @@ export function onTransaction(callback: TransactionNotification) {
   notify = callback;
 }
 
-let payerAccount: Account | null = null;
 export async function sendAndConfirmTransaction(
   title: string,
   connection: Connection,
@@ -23,19 +20,14 @@ export async function sendAndConfirmTransaction(
 ): Promise<void> {
   const when = Date.now();
 
-  if (!payerAccount) {
-    const {feeCalculator} = await connection.getRecentBlockhash();
-    const fees = feeCalculator.lamportsPerSignature * 1000; // wag
-    const newPayerAccount = await newSystemAccountWithAirdrop(connection, fees);
-    // eslint-disable-next-line require-atomic-updates
-    payerAccount = payerAccount || newPayerAccount;
-  }
-
   const signature = await realSendAndConfirmTransaction(
     connection,
     transaction,
-    payerAccount,
-    ...signers,
+    signers,
+    {
+      confirmations: 1,
+      skipPreflight: true,
+    },
   );
 
   const body = {
