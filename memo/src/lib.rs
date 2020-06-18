b581366047709984f6c22019906036f4da20a768
Tyera Eulberg <teulberg@gmail.com>
2020-06-18 10:51:22 -0600

    Memo: add Rust program id and bump version (#41)
    * Add rust program id to memo
    
    * Bump memo and sdk versions
    
    * Use declare_id macro
    
    * Bump solana version to pick up macro docs

--- memo/src/lib.rs
+++ memo/src/lib.rs
@@ -3,3 +3,5 @@
 //! A simple program that accepts a string of encoded characters and verifies that it parses. Currently handles UTF-8.
 
 pub mod processor;
+
+solana_sdk::declare_id!("Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo");
