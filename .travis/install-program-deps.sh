ab55c1aaf965996fefcf5aaeb2ee4bb2ed9edcf2
Dan Albert <dan@solana.com>
2020-07-15 22:35:29 -0600

    Fixup travis config

---
+++ .travis/install-program-deps.sh
@@ -0,0 +1,16 @@
+# |source| this file
+
+cargo --version
+rustup install nightly
+rustup component add clippy --toolchain nightly
+docker --version
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main"
+sudo apt-get update
+sudo apt-get install -y clang-7 --allow-unauthenticated
+sudo apt-get install -y openssl --allow-unauthenticated
+sudo apt-get install -y libssl-dev --allow-unauthenticated
+sudo apt-get install -y libssl1.1 --allow-unauthenticated
+clang-7 --version
+nvm install node
+node --version
