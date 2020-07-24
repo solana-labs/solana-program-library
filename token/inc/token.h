66b579d83eb6a31eebed36b279836b2b44b61c67
Tyera Eulberg <tyera@solana.com>
2020-07-24 14:14:48 -0600

    Bump sdk version, and adjust accordingly

--- token/inc/token.h
+++ token/inc/token.h
@@ -9,7 +9,7 @@
 
 #define TOKEN_MAJOR_VERSION 1
 #define TOKEN_MINOR_VERSION 0
-#define TOKEN_PATCH_VERSION 1
+#define TOKEN_PATCH_VERSION 2
 
 /**
  * Maximum number of multisignature signers (max N)
