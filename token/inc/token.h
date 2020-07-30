0d885b9baaa5f4a7c0ac5dbe54b7a70da8efd200
Michael Vines <mvines@gmail.com>
2020-07-30 15:30:28 -0700

    Bump version to 1.0.3

--- token/inc/token.h
+++ token/inc/token.h
@@ -9,7 +9,7 @@
 
 #define TOKEN_MAJOR_VERSION 1
 #define TOKEN_MINOR_VERSION 0
-#define TOKEN_PATCH_VERSION 2
+#define TOKEN_PATCH_VERSION 3
 
 /**
  * Maximum number of multisignature signers (max N)
