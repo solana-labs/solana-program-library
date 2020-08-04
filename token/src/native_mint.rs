72608ab2e3d6ea4972d42ee97e8e14d189fc6ae2
Michael Vines <mvines@gmail.com>
2020-08-04 16:12:19 -0700

    Add native_mint::DECIMALS

--- token/src/native_mint.rs
+++ token/src/native_mint.rs
@@ -1,4 +1,7 @@
 //! The Mint that represents the native token
 
+/// There are 10^9 lamports in one SOL
+pub const DECIMALS: u8 = 9;
+
 // The Mint for native SOL Token accounts
 solana_sdk::declare_id!("So11111111111111111111111111111111111111111");
