4c8b28b387271217282c71399395711d7e448c83
Michael Vines <mvines@gmail.com>
2020-08-04 21:28:40 -0700

    Add ui amount conversion helpers

--- token/src/lib.rs
+++ token/src/lib.rs
@@ -14,4 +14,15 @@ pub mod state;
 // solana-sdk version
 pub use solana_sdk;
 
+/// Convert the UI representation of a token amount (using the decimals field defined in its mint)
+/// to the raw amount
+pub fn ui_amount_to_amount(ui_amount: f64, decimals: u8) -> u64 {
+    (ui_amount * 10_usize.pow(decimals as u32) as f64) as u64
+}
+
+/// Convert a raw amount to its UI representation (using the decimals field defined in its mint)
+pub fn amount_to_ui_amount(amount: u64, decimals: u8) -> f64 {
+    amount as f64 / 10_usize.pow(decimals as u32) as f64
+}
+
 solana_sdk::declare_id!("TokenSVp5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o");
