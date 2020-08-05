02bf72b8e6236762ebc17a452817df37311bf248
Michael Vines <mvines@gmail.com>
2020-08-04 21:28:40 -0700

    Add ui amount conversion helpers

--- token/src/native_mint.rs
+++ token/src/native_mint.rs
@@ -5,3 +5,21 @@ pub const DECIMALS: u8 = 9;
 
 // The Mint for native SOL Token accounts
 solana_sdk::declare_id!("So11111111111111111111111111111111111111111");
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use solana_sdk::native_token::*;
+
+    #[test]
+    fn test_decimals() {
+        assert_eq!(
+            lamports_to_sol(42),
+            crate::amount_to_ui_amount(42, DECIMALS)
+        );
+        assert_eq!(
+            sol_to_lamports(42.),
+            crate::ui_amount_to_amount(42., DECIMALS)
+        );
+    }
+}
