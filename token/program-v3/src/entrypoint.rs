a8df113101c6ac5b796ea8734f6cf28c412e3126
Tyera Eulberg <teulberg@gmail.com>
2020-09-10 16:04:50 -0600

    Add token/program2 for next changeset (#412)
    * Add token/program2 for next changeset
    
    * Use version number instead of index

---
+++ token/program-v3/src/entrypoint.rs
@@ -0,0 +1,24 @@
+//! Program entrypoint
+
+#![cfg(feature = "program")]
+#![cfg(not(feature = "no-entrypoint"))]
+
+use crate::{error::TokenError, processor::Processor};
+use solana_sdk::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult,
+    program_error::PrintProgramError, pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+fn process_instruction<'a>(
+    program_id: &Pubkey,
+    accounts: &'a [AccountInfo<'a>],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    if let Err(error) = Processor::process(program_id, accounts, instruction_data) {
+        // catch the error so we can print it
+        error.print::<TokenError>();
+        return Err(error);
+    }
+    Ok(())
+}
---
+++ token/program-v3/src/pack.rs
@@ -0,0 +1,85 @@
+//! State transition types
+
+use crate::error::TokenError;
+use solana_sdk::program_error::ProgramError;
+
+/// Check is a token state is initialized
+pub trait IsInitialized {
+    /// Is initialized
+    fn is_initialized(&self) -> bool;
+}
+
+/// Depends on Sized
+pub trait Sealed: Sized {}
+
+/// Safely and efficiently (de)serialize account state
+pub trait Pack: Sealed {
+    /// The length, in bytes, of the packed representation
+    const LEN: usize;
+    #[doc(hidden)]
+    fn pack_into_slice(&self, dst: &mut [u8]);
+    #[doc(hidden)]
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError>;
+
+    /// Get the packed length
+    fn get_packed_len() -> usize {
+        Self::LEN
+    }
+
+    /// Unpack from slice and check if initialized
+    fn unpack(input: &[u8]) -> Result<Self, ProgramError>
+    where
+        Self: IsInitialized,
+    {
+        let value = Self::unpack_unchecked(input)?;
+        if value.is_initialized() {
+            Ok(value)
+        } else {
+            Err(TokenError::UninitializedState.into())
+        }
+    }
+
+    /// Unpack from slice without checking if initialized
+    fn unpack_unchecked(input: &[u8]) -> Result<Self, ProgramError> {
+        if input.len() != Self::LEN {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        Ok(Self::unpack_from_slice(input)?)
+    }
+
+    /// Borrow `Self` from `input` for the duration of the call to `f`, but first check that `Self`
+    /// is initialized
+    #[inline(never)]
+    fn unpack_mut<F, U>(input: &mut [u8], f: &mut F) -> Result<U, ProgramError>
+    where
+        F: FnMut(&mut Self) -> Result<U, ProgramError>,
+        Self: IsInitialized,
+    {
+        let mut t = Self::unpack(input)?;
+        let u = f(&mut t)?;
+        Self::pack(t, input)?;
+        Ok(u)
+    }
+
+    /// Borrow `Self` from `input` for the duration of the call to `f`, without checking that
+    /// `Self` has been initialized
+    #[inline(never)]
+    fn unpack_unchecked_mut<F, U>(input: &mut [u8], f: &mut F) -> Result<U, ProgramError>
+    where
+        F: FnMut(&mut Self) -> Result<U, ProgramError>,
+    {
+        let mut t = Self::unpack_unchecked(input)?;
+        let u = f(&mut t)?;
+        Self::pack(t, input)?;
+        Ok(u)
+    }
+
+    /// Pack into slice
+    fn pack(src: Self, dst: &mut [u8]) -> Result<(), ProgramError> {
+        if dst.len() != Self::LEN {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        src.pack_into_slice(dst);
+        Ok(())
+    }
+}
