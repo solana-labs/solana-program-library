58a547fcfe685578fe210054dcc86ae958b9f1c6
Greg Fitzgerald <greg@solana.com>
2020-09-13 19:49:21 -0600

    Simplify

--- themis/program/src/lib.rs
+++ themis/program/src/lib.rs
@@ -4,14 +4,11 @@
 //! An implementation of Brave's THEMIS for the Solana blockchain
 
 pub mod entrypoint;
-pub mod errors;
 pub mod instruction;
 //pub mod option;
 //pub mod pack;
 //pub mod processor;
 pub mod state;
-#[cfg(test)]
-mod utils;
 
 // Export current solana-sdk types for downstream users who may also be building with a different
 // solana-sdk version
