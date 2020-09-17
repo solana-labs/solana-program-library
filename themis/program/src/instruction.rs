655c98db92c04037e182e84c2a58106b28cce401
Greg Fitzgerald <greg@solana.com>
2020-09-17 17:21:00 -0600

    Add instruction constructors

--- themis/program/src/instruction.rs
+++ themis/program/src/instruction.rs
@@ -1,11 +1,18 @@
 //! Instruction types
 
+use crate::state::{Policies, User};
+use bincode::{serialize, serialized_size};
 use curve25519_dalek::{
     ristretto::{CompressedRistretto, RistrettoPoint},
     scalar::Scalar,
 };
 use elgamal_ristretto::public::PublicKey;
 use serde::{Deserialize, Serialize};
+use solana_sdk::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+    system_instruction,
+};
 
 /// Instructions supported by the Themis program.
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -41,8 +48,8 @@ pub enum ThemisInstruction {
     ///
     /// Accounts expected by this instruction:
     ///
-    ///   0. `[writable]`  The user account
-    ///   1. `[writable]`  The policies account
+    ///   0. `[writable, signer]`  The user account
+    ///   1. `[]`  The policies account
     CalculateAggregate {
         /// Encrypted interactions
         encrypted_interactions: Vec<(RistrettoPoint, RistrettoPoint)>,
@@ -55,7 +62,7 @@ pub enum ThemisInstruction {
     ///
     /// Accounts expected by this instruction:
     ///
-    ///   0. `[writable]`  The user account
+    ///   0. `[writable, signer]`  The user account
     SubmitProofDecryption {
         /// plaintext
         plaintext: RistrettoPoint,
@@ -74,7 +81,7 @@ pub enum ThemisInstruction {
     ///
     /// Accounts expected by this instruction:
     ///
-    ///   0. `[writable]`  The user account
+    ///   0. `[writable, signer]`  The user account
     RequestPayment {
         /// Encrypted aggregate
         encrypted_aggregate: (RistrettoPoint, RistrettoPoint),
@@ -85,4 +92,119 @@ pub enum ThemisInstruction {
         /// Proof correct decryption
         proof_correct_decryption: RistrettoPoint,
     },
+}
+
+/// Return an `InitializeUserAccount` instruction.
+fn initialize_user_account(user_pubkey: &Pubkey) -> Instruction {
+    let data = ThemisInstruction::InitializeUserAccount;
+
+    let accounts = vec![AccountMeta::new(*user_pubkey, false)];
+
+    Instruction {
+        program_id: crate::id(),
+        accounts,
+        data: serialize(&data).unwrap(),
+    }
+}
+
+/// Return two instructions that create and initialize a user account.
+pub fn create_user_account(from: &Pubkey, user_pubkey: &Pubkey, lamports: u64) -> Vec<Instruction> {
+    let space = serialized_size(&User::default()).unwrap();
+    vec![
+        system_instruction::create_account(from, user_pubkey, lamports, space, &crate::id()),
+        initialize_user_account(user_pubkey),
+    ]
+}
+
+/// Return an `InitializePoliciesAccount` instruction.
+fn initialize_policies_account(policies_pubkey: &Pubkey, scalars: Vec<Scalar>) -> Instruction {
+    let data = ThemisInstruction::InitializePoliciesAccount { scalars };
+    let accounts = vec![AccountMeta::new(*policies_pubkey, false)];
+    Instruction {
+        program_id: crate::id(),
+        accounts,
+        data: serialize(&data).unwrap(),
+    }
+}
+
+/// Return two instructions that create and initialize a policies account.
+pub fn create_policies_account(
+    from: &Pubkey,
+    policies_pubkey: &Pubkey,
+    lamports: u64,
+    scalars: Vec<Scalar>,
+) -> Vec<Instruction> {
+    let space = serialized_size(&Policies {
+        scalars: scalars.clone(),
+        ..Policies::default()
+    })
+    .unwrap();
+    vec![
+        system_instruction::create_account(from, policies_pubkey, lamports, space, &crate::id()),
+        initialize_policies_account(policies_pubkey, scalars),
+    ]
+}
+
+/// Return a `CalculateAggregate` instruction.
+pub fn calculate_aggregate(
+    user_pubkey: &Pubkey,
+    policies_pubkey: &Pubkey,
+    encrypted_interactions: Vec<(RistrettoPoint, RistrettoPoint)>,
+    public_key: PublicKey,
+) -> Instruction {
+    let data = ThemisInstruction::CalculateAggregate {
+        encrypted_interactions,
+        public_key,
+    };
+    let accounts = vec![
+        AccountMeta::new(*user_pubkey, true),
+        AccountMeta::new_readonly(*policies_pubkey, false),
+    ];
+    Instruction {
+        program_id: crate::id(),
+        accounts,
+        data: serialize(&data).unwrap(),
+    }
+}
+
+/// Return a `SubmitProofDecryption` instruction.
+pub fn submit_proof_decryption(
+    user_pubkey: &Pubkey,
+    plaintext: RistrettoPoint,
+    announcement_g: CompressedRistretto,
+    announcement_ctx: CompressedRistretto,
+    response: Scalar,
+) -> Instruction {
+    let data = ThemisInstruction::SubmitProofDecryption {
+        plaintext,
+        announcement_g,
+        announcement_ctx,
+        response,
+    };
+    let accounts = vec![AccountMeta::new(*user_pubkey, true)];
+    Instruction {
+        program_id: crate::id(),
+        accounts,
+        data: serialize(&data).unwrap(),
+    }
+}
+
+/// Return a `RequestPayment` instruction.
+pub fn request_payment(
+    user_pubkey: &Pubkey,
+    encrypted_aggregate: (RistrettoPoint, RistrettoPoint),
+    decrypted_aggregate: RistrettoPoint,
+    proof_correct_decryption: RistrettoPoint,
+) -> Instruction {
+    let data = ThemisInstruction::RequestPayment {
+        encrypted_aggregate,
+        decrypted_aggregate,
+        proof_correct_decryption,
+    };
+    let accounts = vec![AccountMeta::new(*user_pubkey, true)];
+    Instruction {
+        program_id: crate::id(),
+        accounts,
+        data: serialize(&data).unwrap(),
+    }
 }
