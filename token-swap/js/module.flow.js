23a393801f1d08cf6e36036a7978f25b07c062e4
Daniel Kelleher <dankelleher@yahoo.com>
2020-09-18 09:31:16 +0200

    Token-swap ts, flow bindings and build using rollup. (#463)
    * Added token-swap ts and flow bindings and build pure JS versions using rollup, to match the token program
    
    * Added references to the built files in token-swap package.json
    
    * Added missing Account import
    
    * Fix flow module syntax errors

---
+++ token-swap/js/module.flow.js
@@ -0,0 +1,117 @@
+/**
+ * Flow Library definition for spl-token-swap
+ *
+ * This file is manually maintained
+ *
+ */
+
+declare module '@solana/spl-token-swap' {
+  // === client/token-swap.js ===
+  declare export class Numberu64 extends BN {
+    toBuffer(): Buffer;
+    static fromBuffer(buffer: Buffer): Numberu64;
+  }
+
+  declare export type TokenSwapInfo = {|
+    nonce: number,
+    tokenAccountA: PublicKey,
+    tokenAccountB: PublicKey,
+    tokenPool: PublicKey,
+    feesNumerator: Numberu64,
+    feesDenominator: Numberu64,
+    feeRatio: number,
+  |};
+
+  declare export class TokenSwap {
+    constructor(connection: Connection, tokenSwap: PublicKey, programId: PublicKey, payer: Account): TokenSwap;
+
+    static getMinBalanceRentForExemptTokenSwap(
+      connection: Connection,
+    ): Promise<number>;
+
+    static createTokenSwap(
+      connection: Connection,
+      payer: Account,
+      tokenSwapAccount: Account,
+      authority: PublicKey,
+      tokenAccountA: PublicKey,
+      tokenAccountB: PublicKey,
+      tokenPool: PublicKey,
+      tokenAccountPool: PublicKey,
+      tokenProgramId: PublicKey,
+      nonce: number,
+      feeNumerator: number,
+      feeDenominator: number,
+      programId: PublicKey,
+    ): Promise<TokenSwap>;
+
+    getInfo(): Promise<TokenSwapInfo>;
+    swap(
+      authority: PublicKey,
+      source: PublicKey,
+      swap_source: PublicKey,
+      swap_destination: PublicKey,
+      destination: PublicKey,
+      tokenProgramId: PublicKey,
+      amount: number | Numberu64,
+    ): Promise<TransactionSignature>;
+
+    swapInstruction(
+      authority: PublicKey,
+      source: PublicKey,
+      swap_source: PublicKey,
+      swap_destination: PublicKey,
+      destination: PublicKey,
+      tokenProgramId: PublicKey,
+      amount: number | Numberu64,
+    ): TransactionInstruction;
+
+    deposit(
+      authority: PublicKey,
+      sourceA: PublicKey,
+      sourceB: PublicKey,
+      intoA: PublicKey,
+      intoB: PublicKey,
+      poolToken: PublicKey,
+      poolAccount: PublicKey,
+      tokenProgramId: PublicKey,
+      amount: number | Numberu64,
+    ): Promise<TransactionSignature>;
+
+    depositInstruction(
+      authority: PublicKey,
+      sourceA: PublicKey,
+      sourceB: PublicKey,
+      intoA: PublicKey,
+      intoB: PublicKey,
+      poolToken: PublicKey,
+      poolAccount: PublicKey,
+      tokenProgramId: PublicKey,
+      amount: number | Numberu64,
+    ): TransactionInstruction;
+
+    withdraw(
+      authority: PublicKey,
+      poolMint: PublicKey,
+      sourcePoolAccount: PublicKey,
+      fromA: PublicKey,
+      fromB: PublicKey,
+      userAccountA: PublicKey,
+      userAccountB: PublicKey,
+      tokenProgramId: PublicKey,
+      amount: number | Numberu64,
+    ): Promise<TransactionSignature>;
+
+    withdrawInstruction(
+      authority: PublicKey,
+      poolMint: PublicKey,
+      sourcePoolAccount: PublicKey,
+      fromA: PublicKey,
+      fromB: PublicKey,
+      userAccountA: PublicKey,
+      userAccountB: PublicKey,
+      tokenProgramId: PublicKey,
+      amount: number | Numberu64,
+    ): TransactionInstruction;
+  }
+}
---
+++ token-swap/js/rollup.config.js
@@ -0,0 +1,74 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+
+function generateConfig(configType) {
+  const config = {
+    input: 'client/token-swap.js',
+    plugins: [
+      babel({
+        configFile: './babel.rollup.config.json',
+        exclude: 'node_modules/**',
+        babelHelpers: 'runtime',
+      }),
+      commonjs(),
+      copy({
+        targets: [{src: 'module.d.ts', dest: 'lib', rename: 'index.d.ts'}],
+      }),
+    ],
+  };
+
+  switch (configType) {
+    case 'browser':
+      // TODO: Add support
+      break;
+    case 'node':
+      config.output = [
+        {
+          file: 'lib/index.cjs.js',
+          format: 'cjs',
+          sourcemap: true,
+        },
+        {
+          file: 'lib/index.esm.js',
+          format: 'es',
+          sourcemap: true,
+        },
+      ];
+
+      // Quash 'Unresolved dependencies' complaints for modules listed in the
+      // package.json "dependencies" section.  Unfortunately this list is manually
+      // maintained.
+      config.external = [
+        'assert',
+        '@babel/runtime/core-js/get-iterator',
+        '@babel/runtime/core-js/json/stringify',
+        '@babel/runtime/core-js/object/assign',
+        '@babel/runtime/core-js/object/get-prototype-of',
+        '@babel/runtime/core-js/object/keys',
+        '@babel/runtime/core-js/promise',
+        '@babel/runtime/helpers/asyncToGenerator',
+        '@babel/runtime/helpers/classCallCheck',
+        '@babel/runtime/helpers/createClass',
+        '@babel/runtime/helpers/defineProperty',
+        '@babel/runtime/helpers/get',
+        '@babel/runtime/helpers/getPrototypeOf',
+        '@babel/runtime/helpers/inherits',
+        '@babel/runtime/helpers/possibleConstructorReturn',
+        '@babel/runtime/helpers/slicedToArray',
+        '@babel/runtime/helpers/toConsumableArray',
+        '@babel/runtime/helpers/typeof',
+        '@babel/runtime/regenerator',
+        'bn.js',
+        'buffer-layout',
+        '@solana/web3.js',
+      ];
+      break;
+    default:
+      throw new Error(`Unknown configType: ${configType}`);
+  }
+
+  return config;
+}
+
+export default [generateConfig('node')];
