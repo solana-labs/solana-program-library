08fb35ee26730d5d5e6e29506d8e3c2189b91a99
Trent Nelson <trent@solana.com>
2020-09-30 22:14:14 -0600

    CI: Run THEMIS tests

--- ci/script.sh
+++ ci/script.sh
@@ -67,6 +67,7 @@ done
 
 # Run SPL Token's performance monitor
 _ cargo test --manifest-path=token/perf-monitor/Cargo.toml -- --nocapture
+_ cargo test --manifest-path=themis/client/Cargo.toml -- --nocapture
 
 
 # Test token js bindings
