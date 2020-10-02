a2e2f19123071f2ce33fc185c4a4fef76afa4369
Greg Fitzgerald <greg@solana.com>
2020-10-02 16:20:41 -0600

    Update Themis program

--- themis/program/src/lib.rs
+++ themis/program/src/lib.rs
@@ -11,4 +11,4 @@ pub mod state;
 // solana-sdk version
 pub use solana_sdk;
 
-solana_sdk::declare_id!("6q92wS9neZdex1jT72zqeWEPJ6YYNSqYjF6PKrqbVUwJ");
+solana_sdk::declare_id!("F3FWeYPjD1jeR6UykMj1GRbCcmoxtJnDiPuFdTLRGvb6");
