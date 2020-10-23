a17bcc50ef247812da7aa8fab63e73b4b9ab1a2d
Greg Fitzgerald <greg@solana.com>
2020-10-23 13:14:25 -0600

    Update Ristretto keys

--- themis/program_ristretto/src/lib.rs
+++ themis/program_ristretto/src/lib.rs
@@ -11,4 +11,4 @@ pub mod state;
 // solana-sdk version
 pub use solana_sdk;
 
-solana_sdk::declare_id!("FHpg4MzrRSmTjNhPmEMbYhhe7sjrUFefe2hJg2m7fGvP");
+solana_sdk::declare_id!("C8tR6A3CWcEL46KHx7TJcbyR4hdoPi1wrBBQa42FuJMF");
