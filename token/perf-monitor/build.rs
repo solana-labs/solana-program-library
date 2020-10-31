55c33b9b8094059a5ba18de6a61b885a5acc9cfe
Michael Vines <mvines@gmail.com>
2020-10-30 22:05:04 -0700

    Update token/perf-monitor to latest solana-program/solana-sdk

--- token/perf-monitor/build.rs
+++ token/perf-monitor/build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     println!("cargo:warning=(not a warning) Building BPF token program");
     Command::new("cargo")
-        .arg("build-bpf")
+        .args(&["build-bpf", "--manifest-path", "../program/Cargo.toml"])
         .status()
         .expect("Failed to build BPF token program")
         .success();
