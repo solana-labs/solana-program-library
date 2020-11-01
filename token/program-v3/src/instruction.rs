c312a0576aab034448e598fe04a4bee6973ef135
Tyera Eulberg <teulberg@gmail.com>
2020-10-31 19:10:33 -0600

    Clippy suggestions (#765)

--- token/program-v3/src/instruction.rs
+++ token/program-v3/src/instruction.rs
@@ -1056,7 +1056,7 @@ pub fn burn2(
 
 /// Utility function that checks index is between MIN_SIGNERS and MAX_SIGNERS
 pub fn is_valid_signer_index(index: usize) -> bool {
-    !(index < MIN_SIGNERS || index > MAX_SIGNERS)
+    (MIN_SIGNERS..=MAX_SIGNERS).contains(&index)
 }
 
 #[cfg(test)]
