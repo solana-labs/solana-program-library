8944f428fe693c3a4226bf766a79be9c75e8e520
Michael Vines <mvines@gmail.com>
2020-11-01 19:26:17 -0800

    Skip bpf-build while running clippy

--- shared-memory/program/build.rs
+++ shared-memory/program/build.rs
@@ -1,8 +1,14 @@
 use std::process::{exit, Command};
 
 fn main() {
-    if std::env::var("XARGO").is_err() {
-        println!("cargo:warning=(not a warning) Building BPF shared-memory program");
+    if std::env::var("XARGO").is_err()
+        && std::env::var("RUSTC_WRAPPER").is_err()
+        && std::env::var("RUSTC_WORKSPACE_WRAPPER").is_err()
+    {
+        println!(
+            "cargo:warning=(not a warning) Building BPF {} program",
+            std::env::var("CARGO_PKG_NAME").unwrap()
+        );
         if !Command::new("cargo")
             .arg("build-bpf")
             .status()
