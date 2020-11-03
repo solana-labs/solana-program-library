7908817ccd709af326cdb3da59adbd5e027af8b0
Michael Vines <mvines@gmail.com>
2020-11-03 11:22:07 -0800

    Clean up comments

--- associated-token-account/program/src/lib.rs
+++ associated-token-account/program/src/lib.rs
@@ -1,11 +1,11 @@
-//! Convention for associating token accounts with a primary account (such as a user wallet)
+//! Convention for associating token accounts with a user wallet
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
 
 mod entrypoint;
 pub mod processor;
 
-// Export current sdk types for downstream users building with a different sdk version
+// Export current SDK types for downstream users building with a different SDK version
 pub use solana_program;
 use solana_program::{
     instruction::{AccountMeta, Instruction},
@@ -31,7 +31,7 @@ pub(crate) fn get_associated_token_address_and_bump_seed(
     )
 }
 
-/// Derives the associated SPL token address for the given wallet address and SPL Token mint
+/// Derives the associated token account address for the given wallet address and token mint
 pub fn get_associated_token_address(
     wallet_address: &Pubkey,
     spl_token_mint_address: &Pubkey,
@@ -39,14 +39,14 @@ pub fn get_associated_token_address(
     get_associated_token_address_and_bump_seed(&wallet_address, &spl_token_mint_address, &id()).0
 }
 
-/// Create an associated token account for a wallet address
+/// Create an associated token account for the given wallet address and token mint
 ///
 /// Accounts expected by this instruction:
 ///
 ///   0. `[writeable,signer]` Funding account (must be a system account)
-///   1. `[writeable]` Associated token account address
+///   1. `[writeable]` Associated token account address to be created
 ///   2. `[]` Wallet address for the new associated token account
-///   3. `[]` The SPL token mint for the associated token account
+///   3. `[]` The token mint for new associated token account
 ///   4. `[]` System program
 ///   4. `[]` SPL Token program
 ///   5. `[]` Rent sysvar
