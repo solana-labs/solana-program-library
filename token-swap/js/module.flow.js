d9e2930993e6b75a0f64442453f1f6ef185b7501
Yutaro Mori <me@yutaromori.com>
2020-11-09 16:00:06 +0900

    Fix type definitions for TokenSwap class

--- token-swap/js/module.flow.js
+++ token-swap/js/module.flow.js
@@ -46,7 +46,6 @@ declare module '@solana/spl-token-swap' {
     ): Promise<number>;
 
     static createInitSwapInstruction(
-      programId: PublicKey,
       tokenSwapAccount: Account,
       authority: PublicKey,
       tokenAccountA: PublicKey,
@@ -55,6 +54,7 @@ declare module '@solana/spl-token-swap' {
       feeAccount: PublicKey,
       tokenAccountPool: PublicKey,
       tokenProgramId: PublicKey,
+      swapProgramId: PublicKey,
       nonce: number,
       curveType: number,
       tradeFeeNumerator: number,
@@ -81,11 +81,12 @@ declare module '@solana/spl-token-swap' {
       authority: PublicKey,
       tokenAccountA: PublicKey,
       tokenAccountB: PublicKey,
-      tokenPool: PublicKey,
+      poolToken: PublicKey,
       mintA: PublicKey,
       mintB: PublicKey,
       feeAccount: PublicKey,
       tokenAccountPool: PublicKey,
+      swapProgramId: PublicKey,
       tokenProgramId: PublicKey,
       nonce: number,
       curveType: number,
@@ -97,7 +98,6 @@ declare module '@solana/spl-token-swap' {
       ownerWithdrawFeeDenominator: number,
       hostFeeNumerator: number,
       hostFeeDenominator: number,
-      programId: PublicKey,
     ): Promise<TokenSwap>;
 
     swap(
@@ -127,14 +127,9 @@ declare module '@solana/spl-token-swap' {
     ): TransactionInstruction;
 
     deposit(
-      authority: PublicKey,
-      sourceA: PublicKey,
-      sourceB: PublicKey,
-      intoA: PublicKey,
-      intoB: PublicKey,
-      poolToken: PublicKey,
+      userAccountA: PublicKey,
+      userAccountB: PublicKey,
       poolAccount: PublicKey,
-      tokenProgramId: PublicKey,
       poolTokenAmount: number | Numberu64,
       maximumTokenA: number | Numberu64,
       maximumTokenB: number | Numberu64,
