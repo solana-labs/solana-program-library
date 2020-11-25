781435634bb6e10a6a83961058672badf47583f2
Jon Cinque <jon.cinque@gmail.com>
2020-11-25 13:39:24 +0100

    Add nightly run

--- ci/fuzz.sh
+++ ci/fuzz.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
-if [[ -z $1 ]]; then
-  fuzz_targets=(
-    token-swap-instructions
-  )
-else
-  fuzz_targets=( "$@" )
+usage() {
+  exitcode=0
+  if [[ -n "$1" ]]; then
+    exitcode=1
+    echo "Error: $*"
+  fi
+  echo "Usage: $0 [fuzz-target] [run-time-in-seconds]"
+  exit $exitcode
+}
+
+fuzz_target=$1
+if [[ -z $fuzz_target ]]; then
+  usage "No fuzz target provided"
 fi
 
-for fuzz_target in ${fuzz_targets[@]}; do
-  HFUZZ_RUN_ARGS="--run_time 30 --exit_upon_crash" cargo hfuzz run $fuzz_target
+run_time=$2
+if [[ -z $2 ]]; then
+  usage "No runtime provided"
+fi
 
-  # Until https://github.com/rust-fuzz/honggfuzz-rs/issues/16 is resolved,
-  # hfuzz does not return an error code on crash, so look for a crash artifact
-  for crash_file in ./hfuzz_workspace/"$fuzz_target"/*.fuzz; do
-    # Check if the glob gets expanded to existing files.
-    if [[ -e "$crash_file" ]]; then
-      echo ".fuzz file $crash_file found, meaning some error occurred, exiting"
-      exit 1
-    fi
-    # Break early -- we just need one iteration to see if a failure occurred
-    break
-  done
+set -x
+
+HFUZZ_RUN_ARGS="--run_time $run_time --exit_upon_crash" cargo hfuzz run $fuzz_target
+
+# Until https://github.com/rust-fuzz/honggfuzz-rs/issues/16 is resolved,
+# hfuzz does not return an error code on crash, so look for a crash artifact
+for crash_file in ./hfuzz_workspace/"$fuzz_target"/*.fuzz; do
+  # Check if the glob gets expanded to existing files.
+  if [[ -e "$crash_file" ]]; then
+    echo ".fuzz file $crash_file found, meaning some error occurred, exiting"
+    exit 1
+  fi
+  # Break early -- we just need one iteration to see if a failure occurred
+  break
 done
