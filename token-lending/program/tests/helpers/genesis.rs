5676d0f45c311fd8113209a4a973c283bc260405
Justin Starry <justin@solana.com>
2021-01-09 09:59:42 +0800

    lending: Add upgradable loader to genesis accounts creation

--- token-lending/program/tests/helpers/genesis.rs
+++ token-lending/program/tests/helpers/genesis.rs
@@ -18,6 +18,17 @@ pub struct Base64Account {
     pub executable: bool,
 }
 
+impl From<Account> for Base64Account {
+    fn from(account: Account) -> Self {
+        Self {
+            owner: account.owner.to_string(),
+            balance: account.lamports,
+            executable: account.executable,
+            data: base64::encode(&account.data),
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct GenesisAccounts(HashMap<String, Base64Account>);
 
@@ -25,66 +36,49 @@ impl GenesisAccounts {
     pub fn insert_upgradeable_program(&mut self, program_id: Pubkey, filename: &str) {
         let program_file = find_file(filename).expect(&format!("couldn't find {}", filename));
         let program_data = read_file(program_file);
-        let programdata_address = Pubkey::new_unique();
         let upgrade_authority_keypair =
             read_keypair_file("tests/fixtures/lending_market.json").unwrap();
+
+        let programdata_address =
+            Pubkey::find_program_address(&[program_id.as_ref()], &bpf_loader_upgradeable::id()).0;
         let programdata_data_offset = UpgradeableLoaderState::programdata_data_offset().unwrap();
-        let programdata_space = program_data.len() + programdata_data_offset;
-        let programdata_state = UpgradeableLoaderState::ProgramData {
-            slot: 0,
-            upgrade_authority_address: Some(upgrade_authority_keypair.pubkey()),
-        };
+        let programdata_space = 2 * program_data.len() + programdata_data_offset;
         let mut programdata_account = Account::new_data_with_space(
             u32::MAX as u64,
-            &programdata_state,
+            &UpgradeableLoaderState::ProgramData {
+                slot: 0,
+                upgrade_authority_address: Some(upgrade_authority_keypair.pubkey()),
+            },
             programdata_space,
             &bpf_loader_upgradeable::id(),
         )
         .unwrap();
+
         programdata_account.data
             [programdata_data_offset..programdata_data_offset + program_data.len()]
             .copy_from_slice(&program_data[..]);
-        self.0.insert(
-            programdata_address.to_string(),
-            Base64Account {
-                owner: programdata_account.owner.to_string(),
-                balance: programdata_account.lamports,
-                executable: false,
-                data: base64::encode(&programdata_account.data),
-            },
-        );
 
-        let program_state = UpgradeableLoaderState::Program {
-            programdata_address,
-        };
-        let program_account = Account::new_data(
+        self.0
+            .insert(programdata_address.to_string(), programdata_account.into());
+
+        let mut program_account = Account::new_data(
             u32::MAX as u64,
-            &program_state,
+            &UpgradeableLoaderState::Program {
+                programdata_address,
+            },
             &bpf_loader_upgradeable::id(),
         )
         .unwrap();
-        self.0.insert(
-            program_id.to_string(),
-            Base64Account {
-                owner: program_account.owner.to_string(),
-                balance: program_account.lamports,
-                executable: true,
-                data: base64::encode(&program_account.data),
-            },
-        );
+        program_account.executable = true;
+
+        self.0
+            .insert(program_id.to_string(), program_account.into());
     }
 
     pub async fn fetch_and_insert(&mut self, banks_client: &mut BanksClient, pubkey: Pubkey) {
-        let account: Account = banks_client.get_account(pubkey).await.unwrap().unwrap();
-        self.0.insert(
-            pubkey.to_string(),
-            Base64Account {
-                owner: account.owner.to_string(),
-                balance: u32::MAX as u64,
-                executable: false,
-                data: base64::encode(&account.data),
-            },
-        );
+        let mut account: Account = banks_client.get_account(pubkey).await.unwrap().unwrap();
+        account.lamports = u32::MAX as u64;
+        self.0.insert(pubkey.to_string(), account.into());
     }
 
     pub fn write_yaml(&self) {
