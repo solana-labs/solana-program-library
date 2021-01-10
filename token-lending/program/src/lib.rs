e360fc9e4f5c1a842f222cf4548ad943d7615b5a
Justin Starry <justin@solana.com>
2021-01-10 08:37:27 +0800

    lending: Add upgradable loader to genesis accounts creation (#1048)
    * wip
    
    * lending: Add upgradable loader to genesis accounts creation
    
    * clippy
    
    * fix warning

--- token-lending/program/src/lib.rs
+++ token-lending/program/src/lib.rs
@@ -13,4 +13,4 @@ pub mod state;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("TokenLend1ng1111111111111111111111111111111");
+solana_program::declare_id!("TokenLending1111111111111111111111111111111");
--- token-lending/program/tests/genesis_accounts.rs
+++ token-lending/program/tests/genesis_accounts.rs
@@ -2,7 +2,6 @@
 
 mod helpers;
 
-use helpers::genesis::GenesisAccounts;
 use helpers::*;
 use solana_sdk::signature::Keypair;
 use spl_token_lending::{
@@ -252,27 +251,31 @@ async fn test_success() {
         )
         .await;
 
-    let mut genesis_accounts = GenesisAccounts::default();
-    lending_market
-        .add_to_genesis(&mut banks_client, &mut genesis_accounts)
-        .await;
-    sol_reserve
-        .add_to_genesis(&mut banks_client, &mut genesis_accounts)
-        .await;
-    srm_reserve
-        .add_to_genesis(&mut banks_client, &mut genesis_accounts)
-        .await;
-    usdc_reserve
-        .add_to_genesis(&mut banks_client, &mut genesis_accounts)
-        .await;
-    sol_usdc_dex_market
-        .add_to_genesis(&mut banks_client, &mut genesis_accounts)
-        .await;
-    srm_usdc_dex_market
-        .add_to_genesis(&mut banks_client, &mut genesis_accounts)
-        .await;
-
     // Only dump the accounts if the feature is specified
     #[cfg(feature = "test-dump-genesis-accounts")]
-    genesis_accounts.write_yaml();
+    {
+        use helpers::genesis::GenesisAccounts;
+        let mut genesis_accounts = GenesisAccounts::default();
+        lending_market
+            .add_to_genesis(&mut banks_client, &mut genesis_accounts)
+            .await;
+        sol_reserve
+            .add_to_genesis(&mut banks_client, &mut genesis_accounts)
+            .await;
+        srm_reserve
+            .add_to_genesis(&mut banks_client, &mut genesis_accounts)
+            .await;
+        usdc_reserve
+            .add_to_genesis(&mut banks_client, &mut genesis_accounts)
+            .await;
+        sol_usdc_dex_market
+            .add_to_genesis(&mut banks_client, &mut genesis_accounts)
+            .await;
+        srm_usdc_dex_market
+            .add_to_genesis(&mut banks_client, &mut genesis_accounts)
+            .await;
+        genesis_accounts
+            .insert_upgradeable_program(spl_token_lending::id(), "spl_token_lending.so");
+        genesis_accounts.write_yaml();
+    }
 }
