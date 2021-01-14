f413d624ed12da69dc81e024b8676863106fea2e
Justin Starry <justin@solana.com>
2021-01-14 12:53:36 +0800

    Pin rust nightly version until ICE is fixed

--- ci/rust-version.sh
+++ ci/rust-version.sh
@@ -21,18 +21,19 @@ else
   stable_version=1.48.0
 fi
 
-# if [[ -n $RUST_NIGHTLY_VERSION ]]; then
-#   nightly_version="$RUST_NIGHTLY_VERSION"
-# else
-#   nightly_version=2020-08-17
-# fi
+if [[ -n $RUST_NIGHTLY_VERSION ]]; then
+  nightly_version="$RUST_NIGHTLY_VERSION"
+else
+  # nightly 2021-01-13 fails due to https://github.com/rust-lang/rust/issues/80956
+  nightly_version=2021-01-12
+fi
 
 
 export rust_stable="$stable_version"
 export rust_stable_docker_image=solanalabs/rust:"$stable_version"
 
-# export rust_nightly=nightly-"$nightly_version"
-# export rust_nightly_docker_image=solanalabs/rust-nightly:"$nightly_version"
+export rust_nightly=nightly-"$nightly_version"
+export rust_nightly_docker_image=solanalabs/rust-nightly:"$nightly_version"
 
 [[ -z $1 ]] || (
 
