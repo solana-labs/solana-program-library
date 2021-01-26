b2e5f3cc5166c98031f0c3f1e8375581858b32b0
Trent Nelson <trent@solana.com>
2021-01-25 22:09:30 -0700

    CI: Don't install solana bins if we already have them

--- ci/solana-version.sh
+++ ci/solana-version.sh
@@ -21,18 +21,27 @@ fi
 export solana_version="$solana_version"
 export solana_docker_image=solanalabs/solana:"$solana_version"
 export solana_path="$PWD/solana-install"
+export PATH="$solana_path"/active_release/bin:"$PATH"
+
+installed_solana_version() {
+  declare version_yml="$solana_path"/active_release/version.yml
+  sed -e $'s/channel: \\(.*\\)/\\1/\nt\nd' "$version_yml" 2>/dev/null
+}
 
 if [[ -n $1 ]]; then
   case $1 in
   install)
-    curl -sSfL https://release.solana.com/$solana_version/install \
-      | sh -s - $solana_version \
-        --no-modify-path \
-        --data-dir "$solana_path" \
-        --config "$solana_path"/config.yml
+    if [[ "$(installed_solana_version)" = "$solana_version" ]]; then
+      echo "$0: Skipping install. Requested version ($solana_version) already available" >&2
+    else
+      curl -sSfL https://release.solana.com/$solana_version/install \
+        | sh -s - $solana_version \
+          --no-modify-path \
+          --data-dir "$solana_path" \
+          --config "$solana_path"/config.yml
 
-    export PATH="$solana_path"/active_release/bin:"$PATH"
-    solana --version
+      solana --version
+    fi
     ;;
   *)
     echo "$0: Note: ignoring unknown argument: $1" >&2
