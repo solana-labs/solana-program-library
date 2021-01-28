190e664dd1595276e56b4b29e2a63b84619159c7
Tyera Eulberg <teulberg@gmail.com>
2021-01-28 12:21:21 -0700

    Add initial signed-memo program (#1135)
    * Initial s-memo
    
    * Populate readme
    
    * Add signed-memo to spl docs
    
    * Log less, fail faster
    
    * Replace and bump memo
    
    * Update memo id
    
    * Add memo prefix and len
    
    * Add test that demonstrates compute bounds
    
    * Add logging and compute to memo docs

--- memo/program/src/entrypoint.rs
+++ memo/program/src/entrypoint.rs
@@ -1,43 +1,16 @@
 //! Program entrypoint
 
+#![cfg(not(feature = "no-entrypoint"))]
+
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, program_error::ProgramError,
-    pubkey::Pubkey,
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
 };
-use std::str::from_utf8;
 
 entrypoint!(process_instruction);
 fn process_instruction(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    from_utf8(instruction_data).map_err(|_| ProgramError::InvalidInstructionData)?;
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use solana_program::{program_error::ProgramError, pubkey::Pubkey};
-
-    #[test]
-    fn test_utf8_memo() {
-        let program_id = Pubkey::new(&[0; 32]);
-
-        let string = b"letters and such";
-        assert_eq!(Ok(()), process_instruction(&program_id, &[], string));
-
-        let emoji = "🐆".as_bytes();
-        let bytes = [0xF0, 0x9F, 0x90, 0x86];
-        assert_eq!(emoji, bytes);
-        assert_eq!(Ok(()), process_instruction(&program_id, &[], &emoji));
-
-        let mut bad_utf8 = bytes;
-        bad_utf8[3] = 0xFF; // Invalid UTF-8 byte
-        assert_eq!(
-            Err(ProgramError::InvalidInstructionData),
-            process_instruction(&program_id, &[], &bad_utf8)
-        );
-    }
+    crate::processor::process_instruction(program_id, accounts, instruction_data)
 }
