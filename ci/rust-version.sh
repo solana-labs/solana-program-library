d90d702f88642cf6d977ce2a8698969d46b06732
Jon Cinque <jon.cinque@gmail.com>
2021-02-04 13:00:15 +0100

    Update rust stable to 1.49

--- ci/rust-version.sh
+++ ci/rust-version.sh
@@ -18,7 +18,7 @@
 if [[ -n $RUST_STABLE_VERSION ]]; then
   stable_version="$RUST_STABLE_VERSION"
 else
-  stable_version=1.48.0
+  stable_version=1.49.0
 fi
 
 if [[ -n $RUST_NIGHTLY_VERSION ]]; then
