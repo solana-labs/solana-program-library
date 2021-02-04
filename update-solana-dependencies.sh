d03a5390fffe629c1d190a610144c6f6c0cb3897
Tyera Eulberg <teulberg@gmail.com>
2021-02-04 12:02:28 -0700

    Update update-solana-dependencies.sh
    Co-authored-by: Michael Vines <mvines@gmail.com>

--- update-solana-dependencies.sh
+++ update-solana-dependencies.sh
@@ -37,6 +37,6 @@ set -x
 for crate in "${crates[@]}"; do
   sed -i -e "s#\(${crate} = \"\).*\(\"\)#\1$solana_ver\2#g" "${tomls[@]}"
 done
-if [[ -z $tokio_ver ]]; then
+if [[ -n $tokio_ver ]]; then
   sed -i -e "s#\(tokio.*version *= *\"\)[^\"]*\(\".*$\)#\1$tokio_ver\2#g" "${tomls[@]}"
 fi
