aebc693a0ef27800990b8f5ed7f9ffd7a47d3808
Trent Nelson <trent@solana.com>
2021-02-05 18:55:44 -0700

    token: Get `COption` and `Pubkey` C bindings from solana-program crate

--- utils/cgen/src/main.rs
+++ utils/cgen/src/main.rs
@@ -32,7 +32,7 @@ fn token<P: AsRef<Path>>(crate_dir: P) {
         },
         parse: cbindgen::ParseConfig {
             parse_deps: true,
-            include: Some(vec!["solana-sdk".to_string()]),
+            include: Some(vec!["solana-program".to_string(), "solana-sdk".to_string()]),
             ..cbindgen::ParseConfig::default()
         },
         ..cbindgen::Config::default()
