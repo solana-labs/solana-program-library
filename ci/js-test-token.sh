e66729e71f673eb71271580875f34c12fbd280a2
Dummy Tester 123 <dummytester123@hey.com>
2021-02-08 10:00:02 -0600

    Fix for js test token

--- ci/js-test-token.sh
+++ ci/js-test-token.sh
@@ -9,6 +9,6 @@ cd token/js
 npm install
 npm run lint
 npm run flow
-npx tsc module.d.ts
+npx tsc --esModuleInterop module.d.ts
 npm run start-with-test-validator
 PROGRAM_VERSION=2.0.4 npm run start-with-test-validator
