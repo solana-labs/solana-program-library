8d8a8f83b00313d958fce792e1fef84303974f0d
Jon Cinque <jon.cinque@gmail.com>
2021-02-10 13:11:26 +0100

    Remove generic newtonian, use traits

--- math/program/src/approximations.rs
+++ math/program/src/approximations.rs
@@ -1,27 +1,26 @@
 //! Approximation calculations
 
-/// Perform newtonian root directly on u64
-pub fn newtonian_root(radicand: u64, root: u64, mut guess: u64, iterations: u64) -> Option<u64> {
-    let zero = 0;
-    if radicand == zero {
-        return Some(zero);
+use {
+    num_traits::{CheckedAdd, CheckedDiv, One, Zero},
+    std::cmp::Eq,
+};
+
+const SQRT_ITERATIONS: u8 = 50;
+
+/// Perform square root
+pub fn sqrt<T: CheckedAdd + CheckedDiv + One + Zero + Eq + Copy>(radicand: T) -> Option<T> {
+    if radicand == T::zero() {
+        return Some(T::zero());
     }
-    if root == zero {
-        return None;
-    }
-    let one = 1;
-    let root_minus_one = root.checked_sub(one)?;
-    let root_minus_one_whole = root_minus_one as u32;
+    // A good initial guess is the average of the interval that contains the
+    // input number.  For all numbers, that will be between 1 and the given number.
+    let one = T::one();
+    let two = one.checked_add(&one)?;
+    let mut guess = radicand.checked_div(&two)?.checked_add(&one)?;
     let mut last_guess = guess;
-    for _ in 0..iterations {
-        // x_k+1 = ((n - 1) * x_k + A / (x_k ^ (n - 1))) / n
-        let first_term = root_minus_one.checked_mul(guess)?;
-        let power = guess.checked_pow(root_minus_one_whole);
-        let second_term = match power {
-            Some(num) => radicand.checked_div(num)?,
-            None => 0,
-        };
-        guess = first_term.checked_add(second_term)?.checked_div(root)?;
+    for _ in 0..SQRT_ITERATIONS {
+        // x_k+1 = (x_k + radicand / x_k) / 2
+        guess = last_guess.checked_add(&radicand.checked_div(&last_guess)?)?.checked_div(&two)?;
         if last_guess == guess {
             break;
         } else {
@@ -31,21 +30,15 @@ pub fn newtonian_root(radicand: u64, root: u64, mut guess: u64, iterations: u64)
     Some(guess)
 }
 
-/// Perform square root
-pub fn sqrt(radicand: u64) -> Option<u64> {
-    // A good initial guess is the average of the interval that contains the
-    // input number.  For all numbers, that will be between 1 and the given number.
-    let guess = radicand.checked_div(2)?.checked_add(1)?;
-    newtonian_root(radicand, 2, guess, 50)
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use proptest::prelude::*;
+    use {
+        super::*,
+        proptest::prelude::*,
+    };
 
-    fn check_square_root(radicand: u64) {
-        let root = sqrt(radicand).unwrap() as u128;
+    fn check_square_root(radicand: u128) {
+        let root = sqrt(radicand).unwrap();
         let lower_bound = root.saturating_sub(1).checked_pow(2).unwrap();
         let upper_bound = root.checked_add(1).unwrap().checked_pow(2).unwrap();
         assert!(radicand as u128 <= upper_bound);
@@ -56,14 +49,14 @@ mod tests {
     fn test_square_root_min_max() {
         let test_roots = [0, u64::MAX];
         for i in test_roots.iter() {
-            check_square_root(*i);
+            check_square_root(*i as u128);
         }
     }
 
     proptest! {
         #[test]
         fn test_square_root(a in 0..u64::MAX) {
-            check_square_root(a);
+            check_square_root(a as u128);
         }
     }
 }
--- math/program/tests/functional.rs
+++ math/program/tests/functional.rs
@@ -46,8 +46,8 @@ async fn test_precise_sqrt_u32_max() {
 async fn test_sqrt() {
     let mut pc = ProgramTest::new("spl_math", id(), processor!(process_instruction));
 
-    // Dial down the BPF compute budget to detect if the program gets bloated in the future
-    pc.set_bpf_compute_max_units(10_000);
+    // Dial down the BPF compute budget to detect if the operation gets bloated in the future
+    pc.set_bpf_compute_max_units(2_500);
 
     let (mut banks_client, payer, recent_blockhash) = pc.start().await;
 
