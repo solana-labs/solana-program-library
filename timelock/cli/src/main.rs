7f180fedacd2687a6671eb1c071afd4cb557757a
Dummy Tester 123 <dummytester123@hey.com>
2021-02-13 17:16:44 -0600

    Update script to use spl token id

--- timelock/cli/src/main.rs
+++ timelock/cli/src/main.rs
@@ -11,21 +11,10 @@ use spl_token;
 use std::str::FromStr;
 
 // -------- UPDATE START -------
-const KEYPAIR_PATH: &str = "/Users/jprince/.config/solana/id.json";
-const TIMELOCK_PROGRAM_PUBKEY_PATH: &str = "/Users/jprince/.config/solana/timelock.json";
-const TOKEN_PROGRAM_PUBKEY: &str = "4ZB4R3cV8jwpukrzQgQjPwfFLapZ5iDSAnDbYvtmLZdK";
-const CLUSTER_ADDRESS: &str = "https://devnet.solana.com";
-solana_program::declare_id!("9iAeqqppjn7g1Jn8o2cQCqU5aQVV3h4q9bbWdKRbeC2w");
-
-//const CLUSTER_ADDRESS: &str = "http://localhost:8899";
-//solana_program::declare_id!("HZsimBH3aruxVeCB8YXY9TKRhpFnrny1oLTYXaev8ssY");
-
-/*
 const KEYPAIR_PATH: &str = "/your/path";
 const TIMELOCK_PROGRAM_PUBKEY_PATH: &str = "/your/path";
-const TOKEN_PROGRAM_PUBKEY: &str = "token_pubkey";
 const CLUSTER_ADDRESS: &str = "https://api.mainnet-beta.solana.com";
-solana_program::declare_id!("TimeLock11111111111111111111111111111111111"); */
+solana_program::declare_id!("TimeLock11111111111111111111111111111111111");
 
 // -------- UPDATE END ---------
 
@@ -33,7 +22,6 @@ pub fn main() {
     let client = RpcClient::new(CLUSTER_ADDRESS.to_owned());
 
     let payer = read_keypair_file(KEYPAIR_PATH).unwrap();
-    let token_program = Pubkey::from_str(TOKEN_PROGRAM_PUBKEY).unwrap();
     let timelock_program_key = read_keypair_file(TIMELOCK_PROGRAM_PUBKEY_PATH).unwrap();
     let timelock_pub = timelock_program_key.pubkey();
 
