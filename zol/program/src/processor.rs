7d950dfd476ff2823b8d4e6254efa96d5219b532
Greg Fitzgerald <greg@solana.com>
2021-02-17 10:35:41 -0700

    Migrate from dalek to zkcrypto repos
    Add example from bulletproofs repo

--- zol/program/src/processor.rs
+++ zol/program/src/processor.rs
@@ -325,4 +325,54 @@ mod tests {
         assert_eq!(mint_account_info.lamports(), 42);
         assert_eq!(vault_account_info.lamports(), 0);
     }
+
+    #[test]
+    fn test_bulletproofs() {
+        use curve25519_dalek::scalar::Scalar;
+        use merlin::Transcript;
+        use rand::thread_rng;
+
+        use bulletproofs::{BulletproofGens, PedersenGens, RangeProof};
+
+        // Generators for Pedersen commitments.  These can be selected
+        // independently of the Bulletproofs generators.
+        let pc_gens = PedersenGens::default();
+
+        // Generators for Bulletproofs, valid for proofs up to bitsize 64
+        // and aggregation size up to 1.
+        let bp_gens = BulletproofGens::new(64, 1);
+
+        // A secret value we want to prove lies in the range [0, 2^32)
+        let secret_value = 1037578891u64;
+
+        // The API takes a blinding factor for the commitment.
+        let blinding = Scalar::random(&mut thread_rng());
+
+        // The proof can be chained to an existing transcript.
+        // Here we create a transcript with a doctest domain separator.
+        let mut prover_transcript = Transcript::new(b"doctest example");
+
+        // Create a 32-bit rangeproof.
+        let (proof, committed_value) = RangeProof::prove_single(
+            &bp_gens,
+            &pc_gens,
+            &mut prover_transcript,
+            secret_value,
+            &blinding,
+            32,
+        )
+        .expect("A real program could handle errors");
+
+        // Verification requires a transcript with identical initial state:
+        let mut verifier_transcript = Transcript::new(b"doctest example");
+        assert!(proof
+            .verify_single(
+                &bp_gens,
+                &pc_gens,
+                &mut verifier_transcript,
+                &committed_value,
+                32
+            )
+            .is_ok());
+    }
 }
