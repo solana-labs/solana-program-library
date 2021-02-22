07f7ba3abca4f59866a1fb664ec00384bf6e4765
Vadim <grozinok@gmail.com>
2021-02-22 09:14:51 +0200

    Add pack/unpack for Pool struct

--- binary-oracle-pair/program/src/processor.rs
+++ binary-oracle-pair/program/src/processor.rs
@@ -1,14 +1,10 @@
 //! Program state processor
 
-use crate::{instruction::Instruction, error::PoolError};
-use solana_program::{
-    account_info::AccountInfo,
-    clock::Slot,
-    msg,
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-};
+use crate::{error::PoolError, instruction::Instruction};
 use borsh::BorshDeserialize;
+use solana_program::{
+    account_info::AccountInfo, clock::Slot, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+};
 
 /// Initialize the pool
 pub fn process_init_pool(
@@ -26,11 +22,18 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     input: &[u8],
 ) -> ProgramResult {
-    let instruction = Instruction::try_from_slice(input).or(Err(PoolError::InstructionUnpackError))?;
+    let instruction =
+        Instruction::try_from_slice(input).or(Err(PoolError::InstructionUnpackError))?;
     match instruction {
         Instruction::InitPool(init_args) => {
             msg!("Instruction: InitPool");
-            process_init_pool(program_id, accounts, init_args.mint_end_slot, init_args.decide_end_slot, init_args.bump_seed)
+            process_init_pool(
+                program_id,
+                accounts,
+                init_args.mint_end_slot,
+                init_args.decide_end_slot,
+                init_args.bump_seed,
+            )
         }
         _ => unimplemented!(),
     }
