697136a237302c1fe3b9ba2bb1b7192a56e6d17d
Jon Cinque <jon.cinque@gmail.com>
2021-02-24 14:27:46 +0100

    Update program id

--- record/program/src/lib.rs
+++ record/program/src/lib.rs
@@ -10,4 +10,4 @@ pub mod state;
 // Export current SDK types for downstream users building with a different SDK version
 pub use solana_program;
 
-solana_program::declare_id!("CRUD3SXMKeggKN2FZePp8WLK2RpQvhp1LANGf6a5MLdu");
+solana_program::declare_id!("ReciQBw6sQKH9TVVJQDnbnJ5W7FP539tPHjZhRF4E9r");
