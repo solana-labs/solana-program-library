a54397ce97683f35c87bcd261e8847288727713a
Justin Starry <justin@solana.com>
2021-03-01 11:35:39 +0800

    fix: Set correct reserve token account owner

--- token-lending/program/src/processor.rs
+++ token-lending/program/src/processor.rs
@@ -289,7 +289,7 @@ fn process_init_reserve(
     spl_token_init_account(TokenInitializeAccountParams {
         account: destination_collateral_info.clone(),
         mint: reserve_collateral_mint_info.clone(),
-        owner: lending_market_authority_info.clone(),
+        owner: user_transfer_authority_info.clone(),
         rent: rent_info.clone(),
         token_program: token_program_id.clone(),
     })?;
