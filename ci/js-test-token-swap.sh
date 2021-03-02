c3ecf82cc89b5959fd05c30a0068b8f2a317da21
Jon Cinque <jon.cinque@gmail.com>
2021-03-02 21:28:07 +0100

    token-swap: Use solana-test-validator for JS tests

--- ci/js-test-token-swap.sh
+++ ci/js-test-token-swap.sh
@@ -10,10 +10,6 @@ npm install
 npm run lint
 npm run flow
 npx tsc module.d.ts
-npm run cluster:localnet
-npm run localnet:update
-npm run localnet:up
-npm run start
-(cd ../../target/bpfel-unknown-unknown/release && mv spl_token_swap_production.so spl_token_swap.so)
-SWAP_PROGRAM_OWNER_FEE_ADDRESS="HfoTxFR1Tm6kGmWgYWD6J7YHVy1UwqSULUGVLXkJqaKN" npm run start
-npm run localnet:down
+npm run start-with-test-validator
+(cd ../../target/deploy && mv spl_token_swap_production.so spl_token_swap.so)
+SWAP_PROGRAM_OWNER_FEE_ADDRESS="HfoTxFR1Tm6kGmWgYWD6J7YHVy1UwqSULUGVLXkJqaKN" npm run start-with-test-validator
