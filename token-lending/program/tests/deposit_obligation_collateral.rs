1a0f35d8385549c0eae00e4926608f489247d026
Jordan Sexton <jordan@jordansexton.com>
2021-03-05 17:04:54 -0600

    feat: adjust obligation collateral; use nonzero initial values

--- token-lending/program/tests/deposit_obligation_collateral.rs
+++ token-lending/program/tests/deposit_obligation_collateral.rs
@@ -16,6 +16,7 @@ use spl_token_lending::{
 };
 
 const LAMPORTS_TO_SOL: u64 = 1_000_000_000;
+const FRACTIONAL_TO_USDC: u64 = 1_000_000;
 
 #[tokio::test]
 async fn test_success() {
@@ -28,8 +29,13 @@ async fn test_success() {
     // limit to track compute unit increase
     test.set_bpf_compute_max_units(25_000);
 
+    const INITIAL_SOL_RESERVE_SUPPLY_LAMPORTS: u64 = 100 * LAMPORTS_TO_SOL;
+    const INITIAL_USDC_RESERVE_SUPPLY_FRACTIONAL: u64 = 100 * FRACTIONAL_TO_USDC;
+
     const DEPOSIT_COLLATERAL: u64 = 1 * LAMPORTS_TO_SOL;
-    const DEPOSIT_LIQUIDITY: u64 = DEPOSIT_COLLATERAL / INITIAL_COLLATERAL_RATIO;
+
+    const OBLIGATION_LOAN: u64 = 10 * FRACTIONAL_TO_USDC;
+    const OBLIGATION_COLLATERAL: u64 = 10 * LAMPORTS_TO_SOL * INITIAL_COLLATERAL_RATIO;
 
     let user_accounts_owner = Keypair::new();
     let user_transfer_authority = Keypair::new();
@@ -41,9 +47,10 @@ async fn test_success() {
         &user_accounts_owner,
         &lending_market,
         AddReserveArgs {
-            liquidity_amount: DEPOSIT_LIQUIDITY,
-            liquidity_mint_pubkey: spl_token::native_mint::id(),
+            liquidity_amount: INITIAL_SOL_RESERVE_SUPPLY_LAMPORTS,
             liquidity_mint_decimals: 9,
+            liquidity_mint_pubkey: spl_token::native_mint::id(),
+            collateral_amount: OBLIGATION_COLLATERAL,
             config: TEST_RESERVE_CONFIG,
             ..AddReserveArgs::default()
         },
@@ -54,8 +61,12 @@ async fn test_success() {
         &user_accounts_owner,
         &lending_market,
         AddReserveArgs {
+            initial_borrow_rate: 1,
+            liquidity_amount: INITIAL_USDC_RESERVE_SUPPLY_FRACTIONAL,
             liquidity_mint_pubkey: usdc_mint.pubkey,
             liquidity_mint_decimals: usdc_mint.decimals,
+            borrow_amount: OBLIGATION_LOAN * 101 / 100,
+            user_liquidity_amount: OBLIGATION_LOAN,
             config: TEST_RESERVE_CONFIG,
             ..AddReserveArgs::default()
         },
@@ -68,8 +79,8 @@ async fn test_success() {
         AddObligationArgs {
             borrow_reserve: &usdc_reserve,
             collateral_reserve: &sol_reserve,
-            collateral_amount: 0,
-            borrowed_liquidity_wads: Decimal::zero(),
+            collateral_amount: OBLIGATION_COLLATERAL,
+            borrowed_liquidity_wads: Decimal::from(OBLIGATION_LOAN),
         },
     );
 
