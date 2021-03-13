bfcceba179b7595f54100b5b0bbae3d66792bbe1
Leonard G <gw980531@gmail.com>
2021-03-13 08:57:19 +0000

    Install test validator.

--- ci/js-test-token-lending.sh
+++ ci/js-test-token-lending.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 (cd ../token/js && npm install)
 
 cd ../token-lending/js
+sh -c "$(curl -sSfL https://release.solana.com/v1.5.14/install)"
 npm install
 npm run lint
 npm run build
