42a824b8cad8e1d59a07a9513ebb041f53d47ac9
Leonard G <gw980531@gmail.com>
2021-03-13 09:54:39 +0000

    Changed install to workflow.

--- ci/js-test-token-lending.sh
+++ ci/js-test-token-lending.sh
@@ -6,7 +6,6 @@ cd "$(dirname "$0")"
 (cd ../token/js && npm install)
 
 cd ../token-lending/js
-sh -c "$(curl -sSfL https://release.solana.com/v1.5.14/install)"
 npm install
 npm run lint
 npm run build
