64edd0ba766d0241b67e140bad6204d10600282c
Michael Vines <mvines@gmail.com>
2021-03-16 14:01:25 -0700

    Remove unknown_clippy_lints

--- libraries/math/src/uint.rs
+++ libraries/math/src/uint.rs
@@ -3,7 +3,6 @@
 // required for clippy
 #![allow(clippy::assign_op_pattern)]
 #![allow(clippy::ptr_offset_with_cast)]
-#![allow(clippy::unknown_clippy_lints)]
 #![allow(clippy::manual_range_contains)]
 
 use uint::construct_uint;
