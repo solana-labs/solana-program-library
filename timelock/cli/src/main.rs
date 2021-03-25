d67cd596493b03f3a338927fd133f6e747beeb67
Dummy Tester 123 <dummytester123@hey.com>
2021-03-25 15:24:44 -0500

    Fix timelock main

--- timelock/cli/src/main.rs
+++ timelock/cli/src/main.rs
@@ -9,34 +9,19 @@ use solana_sdk::{
 use spl_timelock::{instruction::init_timelock_program, state::timelock_program::TimelockProgram};
 use spl_token;
 use std::str::FromStr;
-
 // -------- UPDATE START -------
-const KEYPAIR_PATH: &str = "/Users/jprince/.config/solana/id.json";
-const TIMELOCK_PROGRAM_PUBKEY_PATH: &str = "/Users/jprince/.config/solana/timelock.json";
-const TOKEN_PROGRAM_PUBKEY: &str = "AnF2HE7CStzDBtBtW34NxKbnxZrGBAfXKBK5cW88isGR";
-const CLUSTER_ADDRESS: &str = "https://devnet.solana.com";
-solana_program::declare_id!("5rnMWALKssHK5fDxhpxvgpB9ZM5Yqgegsv2hwMebWMKx");
 
-//const CLUSTER_ADDRESS: &str = "http://localhost:8899";
-//solana_program::declare_id!("C3uwGmuTLceZdE9JNsbgePpeE7tJCQdXMeFf4Nz6bAew");
-
-/*
 const KEYPAIR_PATH: &str = "/your/path";
 const TIMELOCK_PROGRAM_PUBKEY_PATH: &str = "/your/path";
 const TOKEN_PROGRAM_PUBKEY: &str = "token_pubkey";
 const CLUSTER_ADDRESS: &str = "https://api.mainnet-beta.solana.com";
-solana_program::declare_id!("TimeLock11111111111111111111111111111111111"); */
-
+solana_program::declare_id!("TimeLock11111111111111111111111111111111111");
 // -------- UPDATE END ---------
-
 pub fn main() {
     let client = RpcClient::new(CLUSTER_ADDRESS.to_owned());
-
     let payer = read_keypair_file(KEYPAIR_PATH).unwrap();
-    let token_program = Pubkey::from_str(TOKEN_PROGRAM_PUBKEY).unwrap();
     let timelock_program_key = read_keypair_file(TIMELOCK_PROGRAM_PUBKEY_PATH).unwrap();
     let timelock_pub = timelock_program_key.pubkey();
-
     let mut transaction = Transaction::new_with_payer(
         &[
             create_account(
@@ -52,11 +37,9 @@ pub fn main() {
         ],
         Some(&payer.pubkey()),
     );
-
     let recent_blockhash = client.get_recent_blockhash().unwrap().0;
     transaction.sign(&[&payer, &timelock_program_key], recent_blockhash);
     client.send_and_confirm_transaction(&transaction).unwrap();
-
     let account = client.get_account(&timelock_pub).unwrap();
     let program = TimelockProgram::unpack(&account.data).unwrap();
     println!("Created timelock program with pubkey: {}", timelock_pub);
