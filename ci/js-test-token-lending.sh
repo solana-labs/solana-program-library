f34a8f4995d2f40323688a75f69305a5ffad30c3
Jon Cinque <jon.cinque@gmail.com>
2021-03-26 20:56:45 +0100

    lending: Update JS tests to solana-test-validator

--- ci/js-test-token-lending.sh
+++ ci/js-test-token-lending.sh
@@ -3,14 +3,8 @@
 set -ex
 cd "$(dirname "$0")"
 
-(cd ../token/js && npm install)
-
 cd ../token-lending/js
 npm install
 npm run lint
 npm run build
-npm run cluster:localnet
-npm run localnet:update
-npm run localnet:up
-npm run start
-npm run localnet:down
+npm run start-with-test-validator
