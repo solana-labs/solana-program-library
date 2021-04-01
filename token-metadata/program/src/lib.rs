b2321e4412550354803f30f0e3d782b0d2ac3781
Jordan Prince <jordanmprince@gmail.com>
2021-04-01 13:56:20 -0500

    Update token-metadata/program/src/lib.rs
    Co-authored-by: Michael Vines <mvines@gmail.com>

--- token-metadata/program/src/lib.rs
+++ token-metadata/program/src/lib.rs
@@ -5,7 +5,7 @@ pub mod error;
 pub mod instruction;
 pub mod processor;
 pub mod state;
-pub mod utils;
+mod utils;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
