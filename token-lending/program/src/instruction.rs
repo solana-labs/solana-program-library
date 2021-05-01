facf7744051e9fb172377873b326e0e5db3a2e28
Leonard G <gw980531@gmail.com>
2021-05-01 10:53:10 +0800

    Update token-lending/program/src/instruction.rs
    Co-authored-by: Jordan Sexton <jordan@jordansexton.com>

--- token-lending/program/src/instruction.rs
+++ token-lending/program/src/instruction.rs
@@ -937,7 +937,7 @@ pub fn flash_loan(
     derived_lending_market_authority_pubkey: Pubkey,
     flash_loan_receiver_pubkey: Pubkey,
     flash_loan_fee_receiver_pubkey: Pubkey,
-    host_fee_recipient_pubkey: Pubkey,
+    host_fee_receiver_pubkey: Pubkey,
     flash_loan_receiver_program_account_pubkeys: Vec<Pubkey>,
 ) -> Instruction {
     let mut accounts = vec![
