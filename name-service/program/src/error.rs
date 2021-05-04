0ed080a50f66d21c7b37a43c5bca49fae813577f
Lcchy <lucas.chaumeny@mailbox.org>
2021-05-04 01:07:11 +0100

    Add name-service program and js bindings (#1600)

---
+++ name-service/program/src/error.rs
@@ -0,0 +1,23 @@
+use num_derive::FromPrimitive;
+use solana_program::{decode_error::DecodeError, program_error::ProgramError};
+use thiserror::Error;
+
+#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+pub enum NameServiceError {
+    #[error("Out of space.")]
+    OutOfSpace,
+}
+
+pub type NameServiceResult = Result<(), NameServiceError>;
+
+impl From<NameServiceError> for ProgramError {
+    fn from(e: NameServiceError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
+
+impl<T> DecodeError<T> for NameServiceError {
+    fn type_of() -> &'static str {
+        "NameServiceError"
+    }
+}
