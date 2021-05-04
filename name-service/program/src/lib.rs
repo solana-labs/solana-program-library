d9fd11a8f37cda0b61c7144ac920c72f17b75739
Solana Maintainers <maintainers@solana.foundation>
2021-05-04 09:30:18 -0700

    Add program id

--- name-service/program/src/lib.rs
+++ name-service/program/src/lib.rs
@@ -1,8 +1,11 @@
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
-
+pub mod error;
 pub mod instruction;
 pub mod processor;
 pub mod state;
 
-pub mod error;
+// Export current sdk types for downstream users building with a different sdk version
+pub use solana_program;
+
+solana_program::declare_id!("namesLPneVptA9Z5rqUDD9tMTWEJwofgaYwp8cawRkX");
