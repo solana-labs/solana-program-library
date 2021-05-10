b3b0c16abd086f27a0d06a5097834372e51e2ec1
Jon Cinque <jon.cinque@gmail.com>
2021-05-10 20:58:49 +0200

    Move things around, remove babel and rollup

--- ci/js-test-token-swap.sh
+++ ci/js-test-token-swap.sh
@@ -9,8 +9,7 @@ source ./ci/solana-version.sh install
 cd token-swap/js
 npm install
 npm run lint
-npm run flow
-npx tsc module.d.ts
+npm run build
 npm run start-with-test-validator
 (cd ../../target/deploy && mv spl_token_swap_production.so spl_token_swap.so)
 SWAP_PROGRAM_OWNER_FEE_ADDRESS="HfoTxFR1Tm6kGmWgYWD6J7YHVy1UwqSULUGVLXkJqaKN" npm run start-with-test-validator
--- token-swap/js/cli/token-swap-test.ts
+++ token-swap/js/cli/token-swap-test.ts
@@ -7,11 +7,11 @@ import {
 } from '@solana/web3.js';
 import {AccountLayout, Token, TOKEN_PROGRAM_ID} from '@solana/spl-token';
 
-import {TokenSwap, CurveType, TOKEN_SWAP_PROGRAM_ID} from '../client';
-import {sendAndConfirmTransaction} from '../client/util/send-and-confirm-transaction';
-import {newAccountWithLamports} from '../client/util/new-account-with-lamports';
-import {url} from '../client/util/url';
-import {sleep} from '../client/util/sleep';
+import {TokenSwap, CurveType, TOKEN_SWAP_PROGRAM_ID} from '../src';
+import {sendAndConfirmTransaction} from '../src/util/send-and-confirm-transaction';
+import {newAccountWithLamports} from '../src/util/new-account-with-lamports';
+import {url} from '../src/util/url';
+import {sleep} from '../src/util/sleep';
 
 // The following globals are created by `createTokenSwap` and used by subsequent tests
 // Token swap
