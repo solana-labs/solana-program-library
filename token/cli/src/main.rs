3085eac0d18de1b873943513e3dab0f5d2118dc5
drbh <david.richard.holtz@gmail.com>
2021-05-11 20:13:28 -0400

    fix with cargo fmt

--- token/cli/src/main.rs
+++ token/cli/src/main.rs
@@ -259,7 +259,7 @@ fn command_create_token(
     // check for memo text; if it exists convert it to
     // bytes and add memo tx instructions
     if let Some(text) = memo {
-        let memo_instruction = spl_memo::build_memo(text.as_bytes(),&[&config.owner] );
+        let memo_instruction = spl_memo::build_memo(text.as_bytes(), &[&config.owner]);
         instructions.push(memo_instruction);
     }
 
