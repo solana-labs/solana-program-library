ca074229cadac77aa98c5172ab3e92c90fb677e2
Jack May <jack@solana.com>
2021-05-25 14:13:48 -0700

    Update sysvar example

--- examples/rust/sysvar/src/processor.rs
+++ examples/rust/sysvar/src/processor.rs
@@ -17,24 +17,28 @@ pub fn process_instruction(
     // Create in iterator to safety reference accounts in the slice
     let account_info_iter = &mut accounts.iter();
 
-    // The first account is the clock sysvar
+    // Get the clock sysvar via syscall
+    let clock_via_sysvar = Clock::get()?;
+    // Or deserialize the account into a clock struct
     let clock_sysvar_info = next_account_info(account_info_iter)?;
-    // The second account is the rent sysvar
-    let rent_sysvar_info = next_account_info(account_info_iter)?;
-
-    // Deserialize the account into a clock struct
-    let clock = Clock::from_account_info(&clock_sysvar_info)?;
-
-    // Deserialize the account into a rent struct
-    let rent = Rent::from_account_info(&rent_sysvar_info)?;
-
+    let clock_via_account = Clock::from_account_info(&clock_sysvar_info)?;
+    // Both produce the same sysvar
+    assert_eq!(clock_via_sysvar, clock_via_account);
     // Note: `format!` can be very expensive, use cautiously
-    msg!("{:?}", clock);
+    msg!("{:?}", clock_via_sysvar);
+
+    // Get the rent sysvar via syscall
+    let rent_via_sysvar = Rent::get()?;
+    // Or deserialize the account into a rent struct
+    let rent_sysvar_info = next_account_info(account_info_iter)?;
+    let rent_via_account = Rent::from_account_info(&rent_sysvar_info)?;
+    // Both produce the same sysvar
+    assert_eq!(rent_via_sysvar, rent_via_account);
     // Can't print `exemption_threshold` because BPF does not support printing floats
     msg!(
         "Rent: lamports_per_byte_year: {:?}, burn_percent: {:?}",
-        rent.lamports_per_byte_year,
-        rent.burn_percent
+        rent_via_sysvar.lamports_per_byte_year,
+        rent_via_sysvar.burn_percent
     );
 
     Ok(())
