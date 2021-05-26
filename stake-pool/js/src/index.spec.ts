cc3071db727a749ecf993d959003970f252cc9fa
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2021-05-26 17:57:49 +0800

    get mocha test harness working with ts-node, ts-mocha, esm

---
+++ stake-pool/js/src/index.spec.ts
@@ -0,0 +1,10 @@
+import * as index from './index.ts'
+import assert from 'assert'
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', () => {
+            assert.equal([1, 2, 3].indexOf(4), -1)
+        })
+    })
+})
