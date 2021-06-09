4a035a0a23f5fe826b1ff380b4b1189b783a1dd1
jordansexton <jordan@jordansexton.com>
2021-06-08 21:22:57 -0500

    add test for repay with u64::MAX

--- token-lending/program/tests/repay_obligation_liquidity.rs
+++ token-lending/program/tests/repay_obligation_liquidity.rs
@@ -17,7 +17,7 @@ use spl_token_lending::{
 };
 
 #[tokio::test]
-async fn test_success() {
+async fn test_repay_usdc_fixed_amount() {
     let mut test = ProgramTest::new(
         "spl_token_lending",
         spl_token_lending::id(),
@@ -140,6 +140,136 @@ async fn test_success() {
     assert_eq!(
         liquidity_supply_balance,
         initial_liquidity_supply_balance + USDC_BORROW_AMOUNT_FRACTIONAL
+    );
+
+    let obligation = test_obligation.get_state(&mut banks_client).await;
+    assert_eq!(obligation.borrows.len(), 0);
+}
+
+#[tokio::test]
+async fn test_repay_sol_max_amount() {
+    let mut test = ProgramTest::new(
+        "spl_token_lending",
+        spl_token_lending::id(),
+        processor!(process_instruction),
+    );
+
+    // limit to track compute unit increase
+    test.set_bpf_compute_max_units(27_000);
+
+    const USDC_DEPOSIT_AMOUNT_FRACTIONAL: u64 = 1_000 * FRACTIONAL_TO_USDC;
+    const SOL_BORROW_AMOUNT_LAMPORTS: u64 = 100 * LAMPORTS_TO_SOL * INITIAL_COLLATERAL_RATIO;
+    const USDC_RESERVE_COLLATERAL_FRACTIONAL: u64 = 2 * USDC_DEPOSIT_AMOUNT_FRACTIONAL;
+    const SOL_RESERVE_LIQUIDITY_LAMPORTS: u64 = 2 * SOL_BORROW_AMOUNT_LAMPORTS;
+
+    let user_accounts_owner = Keypair::new();
+    let user_transfer_authority = Keypair::new();
+    let lending_market = add_lending_market(&mut test);
+
+    let mut reserve_config = TEST_RESERVE_CONFIG;
+    reserve_config.loan_to_value_ratio = 50;
+
+    let usdc_mint = add_usdc_mint(&mut test);
+    let usdc_oracle = add_usdc_oracle(&mut test);
+    let usdc_test_reserve = add_reserve(
+        &mut test,
+        &lending_market,
+        &usdc_oracle,
+        &user_accounts_owner,
+        AddReserveArgs {
+            collateral_amount: USDC_RESERVE_COLLATERAL_FRACTIONAL,
+            liquidity_mint_pubkey: usdc_mint.pubkey,
+            liquidity_mint_decimals: usdc_mint.decimals,
+            config: reserve_config,
+            mark_fresh: true,
+            ..AddReserveArgs::default()
+        },
+    );
+
+    let sol_oracle = add_sol_oracle(&mut test);
+    let sol_test_reserve = add_reserve(
+        &mut test,
+        &lending_market,
+        &sol_oracle,
+        &user_accounts_owner,
+        AddReserveArgs {
+            borrow_amount: SOL_BORROW_AMOUNT_LAMPORTS,
+            user_liquidity_amount: SOL_BORROW_AMOUNT_LAMPORTS,
+            liquidity_amount: SOL_RESERVE_LIQUIDITY_LAMPORTS,
+            liquidity_mint_pubkey: spl_token::native_mint::id(),
+            liquidity_mint_decimals: 9,
+            config: reserve_config,
+            mark_fresh: true,
+            ..AddReserveArgs::default()
+        },
+    );
+
+    let test_obligation = add_obligation(
+        &mut test,
+        &lending_market,
+        &user_accounts_owner,
+        AddObligationArgs {
+            deposits: &[(&usdc_test_reserve, USDC_DEPOSIT_AMOUNT_FRACTIONAL)],
+            borrows: &[(&sol_test_reserve, SOL_BORROW_AMOUNT_LAMPORTS)],
+            ..AddObligationArgs::default()
+        },
+    );
+
+    let (mut banks_client, payer, recent_blockhash) = test.start().await;
+
+    let initial_user_liquidity_balance =
+        get_token_balance(&mut banks_client, sol_test_reserve.user_liquidity_pubkey).await;
+    let initial_liquidity_supply_balance =
+        get_token_balance(&mut banks_client, sol_test_reserve.liquidity_supply_pubkey).await;
+
+    let mut transaction = Transaction::new_with_payer(
+        &[
+            approve(
+                &spl_token::id(),
+                &sol_test_reserve.user_liquidity_pubkey,
+                &user_transfer_authority.pubkey(),
+                &user_accounts_owner.pubkey(),
+                &[],
+                SOL_BORROW_AMOUNT_LAMPORTS,
+            )
+            .unwrap(),
+            refresh_obligation(
+                spl_token_lending::id(),
+                test_obligation.pubkey,
+                vec![usdc_test_reserve.pubkey, sol_test_reserve.pubkey],
+            ),
+            repay_obligation_liquidity(
+                spl_token_lending::id(),
+                u64::MAX,
+                sol_test_reserve.user_liquidity_pubkey,
+                sol_test_reserve.liquidity_supply_pubkey,
+                sol_test_reserve.pubkey,
+                test_obligation.pubkey,
+                lending_market.pubkey,
+                user_transfer_authority.pubkey(),
+            ),
+        ],
+        Some(&payer.pubkey()),
+    );
+
+    transaction.sign(
+        &[&payer, &user_accounts_owner, &user_transfer_authority],
+        recent_blockhash,
+    );
+    assert!(banks_client.process_transaction(transaction).await.is_ok());
+
+    let user_liquidity_balance =
+        get_token_balance(&mut banks_client, sol_test_reserve.user_liquidity_pubkey).await;
+    assert_eq!(
+        user_liquidity_balance,
+        initial_user_liquidity_balance - SOL_BORROW_AMOUNT_LAMPORTS
+    );
+
+    let liquidity_supply_balance =
+        get_token_balance(&mut banks_client, sol_test_reserve.liquidity_supply_pubkey).await;
+    assert_eq!(
+        liquidity_supply_balance,
+        initial_liquidity_supply_balance + SOL_BORROW_AMOUNT_LAMPORTS
     );
 
     let obligation = test_obligation.get_state(&mut banks_client).await;
