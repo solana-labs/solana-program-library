871935cafc590bf1985bebfbc7e6399b153a5b40
NOPE finance <nope@solend.fi>
2021-08-14 00:43:07 -0700

    prod address and staleness tolerance adjustment

--- token-lending/program/src/lib.rs
+++ token-lending/program/src/lib.rs
@@ -13,4 +13,4 @@ pub mod state;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("ALend7Ketfx5bxh6ghsCDXAoDrhvEmsXT3cynB6aPLgx");
+solana_program::declare_id!("So1endDq2YkqhipRh3WViPa8hdiSpxWy6z3Z6tMCpAo");
