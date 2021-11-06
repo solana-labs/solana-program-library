75ddd9bb229396427977fb679a0763630e83bce6
Jon Cinque <jon.cinque@gmail.com>
2021-11-06 13:48:11 +0100

    stake-pool-docs: Rearrange docs, add quickstart guide (#2561)
    * stake-pool-docs: Rearrange docs, add quickstart guide
    
    * Rearrange doc sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,17 +1,24 @@
 module.exports = {
-  docs: {
-    "Other Thing": [],
-    "Solana Program Library (SPL)": [
-      "introduction",
-      "token",
-      "token-swap",
-      "token-lending",
-      "associated-token-account",
-      "memo",
-      "name-service",
-      "shared-memory",
-      "stake-pool",
-      "feature-proposal",
-    ],
-  },
+  docs: [
+    "introduction",
+    "token",
+    "token-swap",
+    "token-lending",
+    "associated-token-account",
+    "memo",
+    "name-service",
+    "shared-memory",
+    {
+      type: 'category',
+      label: 'Stake Pool',
+      collapsed: true,
+      items: [
+        "stake-pool",
+        "stake-pool/quickstart",
+        "stake-pool/overview",
+        "stake-pool/cli",
+      ],
+    },
+    "feature-proposal",
+  ],
 };
--- stake-pool/cli/scripts/setup-stake-pool.sh
+++ stake-pool/cli/scripts/setup-stake-pool.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-# Script to setup a stake pool, add new validators from a list
+# Script to setup a stake pool from scratch.  Please modify the parameters to
+# create a stake pool to your liking!
 
-cd "$(dirname "$0")"
-global_args=()
+cd "$(dirname "$0")" || exit
 command_args=()
 
 ###################################################
 ### MODIFY PARAMETERS BELOW THIS LINE FOR YOUR POOL
 ###################################################
-
-global_args+=( --manager keys/new_manager.json ) # Keypair of the manager of the stake pool
-global_args+=( --staker keys/new_staker.json ) # Keypair of the staker of the stake pool
 
 # Epoch fee, assessed as a percentage of rewards earned by the pool every epoch,
 # represented as `numerator / denominator`
@@ -30,44 +27,28 @@ command_args+=( --referral-fee 0 ) # Percentage of deposit fee that goes towards
 
 command_args+=( --max-validators 3950 ) # Maximum number of validators in the stake pool, 3950 is the current maximum possible
 
-validator_list=validator_list.txt # File containing validator vote account addresses, each will be added to the stake pool after creation
-
 # (Optional) Deposit authority, required to sign all deposits into the pool.
 # Setting this variable makes the pool "private" or "restricted".
-# Comment it out if you want the pool to be open to all depositors.
-command_args+=( --deposit-authority keys/authority.json )
+# Uncomment and set to a valid keypair if you want the pool to be restricted.
+#command_args+=( --deposit-authority keys/authority.json )
 
 ###################################################
 ### MODIFY PARAMETERS ABOVE THIS LINE FOR YOUR POOL
 ###################################################
 
 keys_dir=keys
-spl_stake_pool=../../../target/debug/spl-stake-pool
+spl_stake_pool=spl-stake-pool
+# Uncomment to use a local build
+#spl_stake_pool=../../../target/debug/spl-stake-pool
 
 mkdir -p $keys_dir
 
-build_stake_pool_cli () {
-  cargo build --manifest-path ../Cargo.toml
-}
-
 create_keypair () {
-  if test ! -f $1
+  if test ! -f "$1"
   then
-    solana-keygen new --no-passphrase -s -o $1
+    solana-keygen new --no-passphrase -s -o "$1"
   fi
 }
-
-add_validator_stakes () {
-  pool=$1
-  validator_list=$2
-  for validator in $(cat $validator_list)
-  do
-    $spl_stake_pool "${global_args[@]}" add-validator $pool $validator
-  done
-}
-
-echo "Building stake pool CLI"
-build_stake_pool_cli
 
 echo "Creating pool"
 stake_pool_keyfile=$keys_dir/stake-pool.json
@@ -81,15 +62,9 @@ create_keypair $reserve_keyfile
 
 set -ex
 $spl_stake_pool \
-  "${global_args[@]}" \
   create-pool \
   "${command_args[@]}" \
   --pool-keypair "$stake_pool_keyfile" \
   --validator-list-keypair "$validator_list_keyfile" \
   --mint-keypair "$mint_keyfile" \
   --reserve-keypair "$reserve_keyfile"
-
-stake_pool_pubkey=$(solana-keygen pubkey $stake_pool_keyfile)
-
-echo "Adding validator stake accounts to the pool"
-add_validator_stakes $stake_pool_pubkey $validator_list
