232ccf1ef581aca4857f0e9d36276934b362f624
Tyera Eulberg <tyera@solana.com>
2021-11-15 19:40:17 -0700

    Memo ts implementation
    Co-authored by: Bryan Ischo <bryan@ischo.com>

---
+++ memo/ts/test/url.ts
@@ -0,0 +1,10 @@
+/**
+ * The connection url to use when running unit tests against a live cluster
+ */
+
+export const MOCK_PORT = 9999;
+export const url = process.env.TEST_LIVE
+  ? 'http://localhost:8899/'
+  : 'http://localhost:9999/';
+
+// export const url = 'https://api.devnet.solana.com/';
