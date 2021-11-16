46a47735fd5e0ca2cfae4ab1e67fa20c89fd8343
Jon Cinque <jon.cinque@gmail.com>
2021-11-16 22:45:35 +0100

    Refactor for clarity

--- token/program-2022/tests/assert_instruction_count.rs
+++ token/program-2022/tests/assert_instruction_count.rs
@@ -1,5 +1,6 @@
 #![cfg(feature = "test-bpf")]
 
+mod action;
 use {
     solana_program_test::{processor, tokio, ProgramTest},
     solana_sdk::{
@@ -20,7 +21,7 @@ const TRANSFER_AMOUNT: u64 = 1_000_000_000_000_000;
 
 #[tokio::test]
 async fn initialize_mint() {
-    let mut pt = ProgramTest::new("spl_token", id(), processor!(Processor::process));
+    let mut pt = ProgramTest::new("spl_token_2022", id(), processor!(Processor::process));
     pt.set_bpf_compute_max_units(2_500); // last known 2252
     let (mut banks_client, payer, recent_blockhash) = pt.start().await;
 
@@ -67,39 +68,28 @@ async fn initialize_account() {
     let account = Keypair::new();
     let decimals = 9;
 
+    action::create_mint(
+        &mut banks_client,
+        &payer,
+        recent_blockhash,
+        &mint,
+        &owner.pubkey(),
+        decimals,
+    )
+    .await
+    .unwrap();
     let rent = banks_client.get_rent().await.unwrap();
-    let mint_rent = rent.minimum_balance(Mint::LEN);
     let account_rent = rent.minimum_balance(Account::LEN);
     let transaction = Transaction::new_signed_with_payer(
-        &[
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &mint.pubkey(),
-                mint_rent,
-                Mint::LEN as u64,
-                &id(),
-            ),
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &account.pubkey(),
-                account_rent,
-                Account::LEN as u64,
-                &id(),
-            ),
-        ],
+        &[system_instruction::create_account(
+            &payer.pubkey(),
+            &account.pubkey(),
+            account_rent,
+            Account::LEN as u64,
+            &id(),
+        )],
         Some(&payer.pubkey()),
-        &[&payer, &mint, &account],
-        recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    let transaction = Transaction::new_signed_with_payer(
-        &[
-            instruction::initialize_mint(&id(), &mint.pubkey(), &owner.pubkey(), None, decimals)
-                .unwrap(),
-        ],
-        Some(&payer.pubkey()),
-        &[&payer],
+        &[&payer, &account],
         recent_blockhash,
     );
     banks_client.process_transaction(transaction).await.unwrap();
@@ -130,56 +120,27 @@ async fn mint_to() {
     let account = Keypair::new();
     let decimals = 9;
 
-    let rent = banks_client.get_rent().await.unwrap();
-    let mint_rent = rent.minimum_balance(Mint::LEN);
-    let account_rent = rent.minimum_balance(Account::LEN);
-    let transaction = Transaction::new_signed_with_payer(
-        &[
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &mint.pubkey(),
-                mint_rent,
-                Mint::LEN as u64,
-                &id(),
-            ),
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &account.pubkey(),
-                account_rent,
-                Account::LEN as u64,
-                &id(),
-            ),
-        ],
-        Some(&payer.pubkey()),
-        &[&payer, &mint, &account],
+    action::create_mint(
+        &mut banks_client,
+        &payer,
         recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
+        &mint,
+        &owner.pubkey(),
+        decimals,
+    )
+    .await
+    .unwrap();
+    action::create_account(
+        &mut banks_client,
+        &payer,
+        recent_blockhash,
+        &account,
+        &mint.pubkey(),
+        &owner.pubkey(),
+    )
+    .await
+    .unwrap();
 
-    let transaction = Transaction::new_signed_with_payer(
-        &[
-            instruction::initialize_mint(&id(), &mint.pubkey(), &owner.pubkey(), None, decimals)
-                .unwrap(),
-        ],
-        Some(&payer.pubkey()),
-        &[&payer],
-        recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction::initialize_account(
-            &id(),
-            &account.pubkey(),
-            &mint.pubkey(),
-            &owner.pubkey(),
-        )
-        .unwrap()],
-        Some(&payer.pubkey()),
-        &[&payer],
-        recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
     let transaction = Transaction::new_signed_with_payer(
         &[instruction::mint_to(
             &id(),
@@ -209,109 +170,60 @@ async fn transfer() {
     let destination = Keypair::new();
     let decimals = 9;
 
-    let rent = banks_client.get_rent().await.unwrap();
-    let mint_rent = rent.minimum_balance(Mint::LEN);
-    let account_rent = rent.minimum_balance(Account::LEN);
-    let transaction = Transaction::new_signed_with_payer(
-        &[
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &mint.pubkey(),
-                mint_rent,
-                Mint::LEN as u64,
-                &id(),
-            ),
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &source.pubkey(),
-                account_rent,
-                Account::LEN as u64,
-                &id(),
-            ),
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &destination.pubkey(),
-                account_rent,
-                Account::LEN as u64,
-                &id(),
-            ),
-        ],
-        Some(&payer.pubkey()),
-        &[&payer, &mint, &source, &destination],
+    action::create_mint(
+        &mut banks_client,
+        &payer,
         recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
+        &mint,
+        &owner.pubkey(),
+        decimals,
+    )
+    .await
+    .unwrap();
+    action::create_account(
+        &mut banks_client,
+        &payer,
+        recent_blockhash,
+        &source,
+        &mint.pubkey(),
+        &owner.pubkey(),
+    )
+    .await
+    .unwrap();
+    action::create_account(
+        &mut banks_client,
+        &payer,
+        recent_blockhash,
+        &destination,
+        &mint.pubkey(),
+        &owner.pubkey(),
+    )
+    .await
+    .unwrap();
 
-    let transaction = Transaction::new_signed_with_payer(
-        &[
-            instruction::initialize_mint(&id(), &mint.pubkey(), &owner.pubkey(), None, decimals)
-                .unwrap(),
-        ],
-        Some(&payer.pubkey()),
-        &[&payer],
+    action::mint_to(
+        &mut banks_client,
+        &payer,
         recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
+        &mint.pubkey(),
+        &source.pubkey(),
+        &owner,
+        TRANSFER_AMOUNT,
+    )
+    .await
+    .unwrap();
 
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction::initialize_account(
-            &id(),
-            &source.pubkey(),
-            &mint.pubkey(),
-            &owner.pubkey(),
-        )
-        .unwrap()],
-        Some(&payer.pubkey()),
-        &[&payer],
+    action::transfer(
+        &mut banks_client,
+        &payer,
         recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction::initialize_account(
-            &id(),
-            &destination.pubkey(),
-            &mint.pubkey(),
-            &owner.pubkey(),
-        )
-        .unwrap()],
-        Some(&payer.pubkey()),
-        &[&payer],
-        recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction::mint_to(
-            &id(),
-            &mint.pubkey(),
-            &source.pubkey(),
-            &owner.pubkey(),
-            &[],
-            TRANSFER_AMOUNT,
-        )
-        .unwrap()],
-        Some(&payer.pubkey()),
-        &[&payer, &owner],
-        recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction::transfer(
-            &id(),
-            &source.pubkey(),
-            &destination.pubkey(),
-            &owner.pubkey(),
-            &[],
-            TRANSFER_AMOUNT,
-        )
-        .unwrap()],
-        Some(&payer.pubkey()),
-        &[&payer, &owner],
-        recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
+        &source.pubkey(),
+        &destination.pubkey(),
+        &owner,
+        TRANSFER_AMOUNT,
+    )
+    .await
+    .unwrap();
 }
 
 #[tokio::test]
@@ -325,85 +237,48 @@ async fn burn() {
     let account = Keypair::new();
     let decimals = 9;
 
-    let rent = banks_client.get_rent().await.unwrap();
-    let mint_rent = rent.minimum_balance(Mint::LEN);
-    let account_rent = rent.minimum_balance(Account::LEN);
-    let transaction = Transaction::new_signed_with_payer(
-        &[
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &mint.pubkey(),
-                mint_rent,
-                Mint::LEN as u64,
-                &id(),
-            ),
-            system_instruction::create_account(
-                &payer.pubkey(),
-                &account.pubkey(),
-                account_rent,
-                Account::LEN as u64,
-                &id(),
-            ),
-        ],
-        Some(&payer.pubkey()),
-        &[&payer, &mint, &account],
+    action::create_mint(
+        &mut banks_client,
+        &payer,
         recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
+        &mint,
+        &owner.pubkey(),
+        decimals,
+    )
+    .await
+    .unwrap();
+    action::create_account(
+        &mut banks_client,
+        &payer,
+        recent_blockhash,
+        &account,
+        &mint.pubkey(),
+        &owner.pubkey(),
+    )
+    .await
+    .unwrap();
 
-    let transaction = Transaction::new_signed_with_payer(
-        &[
-            instruction::initialize_mint(&id(), &mint.pubkey(), &owner.pubkey(), None, decimals)
-                .unwrap(),
-        ],
-        Some(&payer.pubkey()),
-        &[&payer],
+    action::mint_to(
+        &mut banks_client,
+        &payer,
         recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
+        &mint.pubkey(),
+        &account.pubkey(),
+        &owner,
+        TRANSFER_AMOUNT,
+    )
+    .await
+    .unwrap();
 
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction::initialize_account(
-            &id(),
-            &account.pubkey(),
-            &mint.pubkey(),
-            &owner.pubkey(),
-        )
-        .unwrap()],
-        Some(&payer.pubkey()),
-        &[&payer],
+    action::burn(
+        &mut banks_client,
+        &payer,
         recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction::mint_to(
-            &id(),
-            &mint.pubkey(),
-            &account.pubkey(),
-            &owner.pubkey(),
-            &[],
-            TRANSFER_AMOUNT,
-        )
-        .unwrap()],
-        Some(&payer.pubkey()),
-        &[&payer, &owner],
-        recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction::burn(
-            &id(),
-            &account.pubkey(),
-            &mint.pubkey(),
-            &owner.pubkey(),
-            &[],
-            TRANSFER_AMOUNT,
-        )
-        .unwrap()],
-        Some(&payer.pubkey()),
-        &[&payer, &owner],
-        recent_blockhash,
-    );
-    banks_client.process_transaction(transaction).await.unwrap();
+        &mint.pubkey(),
+        &account.pubkey(),
+        &owner,
+        TRANSFER_AMOUNT,
+    )
+    .await
+    .unwrap();
 }
