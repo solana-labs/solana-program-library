1e3a6ed219dd403daf6bf635b6fddfa7814c62c5
Jon Cinque <jon.cinque@gmail.com>
2021-12-11 02:06:16 +0100

    stake-pool-py: Add simple rebalance bot

--- stake-pool/py/tests/test_deposit_withdraw_stake.py
+++ stake-pool/py/tests/test_deposit_withdraw_stake.py
@@ -1,16 +1,29 @@
+import asyncio
 import pytest
+from solana.rpc.async_api import AsyncClient
 from solana.rpc.commitment import Confirmed
 from solana.keypair import Keypair
+from solana.publickey import PublicKey
 from spl.token.instructions import get_associated_token_address
 
 from stake.actions import create_stake, delegate_stake
 from stake.constants import STAKE_LEN
-from stake_pool.actions import deposit_stake, withdraw_stake
+from stake_pool.actions import deposit_stake, withdraw_stake, update_stake_pool
 from stake_pool.state import StakePool
 
 
+async def simple_deposit(
+    async_client: AsyncClient, payer: Keypair, stake_pool_address: PublicKey,
+    validator: PublicKey, token_account: PublicKey, stake_amount: int
+):
+    stake = Keypair()
+    await create_stake(async_client, payer, stake, payer.public_key, stake_amount)
+    await delegate_stake(async_client, payer, payer, stake.public_key, validator)
+    await deposit_stake(async_client, payer, stake_pool_address, validator, stake.public_key, token_account)
+
+
 @pytest.mark.asyncio
-async def test_deposit_withdraw_stake(async_client, validators, payer, stake_pool_addresses):
+async def test_deposit_withdraw_stake(async_client, validators, payer, stake_pool_addresses, waiter):
     (stake_pool_address, validator_list_address) = stake_pool_addresses
     resp = await async_client.get_account_info(stake_pool_address, commitment=Confirmed)
     data = resp['result']['value']['data']
@@ -19,13 +32,15 @@ async def test_deposit_withdraw_stake(async_client, validators, payer, stake_poo
 
     resp = await async_client.get_minimum_balance_for_rent_exemption(STAKE_LEN)
     stake_rent_exemption = resp['result']
+    await waiter.wait_for_next_epoch_if_soon(async_client)
+    await update_stake_pool(async_client, payer, stake_pool_address)
 
     stake_amount = 1_000_000
-    for validator in validators:
-        stake = Keypair()
-        await create_stake(async_client, payer, stake, payer.public_key, stake_amount)
-        await delegate_stake(async_client, payer, payer, stake.public_key, validator)
-        await deposit_stake(async_client, payer, stake_pool_address, validator, stake.public_key, token_account)
+    futures = [
+        simple_deposit(async_client, payer, stake_pool_address, validator, token_account, stake_amount)
+        for validator in validators
+    ]
+    await asyncio.gather(*futures)
 
     pool_token_balance = await async_client.get_token_account_balance(token_account, Confirmed)
     pool_token_balance = pool_token_balance['result']['value']['amount']
