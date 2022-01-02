a4115071d30da14623629c7a6e2f096aad119996
Sebastian.Bor <sebastian_bor@hotmail.com>
2022-01-02 16:13:08 +0000

    chore: fix fmt

--- governance/program/src/state/mod.rs
+++ governance/program/src/state/mod.rs
@@ -3,6 +3,7 @@
 pub mod enums;
 pub mod governance;
 pub mod legacy;
+pub mod native_treasury;
 pub mod program_metadata;
 pub mod proposal;
 pub mod proposal_instruction;
@@ -10,5 +11,4 @@ pub mod realm;
 pub mod realm_config;
 pub mod signatory_record;
 pub mod token_owner_record;
-pub mod native_treasury;
 pub mod vote_record;
