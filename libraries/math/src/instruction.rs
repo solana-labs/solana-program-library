05e3ff3d7daf0fb196aa2231bf7c7826ce042449
Michał Piotrowski <michal@eventhorizonlabs.eu>
2022-01-05 23:23:09 +0100

    libraries tests p2 (#2699)
    * libraries tests p2
    
    * libraries tests p2 - address review comments

--- libraries/math/src/instruction.rs
+++ libraries/math/src/instruction.rs
@@ -167,3 +167,124 @@ pub fn noop() -> Instruction {
         data: MathInstruction::Noop.try_to_vec().unwrap(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_precise_sqrt() {
+        let instruction = precise_sqrt(u64::MAX);
+        assert_eq!(0, instruction.accounts.len());
+        assert_eq!(
+            instruction.data,
+            MathInstruction::PreciseSquareRoot { radicand: u64::MAX }
+                .try_to_vec()
+                .unwrap()
+        );
+        assert_eq!(instruction.program_id, crate::id());
+    }
+
+    #[test]
+    fn test_sqrt_u64() {
+        let instruction = sqrt_u64(u64::MAX);
+        assert_eq!(0, instruction.accounts.len());
+        assert_eq!(
+            instruction.data,
+            MathInstruction::SquareRootU64 { radicand: u64::MAX }
+                .try_to_vec()
+                .unwrap()
+        );
+        assert_eq!(instruction.program_id, crate::id());
+    }
+
+    #[test]
+    fn test_sqrt_u128() {
+        let instruction = sqrt_u128(u128::MAX);
+        assert_eq!(0, instruction.accounts.len());
+        assert_eq!(
+            instruction.data,
+            MathInstruction::SquareRootU128 {
+                radicand: u128::MAX
+            }
+            .try_to_vec()
+            .unwrap()
+        );
+        assert_eq!(instruction.program_id, crate::id());
+    }
+
+    #[test]
+    fn test_u64_multiply() {
+        let instruction = u64_multiply(u64::MAX, u64::MAX);
+        assert_eq!(0, instruction.accounts.len());
+        assert_eq!(
+            instruction.data,
+            MathInstruction::U64Multiply {
+                multiplicand: u64::MAX,
+                multiplier: u64::MAX
+            }
+            .try_to_vec()
+            .unwrap()
+        );
+        assert_eq!(instruction.program_id, crate::id());
+    }
+
+    #[test]
+    fn test_u64_divide() {
+        let instruction = u64_divide(u64::MAX, u64::MAX);
+        assert_eq!(0, instruction.accounts.len());
+        assert_eq!(
+            instruction.data,
+            MathInstruction::U64Divide {
+                dividend: u64::MAX,
+                divisor: u64::MAX
+            }
+            .try_to_vec()
+            .unwrap()
+        );
+        assert_eq!(instruction.program_id, crate::id());
+    }
+
+    #[test]
+    fn test_f32_multiply() {
+        let instruction = f32_multiply(f32::MAX, f32::MAX);
+        assert_eq!(0, instruction.accounts.len());
+        assert_eq!(
+            instruction.data,
+            MathInstruction::F32Multiply {
+                multiplicand: f32::MAX,
+                multiplier: f32::MAX
+            }
+            .try_to_vec()
+            .unwrap()
+        );
+        assert_eq!(instruction.program_id, crate::id());
+    }
+
+    #[test]
+    fn test_f32_divide() {
+        let instruction = f32_divide(f32::MAX, f32::MAX);
+        assert_eq!(0, instruction.accounts.len());
+        assert_eq!(
+            instruction.data,
+            MathInstruction::F32Divide {
+                dividend: f32::MAX,
+                divisor: f32::MAX
+            }
+            .try_to_vec()
+            .unwrap()
+        );
+        assert_eq!(instruction.program_id, crate::id());
+    }
+
+    #[test]
+    fn test_noop() {
+        let instruction = noop();
+        assert_eq!(0, instruction.accounts.len());
+        assert_eq!(
+            instruction.data,
+            MathInstruction::Noop.try_to_vec().unwrap()
+        );
+        assert_eq!(instruction.program_id, crate::id());
+    }
+}
