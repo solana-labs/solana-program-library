9c32580354397a472076146611ae9f857b6edcdb
Michael Vines <mvines@gmail.com>
2022-01-07 14:28:27 -0800

    How about ConfidentialTransferState and ConfidentialTransferAuditor?

--- token/program-2022/src/extension/confidential_transfer/mod.rs
+++ token/program-2022/src/extension/confidential_transfer/mod.rs
@@ -12,23 +12,23 @@ pub mod processor;
 /// Transfer auditor configuration
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Default, PartialEq, Pod, Zeroable)]
-pub struct MintConfidentialTransferAuditor {
+pub struct ConfidentialTransferAuditor {
     // TODO: inline `zk_token_program::state::Auditor` here
 }
 
-impl Extension for MintConfidentialTransferAuditor {
-    const TYPE: ExtensionType = ExtensionType::MintConfidentialTransferAuditor;
+impl Extension for ConfidentialTransferAuditor {
+    const TYPE: ExtensionType = ExtensionType::ConfidentialTransferAuditor;
     const ACCOUNT_TYPE: AccountType = AccountType::Mint;
 }
 
 /// Confidential account state
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Default, PartialEq, Pod, Zeroable)]
-pub struct AccountConfidentialState {
+pub struct ConfidentialTransferState {
     // TODO: inline `zk_token_program::state::ZkAccount` here
 }
 
-impl Extension for AccountConfidentialState {
-    const TYPE: ExtensionType = ExtensionType::AccountConfidentialState;
+impl Extension for ConfidentialTransferState {
+    const TYPE: ExtensionType = ExtensionType::ConfidentialTransferState;
     const ACCOUNT_TYPE: AccountType = AccountType::Account;
 }
