108c8fbd5397cfdc14c4520184a302caea3ff450
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-01-26 20:42:19 +0000

    chore: add SetupMaxVoterWeightRecord to addin mock

--- governance/addin-mock/program/src/processor.rs
+++ governance/addin-mock/program/src/processor.rs
@@ -2,7 +2,10 @@
 
 use borsh::BorshDeserialize;
 use spl_governance::{
-    addins::voter_weight::{VoterWeightAccountType, VoterWeightAction, VoterWeightRecord},
+    addins::{
+        max_voter_weight::{MaxVoterWeightAccountType, MaxVoterWeightRecord},
+        voter_weight::{VoterWeightAccountType, VoterWeightAction, VoterWeightRecord},
+    },
     state::token_owner_record::get_token_owner_record_data_for_realm_and_governing_mint,
 };
 
@@ -42,6 +45,15 @@ pub fn process_instruction(
             voter_weight_expiry,
             weight_action,
             weight_action_target,
+        ),
+        VoterWeightAddinInstruction::SetupMaxVoterWeightRecord {
+            max_voter_weight,
+            max_voter_weight_expiry,
+        } => process_setup_max_voter_weight_record(
+            program_id,
+            accounts,
+            max_voter_weight,
+            max_voter_weight_expiry,
         ),
     }
 }
@@ -87,6 +99,40 @@ pub fn process_setup_voter_weight_record(
         payer_info,
         voter_weight_record_info,
         &voter_weight_record_data,
+        program_id,
+        system_info,
+    )?;
+
+    Ok(())
+}
+
+/// Processes SetupMaxVoterWeightRecord instruction
+pub fn process_setup_max_voter_weight_record(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    max_voter_weight: u64,
+    max_voter_weight_expiry: Option<Slot>,
+) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+
+    let realm_info = next_account_info(account_info_iter)?; // 0
+    let governing_token_mint_info = next_account_info(account_info_iter)?; // 1
+    let max_voter_weight_record_info = next_account_info(account_info_iter)?; // 2
+    let payer_info = next_account_info(account_info_iter)?; // 3
+    let system_info = next_account_info(account_info_iter)?; // 4
+
+    let max_voter_weight_record_data = MaxVoterWeightRecord {
+        account_type: MaxVoterWeightAccountType::MaxVoterWeightRecord,
+        realm: *realm_info.key,
+        governing_token_mint: *governing_token_mint_info.key,
+        max_voter_weight,
+        max_voter_weight_expiry,
+    };
+
+    create_and_serialize_account(
+        payer_info,
+        max_voter_weight_record_info,
+        &max_voter_weight_record_data,
         program_id,
         system_info,
     )?;
