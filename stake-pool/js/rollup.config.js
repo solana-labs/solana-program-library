bfb1d19696a7fb872eb8255203597496743029be
CMEONE <contact@kabirwearsato.ga>
2022-01-26 15:58:11 -0800

    Remove unused external import from rollup configuration

--- stake-pool/js/rollup.config.js
+++ stake-pool/js/rollup.config.js
@@ -2,7 +2,6 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
 import {terser} from 'rollup-plugin-terser';
 
 const extensions = ['.js', '.ts'];
