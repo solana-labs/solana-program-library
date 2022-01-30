8ca3361ad97b96038eb0991d06c14347ab824b27
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-01-30 22:03:27 +0000

    Revert "fix: use saturating_sub for voting_proposal_count"
    This reverts commit 5d357049b7bbc12d8ec9d46caefe3b740967fae3.

--- governance/program/src/processor/process_cancel_proposal.rs
+++ governance/program/src/processor/process_cancel_proposal.rs
@@ -50,12 +50,14 @@ pub fn process_cancel_proposal(program_id: &Pubkey, accounts: &[AccountInfo]) ->
 
     if proposal_data.state == ProposalState::Voting {
         // Update Realm voting_proposal_count
-        realm_data.voting_proposal_count = realm_data.voting_proposal_count.saturating_sub(1);
+        realm_data.voting_proposal_count = realm_data.voting_proposal_count.checked_sub(1).unwrap();
         realm_data.serialize(&mut *realm_info.data.borrow_mut())?;
 
         // Update  Governance voting_proposal_count
-        governance_data.voting_proposal_count =
-            governance_data.voting_proposal_count.saturating_sub(1);
+        governance_data.voting_proposal_count = governance_data
+            .voting_proposal_count
+            .checked_sub(1)
+            .unwrap();
         governance_data.serialize(&mut *governance_info.data.borrow_mut())?;
     }
 
