b0a8872594892813af7e536136ff78423713bd7b
Tyera Eulberg <tyera@solana.com>
2022-02-08 18:41:04 -0700

    Allow anyone to burn and close token Accounts owned by the system program and the incinerator

--- token/program-2022/src/state.rs
+++ token/program-2022/src/state.rs
@@ -116,6 +116,11 @@ impl Account {
     pub fn is_native(&self) -> bool {
         self.is_native.is_some()
     }
+    /// Checks if a token Account's owner is the system_program or the incinerator
+    pub fn is_owned_by_system_program_or_incinerator(&self) -> bool {
+        solana_program::system_program::check_id(&self.owner)
+            || solana_program::incinerator::check_id(&self.owner)
+    }
 }
 impl Sealed for Account {}
 impl IsInitialized for Account {
