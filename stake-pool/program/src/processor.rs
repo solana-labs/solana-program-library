17de64333d70178b82541d052032dae430048890
Jon Cinque <jon.cinque@gmail.com>
2022-02-14 23:52:02 +0100

    stake-pool: Option to use transfer + allocate + assign for PDA

--- stake-pool/program/src/processor.rs
+++ stake-pool/program/src/processor.rs
@@ -160,6 +160,59 @@ fn create_transient_stake_account<'a>(
     )
 }
 
+/// Create an account on a program-derived address
+fn create_pda_account<'a>(
+    payer: &AccountInfo<'a>,
+    required_lamports: u64,
+    space: usize,
+    owner: &Pubkey,
+    system_program: &AccountInfo<'a>,
+    new_pda_account: &AccountInfo<'a>,
+    new_pda_signer_seeds: &[&[u8]],
+) -> ProgramResult {
+    if new_pda_account.lamports() > 0 {
+        let required_lamports = required_lamports.saturating_sub(new_pda_account.lamports());
+        if required_lamports > 0 {
+            invoke(
+                &system_instruction::transfer(payer.key, new_pda_account.key, required_lamports),
+                &[
+                    payer.clone(),
+                    new_pda_account.clone(),
+                    system_program.clone(),
+                ],
+            )?;
+        }
+
+        invoke_signed(
+            &system_instruction::allocate(new_pda_account.key, space as u64),
+            &[new_pda_account.clone(), system_program.clone()],
+            &[new_pda_signer_seeds],
+        )?;
+
+        invoke_signed(
+            &system_instruction::assign(new_pda_account.key, owner),
+            &[new_pda_account.clone(), system_program.clone()],
+            &[new_pda_signer_seeds],
+        )
+    } else {
+        invoke_signed(
+            &system_instruction::create_account(
+                payer.key,
+                new_pda_account.key,
+                required_lamports,
+                space as u64,
+                owner,
+            ),
+            &[
+                payer.clone(),
+                new_pda_account.clone(),
+                system_program.clone(),
+            ],
+            &[new_pda_signer_seeds],
+        )
+    }
+}
+
 /// Program state handler.
 pub struct Processor {}
 impl Processor {
@@ -798,22 +851,19 @@ impl Processor {
         ];
 
         // Fund the stake account with the minimum + rent-exempt balance
-        let required_lamports = MINIMUM_ACTIVE_STAKE
-            + rent.minimum_balance(std::mem::size_of::<stake::state::StakeState>());
+        let space = std::mem::size_of::<stake::state::StakeState>();
+        let required_lamports = MINIMUM_ACTIVE_STAKE + rent.minimum_balance(space);
 
         // Create new stake account
-        invoke_signed(
-            &system_instruction::create_account(
-                funder_info.key,
-                stake_info.key,
-                required_lamports,
-                std::mem::size_of::<stake::state::StakeState>() as u64,
-                &stake::program::id(),
-            ),
-            &[funder_info.clone(), stake_info.clone()],
-            &[stake_account_signer_seeds],
+        create_pda_account(
+            funder_info,
+            required_lamports,
+            space,
+            &stake::program::id(),
+            system_program_info,
+            stake_info,
+            stake_account_signer_seeds,
         )?;
-
         invoke(
             &stake::instruction::initialize(
                 stake_info.key,
