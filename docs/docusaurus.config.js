ff0cc61bfe8665222153a94359d7852f79a6d922
Jacob Creech <jacob.creech@solana.com>
2022-02-22 12:10:25 -0600

    Updated token docs
    Update token docs backlinks
    
    Update docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ module.exports = {
         src: "img/logo-horizontal.svg",
         srcDark: "img/logo-horizontal-dark.svg",
       },
-      links: [
+      items: [
         {
           href: "https://docs.solana.com/",
           label: "Docs »",
