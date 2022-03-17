35a282b231b51e99b10124869d3c74a0b92c15c5
Nope X <nope@solend.fi>
2022-03-17 02:44:07 -0700

    repay floor to ceil hotfix

--- token-lending/program/src/state/reserve.rs
+++ token-lending/program/src/state/reserve.rs
@@ -250,7 +250,7 @@ impl Reserve {
                 }
                 Ordering::Less => {
                     let withdraw_pct = liquidation_value.try_div(collateral.market_value)?;
-                    repay_amount = max_amount.try_floor_u64()?;
+                    repay_amount = max_amount.try_ceil_u64()?;
                     withdraw_amount = Decimal::from(collateral.deposited_amount)
                         .try_mul(withdraw_pct)?
                         .try_floor_u64()?;
@@ -282,7 +282,7 @@ impl Reserve {
                 Ordering::Less => {
                     let withdraw_pct = liquidation_value.try_div(collateral.market_value)?;
                     settle_amount = liquidation_amount;
-                    repay_amount = settle_amount.try_floor_u64()?;
+                    repay_amount = settle_amount.try_ceil_u64()?;
                     withdraw_amount = Decimal::from(collateral.deposited_amount)
                         .try_mul(withdraw_pct)?
                         .try_floor_u64()?;
