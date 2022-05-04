7d2048bd1caf2a498df9ccacd0b4042720122ea3
Jon Cinque <jon.cinque@gmail.com>
2022-05-04 13:02:50 +0200

    token-lending: Fix maximum withdrawal value calculation (#3138)

--- token-lending/program/src/state/obligation.rs
+++ token-lending/program/src/state/obligation.rs
@@ -91,15 +91,19 @@ impl Obligation {
     }
 
     /// Calculate the maximum collateral value that can be withdrawn
-    pub fn max_withdraw_value(&self) -> Result<Decimal, ProgramError> {
-        let required_deposit_value = self
-            .borrowed_value
-            .try_mul(self.deposited_value)?
-            .try_div(self.allowed_borrow_value)?;
-        if required_deposit_value >= self.deposited_value {
+    pub fn max_withdraw_value(
+        &self,
+        withdraw_collateral_ltv: Rate,
+    ) -> Result<Decimal, ProgramError> {
+        if self.allowed_borrow_value <= self.borrowed_value {
             return Ok(Decimal::zero());
         }
-        self.deposited_value.try_sub(required_deposit_value)
+        if withdraw_collateral_ltv == Rate::zero() {
+            return Ok(self.deposited_value);
+        }
+        self.allowed_borrow_value
+            .try_sub(self.borrowed_value)?
+            .try_div(withdraw_collateral_ltv)
     }
 
     /// Calculate the maximum liquidity value that can be borrowed
