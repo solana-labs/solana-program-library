da377e0d546c3e247b8a351b704d404c1fcb3dec
Noah Gundotra <noahgundotra@noahs-mbp.mynetworksettings.com>
2022-06-06 11:42:04 -0700

    add solita for gumball-machine

---
+++ contracts/sdk/gumball-machine/src/generated/index.ts
@@ -0,0 +1,19 @@
+import { PublicKey } from '@solana/web3.js'
+export * from './instructions'
+export * from './types'
+
+/**
+ * Program address
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ADDRESS = 'BRKyVDRGT7SPBtMhjHN4PVSPVYoc3Wa3QTyuRVM4iZkt'
+
+/**
+ * Program public key
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ID = new PublicKey(PROGRAM_ADDRESS)
