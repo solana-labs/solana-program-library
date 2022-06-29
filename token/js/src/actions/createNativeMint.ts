4043db0c9f506a0fbaa662a42af5bab1dd0511af
Jon Cinque <jon.cinque@gmail.com>
2022-06-29 11:56:58 -0400

    Fixup updated params

--- token/js/src/actions/createNativeMint.ts
+++ token/js/src/actions/createNativeMint.ts
@@ -19,7 +19,7 @@ export async function createNativeMint(
     programId = TOKEN_2022_PROGRAM_ID
 ): Promise<void> {
     const transaction = new Transaction().add(
-        createCreateNativeMintInstruction(payer.publicKey, programId, nativeMint)
+        createCreateNativeMintInstruction(payer.publicKey, nativeMint, programId)
     );
     await sendAndConfirmTransaction(connection, transaction, [payer], confirmOptions);
 }
