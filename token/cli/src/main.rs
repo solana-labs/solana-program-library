246867c40b3b87bcc331a6418e5a5fb0453deef0
Sumeet <50953182+SUMEETRM@users.noreply.github.com>
2022-07-18 22:38:20 +0530

    Reverse logic for --recepient-is-ata-owner

--- token/cli/src/main.rs
+++ token/cli/src/main.rs
@@ -1144,7 +1144,7 @@ async fn command_transfer(
     allow_unfunded_recipient: bool,
     fund_recipient: bool,
     mint_decimals: Option<u8>,
-    recipient_is_ata_owner: bool,
+    no_recipient_is_ata_owner: bool,
     use_unchecked_instruction: bool,
     ui_fee: Option<f64>,
     memo: Option<String>,
@@ -1290,7 +1290,7 @@ async fn command_transfer(
         }
     } else {
         // in offline mode we gotta trust them
-        !recipient_is_ata_owner
+        no_recipient_is_ata_owner
     };
 
     // now if its a token account, life is ez
@@ -3134,8 +3134,8 @@ fn app<'a, 'b>(
                         .help("Send tokens to the recipient even if the recipient is not a wallet owned by System Program."),
                 )
                 .arg(
-                    Arg::with_name("recipient_is_ata_owner")
-                        .long("recipient-is-ata-owner")
+                    Arg::with_name("no_recipient_is_ata_owner")
+                        .long("no-recipient-is-ata-owner")
                         .takes_value(false)
                         .requires("sign_only")
                         .help("In sign-only mode, specifies that the recipient is the owner of the associated token account rather than an actual token account"),
@@ -4243,7 +4243,7 @@ async fn process_command<'a>(
             let allow_unfunded_recipient = arg_matches.is_present("allow_empty_recipient")
                 || arg_matches.is_present("allow_unfunded_recipient");
 
-            let recipient_is_ata_owner = arg_matches.is_present("recipient_is_ata_owner");
+            let no_recipient_is_ata_owner = arg_matches.is_present("no_recipient_is_ata_owner");
             let use_unchecked_instruction = arg_matches.is_present("use_unchecked_instruction");
             let expected_fee = value_of::<f64>(arg_matches, "expected_fee");
             let memo = value_t!(arg_matches, "memo", String).ok();
@@ -4263,7 +4263,7 @@ async fn process_command<'a>(
                 allow_unfunded_recipient,
                 fund_recipient,
                 mint_decimals,
-                recipient_is_ata_owner,
+                no_recipient_is_ata_owner,
                 use_unchecked_instruction,
                 expected_fee,
                 memo,
