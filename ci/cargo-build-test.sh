b0ea0bb39ce3ca3ada2b7ff0a1f305a6ff4a9969
Jon Cinque <jon.cinque@gmail.com>
2022-07-21 13:18:00 +0200

    Do it during `build` and not `test`

--- ci/cargo-build-test.sh
+++ ci/cargo-build-test.sh
@@ -15,8 +15,8 @@ set -x
 make -C examples/c
 
 # Build/test all host crates
-cargo +"$rust_stable" build
-BUILD_DEPENDENT_PROGRAMS=1 cargo +"$rust_stable" test -- --nocapture
+BUILD_DEPENDENT_PROGRAMS=1 cargo +"$rust_stable" build
+cargo +"$rust_stable" test -- --nocapture
 
 # Run test-client sanity check
 cargo +"$rust_stable" run --manifest-path=utils/test-client/Cargo.toml
