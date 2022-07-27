57e04021f183532d4bcad42f31bc9718707b8641
Jon Cinque <jon.cinque@gmail.com>
2022-07-27 13:50:09 +0200

    ci: Fixup docs CI to run build on all affecting PRs

--- docs/build.sh
+++ docs/build.sh
@@ -6,6 +6,15 @@ cd "$(dirname "$0")"
 # shellcheck source=ci/env.sh
 source ../ci/env.sh
 
+# Publish only from merge commits and release tags
+if [[ -n $CI ]]; then
+  if [[ -z $CI_PULL_REQUEST ]]; then
+    npm install --global docusaurus-init
+    docusaurus-init
+    npm install --global vercel
+  fi
+fi
+
 # Build from /src into /build
 npm run build
 
