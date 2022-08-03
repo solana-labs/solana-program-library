93ec6cf74a874347b8117235e3462064c756feb7
Jon Cinque <jon.cinque@gmail.com>
2022-08-03 21:54:35 +0200

    token-cli: Split out CI into separate step, run tests in serial (#3425)
    * token-cli: Split out CI into separate step, run tests in serial
    
    * Remove dependency on cargo-test-bpf to speed things up
    
    * Don't download programs since they won't be available

--- ci/cargo-build-test.sh
+++ ci/cargo-build-test.sh
@@ -15,8 +15,8 @@ set -x
 make -C examples/c
 
 # Build/test all host crates
-BUILD_DEPENDENT_PROGRAMS=1 cargo +"$rust_stable" build
-cargo +"$rust_stable" test -- --nocapture
+cargo +"$rust_stable" build --workspace --exclude spl-token-cli
+cargo +"$rust_stable" test --workspace --exclude spl-token-cli -- --nocapture
 
 # Run test-client sanity check
 cargo +"$rust_stable" run --manifest-path=utils/test-client/Cargo.toml
