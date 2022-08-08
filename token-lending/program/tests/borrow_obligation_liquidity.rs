3eba4942bd51f2402bf112edb514882636f0e401
Justin Malčić <j.malcic@me.com>
2022-08-09 00:14:07 +0100

    Split instruction

--- token-lending/program/tests/borrow_obligation_liquidity.rs
+++ token-lending/program/tests/borrow_obligation_liquidity.rs
@@ -11,7 +11,9 @@ use solana_sdk::{
 };
 use spl_token_lending::{
     error::LendingError,
-    instruction::{borrow_obligation_liquidity, refresh_obligation},
+    instruction::{
+        borrow_all_obligation_liquidity, borrow_obligation_liquidity, refresh_obligation,
+    },
     math::Decimal,
     processor::process_instruction,
     state::{FeeCalculation, INITIAL_COLLATERAL_RATIO},
@@ -102,7 +104,6 @@ async fn test_borrow_usdc_fixed_amount() {
             borrow_obligation_liquidity(
                 spl_token_lending::id(),
                 USDC_BORROW_AMOUNT_FRACTIONAL,
-                None,
                 usdc_test_reserve.liquidity_supply_pubkey,
                 usdc_test_reserve.user_liquidity_pubkey,
                 usdc_test_reserve.pubkey,
@@ -250,7 +251,6 @@ async fn test_borrow_sol_max_amount() {
             borrow_obligation_liquidity(
                 spl_token_lending::id(),
                 u64::MAX,
-                None,
                 sol_test_reserve.liquidity_supply_pubkey,
                 sol_test_reserve.user_liquidity_pubkey,
                 sol_test_reserve.pubkey,
@@ -382,7 +382,6 @@ async fn test_borrow_too_large() {
             borrow_obligation_liquidity(
                 spl_token_lending::id(),
                 USDC_BORROW_AMOUNT_FRACTIONAL,
-                None,
                 usdc_test_reserve.liquidity_supply_pubkey,
                 usdc_test_reserve.user_liquidity_pubkey,
                 usdc_test_reserve.pubkey,
@@ -494,10 +493,9 @@ async fn test_borrow_max_receive_less_than_slippage() {
                 test_obligation.pubkey,
                 vec![usdc_test_reserve.pubkey],
             ),
-            borrow_obligation_liquidity(
+            borrow_all_obligation_liquidity(
                 spl_token_lending::id(),
-                u64::MAX,
-                Some(SLIPPAGE_LIMIT),
+                SLIPPAGE_LIMIT,
                 sol_test_reserve.liquidity_supply_pubkey,
                 sol_test_reserve.user_liquidity_pubkey,
                 sol_test_reserve.pubkey,
@@ -631,10 +629,9 @@ async fn test_borrow_max_receive_less_than_slippage() {
                 test_obligation.pubkey,
                 vec![usdc_test_reserve.pubkey],
             ),
-            borrow_obligation_liquidity(
+            borrow_all_obligation_liquidity(
                 spl_token_lending::id(),
-                u64::MAX,
-                Some(SLIPPAGE_LIMIT),
+                SLIPPAGE_LIMIT,
                 sol_test_reserve.liquidity_supply_pubkey,
                 sol_test_reserve.user_liquidity_pubkey,
                 sol_test_reserve.pubkey,
@@ -660,102 +657,4 @@ async fn test_borrow_max_receive_less_than_slippage() {
             InstructionError::Custom(LendingError::ExceededSlippage as u32)
         )
     );
-}
-
-#[tokio::test]
-async fn test_borrow_less_than_max_with_slippage() {
-    let mut test = ProgramTest::new(
-        "spl_token_lending",
-        spl_token_lending::id(),
-        processor!(process_instruction),
-    );
-
-    const USDC_TOTAL_BORROW_FRACTIONAL: u64 = 1_000 * FRACTIONAL_TO_USDC;
-    const FEE_AMOUNT: u64 = 100;
-
-    const SOL_DEPOSIT_AMOUNT_LAMPORTS: u64 = 100 * LAMPORTS_TO_SOL * INITIAL_COLLATERAL_RATIO;
-    const USDC_BORROW_AMOUNT_FRACTIONAL: u64 = USDC_TOTAL_BORROW_FRACTIONAL - FEE_AMOUNT;
-    const SOL_RESERVE_COLLATERAL_LAMPORTS: u64 = 2 * SOL_DEPOSIT_AMOUNT_LAMPORTS;
-    const USDC_RESERVE_LIQUIDITY_FRACTIONAL: u64 = 2 * USDC_TOTAL_BORROW_FRACTIONAL;
-    const SLIPPAGE_LIMIT: u64 = u64::MAX;
-
-    let user_accounts_owner = Keypair::new();
-    let lending_market = add_lending_market(&mut test);
-
-    let mut reserve_config = TEST_RESERVE_CONFIG;
-    reserve_config.loan_to_value_ratio = 50;
-
-    let sol_oracle = add_sol_oracle(&mut test);
-    let sol_test_reserve = add_reserve(
-        &mut test,
-        &lending_market,
-        &sol_oracle,
-        &user_accounts_owner,
-        AddReserveArgs {
-            collateral_amount: SOL_RESERVE_COLLATERAL_LAMPORTS,
-            liquidity_mint_pubkey: spl_token::native_mint::id(),
-            liquidity_mint_decimals: 9,
-            config: reserve_config,
-            mark_fresh: true,
-            ..AddReserveArgs::default()
-        },
-    );
-
-    let usdc_mint = add_usdc_mint(&mut test);
-    let usdc_oracle = add_usdc_oracle(&mut test);
-    let usdc_test_reserve = add_reserve(
-        &mut test,
-        &lending_market,
-        &usdc_oracle,
-        &user_accounts_owner,
-        AddReserveArgs {
-            liquidity_amount: USDC_RESERVE_LIQUIDITY_FRACTIONAL,
-            liquidity_mint_pubkey: usdc_mint.pubkey,
-            liquidity_mint_decimals: usdc_mint.decimals,
-            config: reserve_config,
-            mark_fresh: true,
-            ..AddReserveArgs::default()
-        },
-    );
-
-    let test_obligation = add_obligation(
-        &mut test,
-        &lending_market,
-        &user_accounts_owner,
-        AddObligationArgs {
-            deposits: &[(&sol_test_reserve, SOL_DEPOSIT_AMOUNT_LAMPORTS)],
-            ..AddObligationArgs::default()
-        },
-    );
-
-    let (mut banks_client, payer, recent_blockhash) = test.start().await;
-
-    let mut transaction = Transaction::new_with_payer(
-        &[
-            refresh_obligation(
-                spl_token_lending::id(),
-                test_obligation.pubkey,
-                vec![sol_test_reserve.pubkey],
-            ),
-            borrow_obligation_liquidity(
-                spl_token_lending::id(),
-                USDC_BORROW_AMOUNT_FRACTIONAL,
-                Some(SLIPPAGE_LIMIT),
-                usdc_test_reserve.liquidity_supply_pubkey,
-                usdc_test_reserve.user_liquidity_pubkey,
-                usdc_test_reserve.pubkey,
-                usdc_test_reserve.liquidity_fee_receiver_pubkey,
-                test_obligation.pubkey,
-                lending_market.pubkey,
-                test_obligation.owner,
-                Some(usdc_test_reserve.liquidity_host_pubkey),
-            ),
-        ],
-        Some(&payer.pubkey()),
-    );
-
-    transaction.sign(&[&payer, &user_accounts_owner], recent_blockhash);
-
-    // check that transaction succeeds
-    assert!(banks_client.process_transaction(transaction).await.is_ok());
 }
