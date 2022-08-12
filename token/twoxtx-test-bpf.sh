679561d3475a9ebec4d2db7dc98ee169020ba665
Jon Cinque <jon.cinque@gmail.com>
2022-08-12 14:08:48 +0200

    Build required programs

---
+++ token/twoxtx-test-bpf.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Run token-2022 program tests against a Solana v1.12 monorepo that supports
+# 2x transactions for testing the SPL Token 2022 Confidential Transfer extension
+#
+
+set -e
+
+here="$(dirname "$0")"
+cd "$here"
+
+if [[ ! -d twoxtx-solana ]]; then
+  echo "twoxtx-solana dir not found"
+  exit 1
+fi
+
+echo "Build required programs"
+./twoxtx-solana/cargo-build-sbf --manifest-path ./program-2022/Cargo.toml
+./twoxtx-solana/cargo-build-sbf --manifest-path ../associated-token-account/program/Cargo.toml
+
+echo "Test token-2022"
+./twoxtx-solana/cargo-test-sbf --manifest-path ./program-2022-test/Cargo.toml -- --nocapture
+
+exit 0
+
