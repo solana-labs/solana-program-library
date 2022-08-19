2ec44293a7e2c7427cc0a8270375206f5c71e772
ngundotra <noah@gundotra.org>
2022-08-19 11:48:53 -0700

    AC: add documentation for enabling Canopy

--- account-compression/programs/account-compression/src/lib.rs
+++ account-compression/programs/account-compression/src/lib.rs
@@ -28,16 +28,16 @@ use anchor_lang::{
     solana_program::sysvar::{clock::Clock, rent::Rent},
 };
 use borsh::{BorshDeserialize, BorshSerialize};
-use bytemuck::cast_slice_mut;
-use spl_concurrent_merkle_tree::node::{empty_node_cached, EMPTY};
 use std::mem::size_of;
 
+pub mod canopy;
 pub mod data_wrapper;
 pub mod error;
 pub mod events;
 pub mod state;
 pub mod zero_copy;
 
+use crate::canopy::{fill_in_proof_from_canopy, update_canopy};
 use crate::data_wrapper::{wrap_event, Wrapper};
 use crate::error::AccountCompressionError;
 use crate::events::ChangeLogEvent;
@@ -104,109 +104,6 @@ pub struct TransferAuthority<'info> {
     /// Authority that validates the content of the trees.
     /// Typically a program, e.g., the Bubblegum contract validates that leaves are valid NFTs.
     pub authority: Signer<'info>,
-}
-
-#[inline(always)]
-fn check_canopy_bytes(canopy_bytes: &mut [u8]) -> Result<()> {
-    if canopy_bytes.len() % size_of::<Node>() != 0 {
-        msg!(
-            "Canopy byte length {} is not a multiple of {}",
-            canopy_bytes.len(),
-            size_of::<Node>()
-        );
-        err!(AccountCompressionError::CanopyLengthMismatch)
-    } else {
-        Ok(())
-    }
-}
-
-#[inline(always)]
-fn get_cached_path_length(canopy: &mut [Node], max_depth: u32) -> Result<u32> {
-    // The offset of 2 is applied because the canopy is a full binary tree without the root node
-    // Size: (2^n - 2) -> Size + 2 must be a power of 2
-    let closest_power_of_2 = (canopy.len() + 2) as u32;
-    // This expression will return true if `closest_power_of_2` is actually a power of 2
-    if closest_power_of_2 & (closest_power_of_2 - 1) == 0 {
-        // (1 << max_depth) returns the number of leaves in the full merkle tree
-        // (1 << (max_depth + 1)) - 1 returns the number of nodes in the full tree
-        // The canopy size cannot exceed the size of the tree
-        if closest_power_of_2 > (1 << (max_depth + 1)) {
-            msg!(
-                "Canopy size is too large. Size: {}. Max size: {}",
-                closest_power_of_2 - 2,
-                (1 << (max_depth + 1)) - 2
-            );
-            return err!(AccountCompressionError::CanopyLengthMismatch);
-        }
-    } else {
-        msg!(
-            "Canopy length {} is not 2 less than a power of 2",
-            canopy.len()
-        );
-        return err!(AccountCompressionError::CanopyLengthMismatch);
-    }
-    // 1 is subtracted from the trailing zeros because the root is not stored in the canopy
-    Ok(closest_power_of_2.trailing_zeros() - 1)
-}
-
-fn update_canopy(
-    canopy_bytes: &mut [u8],
-    max_depth: u32,
-    change_log: Option<Box<ChangeLogEvent>>,
-) -> Result<()> {
-    check_canopy_bytes(canopy_bytes)?;
-    let canopy = cast_slice_mut::<u8, Node>(canopy_bytes);
-    let path_len = get_cached_path_length(canopy, max_depth)?;
-    if let Some(cl) = change_log {
-        // Update the canopy from the newest change log
-        for path_node in cl.path.iter().rev().skip(1).take(path_len as usize) {
-            // node_idx - 2 maps to the canopy index
-            canopy[(path_node.index - 2) as usize] = path_node.node;
-        }
-    }
-    Ok(())
-}
-
-fn fill_in_proof_from_canopy(
-    canopy_bytes: &mut [u8],
-    max_depth: u32,
-    index: u32,
-    proof: &mut Vec<Node>,
-) -> Result<()> {
-    // 30 is hard coded as it is the current max depth that SPL Compression supports
-    let mut empty_node_cache = Box::new([EMPTY; 30]);
-    check_canopy_bytes(canopy_bytes)?;
-    let canopy = cast_slice_mut::<u8, Node>(canopy_bytes);
-    let path_len = get_cached_path_length(canopy, max_depth)?;
-
-    // We want to compute the node index (w.r.t. the canopy) where the current path
-    // intersects the leaves of the canopy
-    let mut node_idx = ((1 << max_depth) + index) >> (max_depth - path_len);
-    let mut inferred_nodes = vec![];
-    while node_idx > 1 {
-        // node_idx - 2 maps to the canopy index
-        let shifted_index = node_idx as usize - 2;
-        let cached_idx = if shifted_index % 2 == 0 {
-            shifted_index + 1
-        } else {
-            shifted_index - 1
-        };
-        if canopy[cached_idx] == EMPTY {
-            let level = max_depth - (31 - node_idx.leading_zeros());
-            let empty_node = empty_node_cached::<30>(level, &mut empty_node_cache);
-            canopy[cached_idx] = empty_node;
-            inferred_nodes.push(empty_node);
-        } else {
-            inferred_nodes.push(canopy[cached_idx]);
-        }
-        node_idx >>= 1;
-    }
-    // We only want to add inferred canopy nodes such that the proof length
-    // is equal to the tree depth. If the lengh of proof + lengh of canopy nodes is
-    // less than the tree depth, the instruction will fail.
-    let overlap = (proof.len() + inferred_nodes.len()).saturating_sub(max_depth as usize);
-    proof.extend(inferred_nodes.iter().skip(overlap));
-    Ok(())
 }
 
 /// This macro applies functions on a ConcurrentMerkleT:ee and emits leaf information
@@ -318,6 +215,8 @@ pub mod spl_compression {
     ///
     /// Concurrency limit should be determined by empirically testing the demand for
     /// state built on top of SPL Compression.
+    ///
+    /// For instructions on enabling the canopy, see [canopy].
     pub fn init_empty_merkle_tree(
         ctx: Context<Initialize>,
         max_depth: u32,
