cade7e3f8993a1fe11a48535fb20c944a22f6ba5
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-08-20 17:08:55 +0200

    chore: test_cast_community_vote_with_early_council_and_disabled_community_vote_tipping

--- governance/program/tests/process_cast_vote.rs
+++ governance/program/tests/process_cast_vote.rs
@@ -1329,6 +1329,10 @@ async fn test_cast_early_council_vote_with_disabled_community_vote_tipping() {
         .await
         .unwrap();
 
+    governance_test
+        .mint_community_tokens(&realm_cookie, 20)
+        .await;
+
     // Act
     governance_test
         .with_cast_yes_no_vote(&proposal_cookie, &token_owner_record_cookie, YesNoVote::Yes)
@@ -1342,4 +1346,56 @@ async fn test_cast_early_council_vote_with_disabled_community_vote_tipping() {
         .await;
 
     assert_eq!(proposal_account.state, ProposalState::Succeeded);
+}
+
+#[tokio::test]
+async fn test_cast_community_vote_with_early_council_and_disabled_community_vote_tipping() {
+    // Arrange
+    let mut governance_test = GovernanceProgramTest::start_new().await;
+
+    let realm_cookie = governance_test.with_realm().await;
+    let governed_account_cookie = governance_test.with_governed_account().await;
+
+    let token_owner_record_cookie = governance_test
+        .with_community_token_deposit(&realm_cookie)
+        .await
+        .unwrap();
+
+    let mut governance_config = governance_test.get_default_governance_config();
+
+    governance_config.community_vote_tipping = VoteTipping::Disabled;
+    governance_config.council_vote_tipping = VoteTipping::Early;
+
+    let mut governance_cookie = governance_test
+        .with_governance_using_config(
+            &realm_cookie,
+            &governed_account_cookie,
+            &token_owner_record_cookie,
+            &governance_config,
+        )
+        .await
+        .unwrap();
+
+    let proposal_cookie = governance_test
+        .with_signed_off_proposal(&token_owner_record_cookie, &mut governance_cookie)
+        .await
+        .unwrap();
+
+    governance_test
+        .mint_community_tokens(&realm_cookie, 20)
+        .await;
+
+    // Act
+    governance_test
+        .with_cast_yes_no_vote(&proposal_cookie, &token_owner_record_cookie, YesNoVote::Yes)
+        .await
+        .unwrap();
+
+    // Assert
+
+    let proposal_account = governance_test
+        .get_proposal_account(&proposal_cookie.address)
+        .await;
+
+    assert_eq!(proposal_account.state, ProposalState::Voting);
 }
