d452e5acaf182094928636950f635c2bea0f04c6
hanako mumei <81144685+2501babe@users.noreply.github.com>
2022-08-22 11:12:32 -0700

    haha jk another one

--- token/client/src/token.rs
+++ token/client/src/token.rs
@@ -546,9 +546,9 @@ where
     ) -> TokenResult<T::Output> {
         let signing_pubkeys = signing_keypairs.pubkeys();
         let multisig = if signing_pubkeys == &[*authority] {
-            signing_pubkeys.iter().collect::<Vec<_>>()
-        } else {
             vec![]
+        } else {
+            signing_pubkeys.iter().collect::<Vec<_>>()
         };
 
         self.process_ixs(
