81f34f2d1367c18f33f7c55dbf3eb4d292a49add
0xripleys <105607696+0xripleys@users.noreply.github.com>
2022-08-30 22:04:14 -0400

    bump cargo version (#103)

--- ci/rust-version.sh
+++ ci/rust-version.sh
@@ -18,7 +18,7 @@
 if [[ -n $RUST_STABLE_VERSION ]]; then
   stable_version="$RUST_STABLE_VERSION"
 else
-  stable_version=1.58.1
+  stable_version=1.63.0
 fi
 
 if [[ -n $RUST_NIGHTLY_VERSION ]]; then
