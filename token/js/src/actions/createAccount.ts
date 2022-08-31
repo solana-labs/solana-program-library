ca0627b25a3a7382e246748fe13f0ec0f464a3a9
mwrites <20499416+mwrites@users.noreply.github.com>
2022-09-01 00:41:26 +0100

    revert

--- token/js/src/actions/createAccount.ts
+++ token/js/src/actions/createAccount.ts
@@ -1,4 +1,4 @@
-import { ConfirmOptions, Connection, Keypair, PublicKey, Signer } from '@solana/web3.js';
+import type { ConfirmOptions, Connection, Keypair, PublicKey, Signer } from '@solana/web3.js';
 import { sendAndConfirmTransaction, SystemProgram, Transaction } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID } from '../constants.js';
 import { getAccountLenForMint } from '../extensions/extensionType.js';
