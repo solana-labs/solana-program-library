7fe03ab394169a3959a5fa603125475092ab1f05
hanako mumei <81144685+2501babe@users.noreply.github.com>
2022-09-09 13:31:18 -0700

    token-cli: impl `spl-token display` for mint
    also show decimals for accounts, because we can

--- token/cli/src/sort.rs
+++ token/cli/src/sort.rs
@@ -56,6 +56,7 @@ pub(crate) fn sort_and_parse_token_accounts(
                         let parsed_account = CliTokenAccount {
                             address,
                             program_id: program_id.to_string(),
+                            decimals: None,
                             account: ui_token_account,
                             is_associated,
                         };
