c5b41d7ee3b510a83776e477a3e98d9038c0320b
Guillermo Bescos <gbescos@stanford.edu>
2022-10-25 23:43:00 +0000

    Checkpoint

---
+++ governance/program/src/state/ephemeral_signer.rs
@@ -0,0 +1,15 @@
+//! Ephemeral signer
+use solana_program::pubkey::Pubkey;
+
+/// TO DO DOCUMENTATION
+pub fn get_ephemeral_signer_seeds<'a>(proposal_transaction_pubkey: &'a Pubkey, account_seq_number_le_bytes : &'a [u8; 2]) -> [&'a [u8]; 3] {
+    [b"ephemeral-signer", proposal_transaction_pubkey.as_ref(), account_seq_number_le_bytes]
+}
+
+/// Returns ProposalExtraAccount PDA address
+pub fn get_ephemeral_signer_address_and_seeds<'a>(program_id: &Pubkey, proposal_transaction_pubkey: &'a Pubkey, account_seq_number_le_bytes : &'a [u8; 2]) -> (Pubkey, u8, Vec<&'a [u8]>)  {
+    let seeds = &get_ephemeral_signer_seeds(proposal_transaction_pubkey, account_seq_number_le_bytes);
+    let (address, bump) = Pubkey::find_program_address(seeds, program_id);
+    let seeds_vec = seeds.to_vec();
+    return (address, bump, seeds_vec)
+}
