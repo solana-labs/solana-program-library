574fe73465201a4ab27c8c2cc76dcba29d2dda56
Tyera Eulberg <tyera@solana.com>
2022-11-02 10:33:37 -0600

    Fix CpiGuard typo, but actually DRY processor methods (#3791)
    * Fix typo
    
    * DRY CpiGuard
    
    * DRY RequiredMemoTransfers

--- token/program-2022/src/extension/cpi_guard/processor.rs
+++ token/program-2022/src/extension/cpi_guard/processor.rs
@@ -18,7 +18,12 @@ use {
     },
 };
 
-fn process_enable_cpi_guard(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+/// Toggle the CpiGuard extension, initializing the extension if not already present.
+fn process_toggle_cpi_guard(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    enable: bool,
+) -> ProgramResult {
     let account_info_iter = &mut accounts.iter();
     let token_account_info = next_account_info(account_info_iter)?;
     let owner_info = next_account_info(account_info_iter)?;
@@ -44,37 +49,7 @@ fn process_enable_cpi_guard(program_id: &Pubkey, accounts: &[AccountInfo]) -> Pr
     } else {
         account.init_extension::<CpiGuard>(true)?
     };
-    extension.lock_cpi = true.into();
-    Ok(())
-}
-
-fn process_diasble_cpi_guard(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
-    let account_info_iter = &mut accounts.iter();
-    let token_account_info = next_account_info(account_info_iter)?;
-    let owner_info = next_account_info(account_info_iter)?;
-    let owner_info_data_len = owner_info.data_len();
-
-    let mut account_data = token_account_info.data.borrow_mut();
-    let mut account = StateWithExtensionsMut::<Account>::unpack(&mut account_data)?;
-
-    Processor::validate_owner(
-        program_id,
-        &account.base.owner,
-        owner_info,
-        owner_info_data_len,
-        account_info_iter.as_slice(),
-    )?;
-
-    if in_cpi() {
-        return Err(TokenError::CpiGuardSettingsLocked.into());
-    }
-
-    let extension = if let Ok(extension) = account.get_extension_mut::<CpiGuard>() {
-        extension
-    } else {
-        account.init_extension::<CpiGuard>(true)?
-    };
-    extension.lock_cpi = false.into();
+    extension.lock_cpi = enable.into();
     Ok(())
 }
 
@@ -88,11 +63,11 @@ pub(crate) fn process_instruction(
     match decode_instruction_type(input)? {
         CpiGuardInstruction::Enable => {
             msg!("CpiGuardInstruction::Enable");
-            process_enable_cpi_guard(program_id, accounts)
+            process_toggle_cpi_guard(program_id, accounts, true /* enable */)
         }
         CpiGuardInstruction::Disable => {
             msg!("CpiGuardInstruction::Disable");
-            process_diasble_cpi_guard(program_id, accounts)
+            process_toggle_cpi_guard(program_id, accounts, false /* disable */)
         }
     }
 }
