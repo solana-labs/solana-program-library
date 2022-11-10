c8ee26672b0ac53138f79a37d0713821bad43f7a
Sebastian.Bor <sebastian_bor@hotmail.com>
2022-11-10 23:39:29 +0000

    chore: fix compilation

--- governance/program/tests/process_create_realm.rs
+++ governance/program/tests/process_create_realm.rs
@@ -5,9 +5,6 @@ use solana_program_test::*;
 mod program_test;
 
 use program_test::*;
-use spl_governance::state::{enums::MintMaxVoterWeightSource, realm::get_realm_address};
-
-use crate::program_test::args::RealmSetupArgs;
 
 #[tokio::test]
 async fn test_create_realm() {
