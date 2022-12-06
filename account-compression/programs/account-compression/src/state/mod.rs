ed8522b56ea531dcb992ba41e51d98998c6b4f3e
Austin Adams <austbot@users.noreply.github.com>
2022-12-06 10:32:41 -0600

    Expose more types from CMT (#3875)
    * Expose other types from CMT to allow usage on the client side while deserializing accounts.
    
    * adds a convenient creation slot getter

--- account-compression/programs/account-compression/src/state/mod.rs
+++ account-compression/programs/account-compression/src/state/mod.rs
@@ -2,7 +2,5 @@
 mod concurrent_merkle_tree_header;
 mod path_node;
 
-pub use concurrent_merkle_tree_header::{
-    ConcurrentMerkleTreeHeader, CONCURRENT_MERKLE_TREE_HEADER_SIZE_V1,
-};
+pub use concurrent_merkle_tree_header::*;
 pub use path_node::PathNode;
