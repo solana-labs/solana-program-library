9fa6c5050e29a20df72e6b5da123accd61d02e93
ngundotra <noah@gundotra.org>
2023-01-17 15:23:04 -0500

    Make verifyLeaf work for accounts with canopy

--- account-compression/programs/account-compression/src/state/concurrent_merkle_tree_header.rs
+++ account-compression/programs/account-compression/src/state/concurrent_merkle_tree_header.rs
@@ -1,6 +1,9 @@
 use anchor_lang::prelude::*;
 use borsh::{BorshDeserialize, BorshSerialize};
 
+use spl_concurrent_merkle_tree::concurrent_merkle_tree::ConcurrentMerkleTree;
+use std::mem::size_of;
+
 use crate::error::AccountCompressionError;
 
 pub const CONCURRENT_MERKLE_TREE_HEADER_SIZE_V1: usize = 2 + 54;
@@ -151,5 +154,45 @@ impl ConcurrentMerkleTreeHeader {
             return Err(AccountCompressionError::LeafIndexOutOfBounds.into());
         }
         Ok(())
+    }
+}
+
+pub fn merkle_tree_get_size(header: &ConcurrentMerkleTreeHeader) -> Result<usize> {
+    // Note: max_buffer_size MUST be a power of 2
+    match (header.get_max_depth(), header.get_max_buffer_size()) {
+        (3, 8) => Ok(size_of::<ConcurrentMerkleTree<3, 8>>()),
+        (5, 8) => Ok(size_of::<ConcurrentMerkleTree<5, 8>>()),
+        (14, 64) => Ok(size_of::<ConcurrentMerkleTree<14, 64>>()),
+        (14, 256) => Ok(size_of::<ConcurrentMerkleTree<14, 256>>()),
+        (14, 1024) => Ok(size_of::<ConcurrentMerkleTree<14, 1024>>()),
+        (14, 2048) => Ok(size_of::<ConcurrentMerkleTree<14, 2048>>()),
+        (15, 64) => Ok(size_of::<ConcurrentMerkleTree<15, 64>>()),
+        (16, 64) => Ok(size_of::<ConcurrentMerkleTree<16, 64>>()),
+        (17, 64) => Ok(size_of::<ConcurrentMerkleTree<17, 64>>()),
+        (18, 64) => Ok(size_of::<ConcurrentMerkleTree<18, 64>>()),
+        (19, 64) => Ok(size_of::<ConcurrentMerkleTree<19, 64>>()),
+        (20, 64) => Ok(size_of::<ConcurrentMerkleTree<20, 64>>()),
+        (20, 256) => Ok(size_of::<ConcurrentMerkleTree<20, 256>>()),
+        (20, 1024) => Ok(size_of::<ConcurrentMerkleTree<20, 1024>>()),
+        (20, 2048) => Ok(size_of::<ConcurrentMerkleTree<20, 2048>>()),
+        (24, 64) => Ok(size_of::<ConcurrentMerkleTree<24, 64>>()),
+        (24, 256) => Ok(size_of::<ConcurrentMerkleTree<24, 256>>()),
+        (24, 512) => Ok(size_of::<ConcurrentMerkleTree<24, 512>>()),
+        (24, 1024) => Ok(size_of::<ConcurrentMerkleTree<24, 1024>>()),
+        (24, 2048) => Ok(size_of::<ConcurrentMerkleTree<24, 2048>>()),
+        (26, 512) => Ok(size_of::<ConcurrentMerkleTree<26, 512>>()),
+        (26, 1024) => Ok(size_of::<ConcurrentMerkleTree<26, 1024>>()),
+        (26, 2048) => Ok(size_of::<ConcurrentMerkleTree<26, 2048>>()),
+        (30, 512) => Ok(size_of::<ConcurrentMerkleTree<30, 512>>()),
+        (30, 1024) => Ok(size_of::<ConcurrentMerkleTree<30, 1024>>()),
+        (30, 2048) => Ok(size_of::<ConcurrentMerkleTree<30, 2048>>()),
+        _ => {
+            msg!(
+                "Failed to get size of max depth {} and max buffer size {}",
+                header.get_max_depth(),
+                header.get_max_buffer_size()
+            );
+            err!(AccountCompressionError::ConcurrentMerkleTreeConstantsError)
+        }
     }
 }
