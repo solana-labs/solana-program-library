01508abb03adb059745565c0c126c56aa45a33aa
Giovanni Napoli <gnapoli.dev@gmail.com>
2023-02-02 12:06:44 +0100

    update test

--- stake-pool/py/tests/test_create_token_metadata.py
+++ stake-pool/py/tests/test_create_token_metadata.py
@@ -5,6 +5,7 @@ from solana.rpc.commitment import Confirmed
 from solana.rpc.async_api import AsyncClient
 from solana.keypair import Keypair
 from stake_pool.constants import find_metadata_account
+from solana.utils.helpers import decode_byte_string
 
 
 
@@ -24,7 +25,7 @@ async def test_create_metadata_success(async_client: AsyncClient, payer: Keypair
 
     (metadata_program_address, _seed) = find_metadata_account(stake_pool.pool_mint)
     resp = await async_client.get_account_info(metadata_program_address, commitment=Confirmed)
-    data = resp['result']['value']['data']
-    assert data[0] == name
-    assert data[1] == symbol
-    assert data[2] == uri
+    raw_data = resp['result']['value']['data']
+    assert name == str(raw_data[69:101], "utf-8")
+    assert symbol == str(raw_data[105:115], "utf-8")
+    assert uri == str(raw_data[119:319], "utf-8")
