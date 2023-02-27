799fb15098500117334cb32b4335aeb862a166e0
Jon Cinque <joncinque@pm.me>
2023-02-27 12:26:42 -0800

    ci: Only copy monorepo toolchain file if it exists

--- patch.crates-io.sh
+++ patch.crates-io.sh
@@ -24,7 +24,12 @@ solana_dir=$(cd "$solana_dir" && pwd)
 cd "$(dirname "$0")"
 
 source "$solana_dir"/scripts/read-cargo-variable.sh
-cp "$solana_dir"/rust-toolchain.toml .
+
+# The toolchain file only exists in version >= 1.15
+toolchain_file="$solana_dir"/rust-toolchain.toml
+if [[ -f "$toolchain_file" ]]; then
+  cp "$toolchain_file" .
+fi
 
 # get version from Cargo.toml first. if it is empty, get it from other places.
 solana_ver="$(readCargoVariable version "$solana_dir"/Cargo.toml)"
