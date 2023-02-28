c27a603425d2ca4f42ef6fc461400c900660ddbc
samkim-crypto <skim13@cs.stanford.edu>
2023-02-28 07:51:12 -0800

    token-cli: add a catch-all pattern to cover all UiExtension instances when displaying token-22 extensions (#4062)

--- token/cli/src/output.rs
+++ token/cli/src/output.rs
@@ -688,7 +688,7 @@ fn display_ui_extension(
         }
         // ExtensionType::Uninitialized is a hack to ensure a mint/account is never the same length as a multisig
         UiExtension::Uninitialized => Ok(()),
-        UiExtension::UnparseableExtension => writeln_name_value(
+        _ => writeln_name_value(
             f,
             "    Unparseable extension:",
             "Consider upgrading to a newer version of spl-token",
