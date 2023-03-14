325f3d26fbc7b7bdb97b0b88dc1b02cd585242fc
Giovanni Napoli <gnapoli.dev@gmail.com>
2023-03-14 21:22:50 +0100

    Support `create` and `update` token metadata in stake-pool python bindings (#3978)

--- stake-pool/py/tests/conftest.py
+++ stake-pool/py/tests/conftest.py
@@ -33,6 +33,8 @@ def solana_test_validator():
         "--reset", "--quiet",
         "--bpf-program", "SPoo1Ku8WFXoNDMHPsrGSTSG1Y47rzgn41SLUNakuHy",
         f"{old_cwd}/../../target/deploy/spl_stake_pool.so",
+        "--bpf-program", "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+        f"{old_cwd}/../program/tests/fixtures/mpl_token_metadata.so",
         "--slots-per-epoch", str(NUM_SLOTS_PER_EPOCH),
     ],)
     yield
