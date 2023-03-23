a1ce4d2b385845d2eff67ce3cd3c127f0f3dc20d
Dzonixy <dzonixy@gmail.com>
2023-03-23 14:01:36 +0100

    removes helper files

--- token/local_testing/initialize_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf test-ledger
-cargo build
-anchor build
-solana-test-validator --bpf-program TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb ../../target/deploy/spl_token_2022.so \
---bpf-program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA ../../target/deploy/spl_token.so 
