e4ef5c72141707a7e38e983415bde30bc3e371a9
Nikola Stojiljkovic <34912809+Dzonixy@users.noreply.github.com>
2023-04-11 17:34:28 +0200

    Update token/program-2022/src/instruction.rs
    Co-authored-by: Jon Cinque <joncinque@pm.me>

--- token/program-2022/src/instruction.rs
+++ token/program-2022/src/instruction.rs
@@ -1882,7 +1882,7 @@ pub fn withdraw_excess_lamports(
     let mut accounts = vec![
         AccountMeta::new(*destination_account, false),
         AccountMeta::new(*source_account, false),
-        AccountMeta::new(*authority, false),
+        AccountMeta::new(*authority, signers.is_empty()),
     ];
 
     for signer in signers {
