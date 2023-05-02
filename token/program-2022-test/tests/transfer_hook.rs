776becd37f9c100c2a2800912c896836c8851ecf
Jon Cinque <joncinque@pm.me>
2023-05-02 23:02:47 +0200

    Fixup tests to use transfer-hook

--- token/program-2022-test/tests/transfer_hook.rs
+++ token/program-2022-test/tests/transfer_hook.rs
@@ -6,7 +6,7 @@ use {
     solana_program_test::{processor, tokio, ProgramTest},
     solana_sdk::{
         account::Account,
-        account_info::{next_account_info, AccountInfo},
+        account_info::AccountInfo,
         entrypoint::ProgramResult,
         instruction::InstructionError,
         program_error::ProgramError,
@@ -16,70 +16,16 @@ use {
         transaction::TransactionError,
         transport::TransportError,
     },
-    spl_permissioned_transfer::{
-        get_extra_account_metas_address,
-        instruction::PermissionedTransferInstruction,
-        pod::PodAccountMeta,
-        state::ExtraAccountMetas,
-        tlv::{TlvState, TlvStateBorrowed, TlvType},
-    },
+    spl_transfer_hook_interface::get_extra_account_metas_address,
     spl_token_2022::{
         error::TokenError,
-        extension::{permissioned_transfer::PermissionedTransfer, BaseStateWithExtensions},
+        extension::{transfer_hook::TransferHook, BaseStateWithExtensions},
         instruction,
         processor::Processor,
     },
     spl_token_client::token::{ExtensionInitializationParams, TokenError as TokenClientError},
     std::{convert::TryInto, sync::Arc},
 };
-
-/// Test program to validate a transfer, conforms to permssioned-transfer
-/// `validate`
-pub fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    input: &[u8],
-) -> ProgramResult {
-    let instruction = PermissionedTransferInstruction::unpack(input)?;
-    let _amount = match instruction {
-        PermissionedTransferInstruction::Validate { amount } => amount,
-        _ => return Err(ProgramError::InvalidInstructionData),
-    };
-    let account_info_iter = &mut accounts.iter();
-
-    let _source_account_info = next_account_info(account_info_iter)?;
-    let mint_info = next_account_info(account_info_iter)?;
-    let _destination_account_info = next_account_info(account_info_iter)?;
-    let _authority_info = next_account_info(account_info_iter)?;
-    let extra_account_metas_info = next_account_info(account_info_iter)?;
-
-    // Only check that the correct pda and account are provided
-    let expected_validation_address = get_extra_account_metas_address(mint_info.key, program_id);
-    if expected_validation_address != *extra_account_metas_info.key {
-        return Err(ProgramError::InvalidSeeds);
-    }
-
-    let data = extra_account_metas_info.try_borrow_data()?;
-    let state = TlvStateBorrowed::unpack(&data).unwrap();
-    let bytes = state.get_bytes::<ExtraAccountMetas>()?;
-    let extra_account_metas = ExtraAccountMetas::unpack(bytes)?;
-
-    // if incorrect number of accounts is provided, error
-    let extra_account_infos = account_info_iter.as_slice();
-    let account_metas = extra_account_metas.data();
-    if extra_account_infos.len() != account_metas.len() {
-        return Err(ProgramError::InvalidInstructionData);
-    }
-
-    // Let's assume that they're provided in the correct order
-    for (i, account_info) in extra_account_infos.iter().enumerate() {
-        if &account_metas[i] != account_info {
-            return Err(ProgramError::InvalidInstructionData);
-        }
-    }
-
-    Ok(())
-}
 
 /// Test program to fail transfer validation, conforms to permssioned-transfer
 /// `validate`
@@ -130,37 +76,16 @@ async fn setup(mint: Keypair, program_id: &Pubkey, authority: &Pubkey) -> TokenC
         processor!(Processor::process),
     );
     program_test.add_program(
-        "my_permissioned_transfer",
+        "my_transfer_hook",
         *program_id,
-        processor!(process_instruction),
+        processor!(spl_transfer_hook_example::processor::process),
     );
-    let account_metas = vec![
-        PodAccountMeta {
-            pubkey: Pubkey::new_unique(),
-            is_signer: false.into(),
-            is_writable: false.into(),
-        },
-        PodAccountMeta {
-            pubkey: Pubkey::new_unique(),
-            is_signer: false.into(),
-            is_writable: false.into(),
-        },
-    ];
-    let mut tlv_data = vec![];
-    tlv_data.extend_from_slice(&(account_metas.len() as u16).to_le_bytes());
-    account_metas
-        .iter()
-        .for_each(|m| tlv_data.extend_from_slice(bytemuck::bytes_of(m)));
-    let mut data = vec![];
-    data.extend_from_slice(ExtraAccountMetas::TYPE.as_ref());
-    data.extend_from_slice(&(tlv_data.len() as u32).to_le_bytes());
-    data.extend_from_slice(&tlv_data);
     let validation_address = get_extra_account_metas_address(&mint.pubkey(), program_id);
     program_test.add_account(
         validation_address,
         Account {
             lamports: 1_000_000_000, // a lot, just to be safe
-            data,
+            data: spl_transfer_hook_example::state::example_data().unwrap(),
             owner: *program_id,
             ..Account::default()
         },
@@ -175,9 +100,9 @@ async fn setup(mint: Keypair, program_id: &Pubkey, authority: &Pubkey) -> TokenC
     context
         .init_token_with_mint_keypair_and_freeze_authority(
             mint,
-            vec![ExtensionInitializationParams::PermissionedTransfer {
+            vec![ExtensionInitializationParams::TransferHook {
                 authority: Some(*authority),
-                permissioned_transfer_program_id: Some(*program_id),
+                program_id: Some(*program_id),
             }],
             None,
         )
@@ -195,7 +120,7 @@ async fn success_init() {
 
     let state = token.get_mint_info().await.unwrap();
     assert!(state.base.is_initialized);
-    let extension = state.get_extension::<PermissionedTransfer>().unwrap();
+    let extension = state.get_extension::<TransferHook>().unwrap();
     assert_eq!(extension.authority, Some(authority).try_into().unwrap());
     assert_eq!(extension.program_id, Some(program_id).try_into().unwrap());
 }
@@ -217,7 +142,7 @@ async fn set_authority() {
             token.get_address(),
             &wrong.pubkey(),
             Some(&new_authority.pubkey()),
-            instruction::AuthorityType::PermissionedTransfer,
+            instruction::AuthorityType::TransferHook,
             &[&wrong],
         )
         .await
@@ -238,13 +163,13 @@ async fn set_authority() {
             token.get_address(),
             &authority.pubkey(),
             Some(&new_authority.pubkey()),
-            instruction::AuthorityType::PermissionedTransfer,
+            instruction::AuthorityType::TransferHook,
             &[&authority],
         )
         .await
         .unwrap();
     let state = token.get_mint_info().await.unwrap();
-    let extension = state.get_extension::<PermissionedTransfer>().unwrap();
+    let extension = state.get_extension::<TransferHook>().unwrap();
     assert_eq!(
         extension.authority,
         Some(new_authority.pubkey()).try_into().unwrap(),
@@ -256,13 +181,13 @@ async fn set_authority() {
             token.get_address(),
             &new_authority.pubkey(),
             None,
-            instruction::AuthorityType::PermissionedTransfer,
+            instruction::AuthorityType::TransferHook,
             &[&new_authority],
         )
         .await
         .unwrap();
     let state = token.get_mint_info().await.unwrap();
-    let extension = state.get_extension::<PermissionedTransfer>().unwrap();
+    let extension = state.get_extension::<TransferHook>().unwrap();
     assert_eq!(extension.authority, None.try_into().unwrap(),);
 
     // fail set again
@@ -271,7 +196,7 @@ async fn set_authority() {
             token.get_address(),
             &new_authority.pubkey(),
             Some(&authority.pubkey()),
-            instruction::AuthorityType::PermissionedTransfer,
+            instruction::AuthorityType::TransferHook,
             &[&new_authority],
         )
         .await
@@ -288,7 +213,7 @@ async fn set_authority() {
 }
 
 #[tokio::test]
-async fn update_permissioned_transfer_program_id() {
+async fn update_transfer_hook_program_id() {
     let authority = Keypair::new();
     let program_id = Pubkey::new_unique();
     let mint_keypair = Keypair::new();
@@ -300,7 +225,7 @@ async fn update_permissioned_transfer_program_id() {
     // fail, wrong signature
     let wrong = Keypair::new();
     let err = token
-        .update_permissioned_transfer_program_id(&wrong.pubkey(), Some(new_program_id), &[&wrong])
+        .update_transfer_hook_program_id(&wrong.pubkey(), Some(new_program_id), &[&wrong])
         .await
         .unwrap_err();
     assert_eq!(
@@ -315,7 +240,7 @@ async fn update_permissioned_transfer_program_id() {
 
     // success
     token
-        .update_permissioned_transfer_program_id(
+        .update_transfer_hook_program_id(
             &authority.pubkey(),
             Some(new_program_id),
             &[&authority],
@@ -323,7 +248,7 @@ async fn update_permissioned_transfer_program_id() {
         .await
         .unwrap();
     let state = token.get_mint_info().await.unwrap();
-    let extension = state.get_extension::<PermissionedTransfer>().unwrap();
+    let extension = state.get_extension::<TransferHook>().unwrap();
     assert_eq!(
         extension.program_id,
         Some(new_program_id).try_into().unwrap(),
@@ -331,11 +256,11 @@ async fn update_permissioned_transfer_program_id() {
 
     // set to none
     token
-        .update_permissioned_transfer_program_id(&authority.pubkey(), None, &[&authority])
+        .update_transfer_hook_program_id(&authority.pubkey(), None, &[&authority])
         .await
         .unwrap();
     let state = token.get_mint_info().await.unwrap();
-    let extension = state.get_extension::<PermissionedTransfer>().unwrap();
+    let extension = state.get_extension::<TransferHook>().unwrap();
     assert_eq!(extension.program_id, None.try_into().unwrap(),);
 }
 
@@ -369,7 +294,7 @@ async fn success_transfer() {
 }
 
 #[tokio::test]
-async fn fail_permissioned_transfer_program() {
+async fn fail_transfer_hook_program() {
     let authority = Pubkey::new_unique();
     let program_id = Pubkey::new_unique();
     let mint = Keypair::new();
@@ -381,22 +306,16 @@ async fn fail_permissioned_transfer_program() {
         processor!(Processor::process),
     );
     program_test.add_program(
-        "my_permissioned_transfer",
+        "my_transfer_hook",
         program_id,
         processor!(process_instruction_fail),
     );
-    let mut tlv_data = vec![];
-    tlv_data.extend_from_slice(&0u16.to_le_bytes());
-    let mut data = vec![];
-    data.extend_from_slice(ExtraAccountMetas::TYPE.as_ref());
-    data.extend_from_slice(&(tlv_data.len() as u32).to_le_bytes());
-    data.extend_from_slice(&tlv_data);
     let validation_address = get_extra_account_metas_address(&mint.pubkey(), &program_id);
     program_test.add_account(
         validation_address,
         Account {
             lamports: 1_000_000_000, // a lot, just to be safe
-            data,
+            data: spl_transfer_hook_example::state::example_data().unwrap(),
             owner: program_id,
             ..Account::default()
         },
@@ -410,9 +329,9 @@ async fn fail_permissioned_transfer_program() {
     context
         .init_token_with_mint_keypair_and_freeze_authority(
             mint,
-            vec![ExtensionInitializationParams::PermissionedTransfer {
+            vec![ExtensionInitializationParams::TransferHook {
                 authority: Some(authority),
-                permissioned_transfer_program_id: Some(program_id),
+                program_id: Some(program_id),
             }],
             None,
         )
