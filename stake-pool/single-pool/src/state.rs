3a98f533dd3bd6974c70c647c500edef14c3e5dc
hanako mumei <81144685+2501babe@users.noreply.github.com>
2023-06-05 19:17:54 -0700

    it helps when you add all the files

---
+++ stake-pool/single-pool/src/state.rs
@@ -0,0 +1,53 @@
+//! State transition types
+
+use {
+    crate::error::SinglePoolError,
+    borsh::{BorshDeserialize, BorshSchema, BorshSerialize},
+    solana_program::{
+        account_info::AccountInfo, borsh::try_from_slice_unchecked, program_error::ProgramError,
+        pubkey::Pubkey,
+    },
+};
+
+/// Single-Validator Stake Pool account type
+#[derive(Clone, Debug, PartialEq, BorshDeserialize, BorshSerialize, BorshSchema)]
+pub enum SinglePoolAccountType {
+    /// Uninitialized account
+    Uninitialized,
+    /// Main pool account
+    Pool,
+}
+// TODO derive default when on a rust version where its stabilized
+impl Default for SinglePoolAccountType {
+    fn default() -> Self {
+        SinglePoolAccountType::Uninitialized
+    }
+}
+
+/// Single-Validator Stake Pool account, used to derive all PDAs
+#[derive(Clone, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize, BorshSchema)]
+pub struct SinglePool {
+    /// Pool account type, reserved for future compat
+    pub account_type: SinglePoolAccountType,
+    /// The vote account this pool is mapped to
+    pub vote_account_address: Pubkey,
+}
+impl SinglePool {
+    /// XXX do this
+    pub fn from_account_info(
+        program_id: &Pubkey,
+        account_info: &AccountInfo,
+    ) -> Result<Self, ProgramError> {
+        if account_info.data_len() == 0 || account_info.owner != program_id {
+            return Err(SinglePoolError::InvalidPoolAccount.into());
+        }
+
+        // FIXME why unchecked????
+        let pool = try_from_slice_unchecked::<SinglePool>(&account_info.data.borrow())?;
+        if pool.account_type != SinglePoolAccountType::Pool {
+            return Err(SinglePoolError::InvalidPoolAccount.into());
+        }
+
+        Ok(pool)
+    }
+}
