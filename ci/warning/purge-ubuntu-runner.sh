066478d180f793e0145078d28e1aa57db7686a6e
Jon Cinque <joncinque@pm.me>
2023-06-08 01:14:27 +0200

    Fix broken purge command

--- ci/warning/purge-ubuntu-runner.sh
+++ ci/warning/purge-ubuntu-runner.sh
@@ -13,30 +13,22 @@ if [[ -n "$CI" ]]; then
     rpm \
     xorriso \
     zsync \
-    clang-6.0 lldb-6.0 lld-6.0 clang-format-6.0 \
-    clang-8 lldb-8 lld-8 clang-format-8 \
-    clang-9 lldb-9 lld-9 clangd-9 clang-format-9 \
     dotnet-sdk-* \
-    esl-erlang \
     firefox \
     g++-8 g++-9 \
-    gfortran-8 gfortran-9 \
+    gfortran-9 \
     google-chrome-stable google-cloud-sdk \
-    ghc-* \
-    cabal-install-* \
-    heroku \
     imagemagick libmagickcore-dev libmagickwand-dev libmagic-dev \
     ant ant-optional \
-    kubectl \
     mercurial mono-complete \
     mysql-client libmysqlclient-dev mysql-server \
-    mssql-tools unixodbc-dev yarn bazel chrpath libssl-dev libxft-dev \
+    mssql-tools unixodbc-dev yarn chrpath libssl-dev libxft-dev \
     libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev \
     php* \
     snmp pollinate \
     libpq-dev postgresql-client \
     powershell ruby-full \
-    sphinxsearch subversion mongodb-org -yq
+    sphinxsearch subversion -yq
   sudo apt-get autopurge -y
   sudo apt-get autoclean -y
 fi
