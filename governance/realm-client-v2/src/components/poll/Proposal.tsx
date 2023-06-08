560846c6071250f0e2a61c30529a70080b728e80
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-06-08 17:08:51 +0100

    fixed margin for header

--- governance/realm-client-v2/src/components/poll/Proposal.tsx
+++ governance/realm-client-v2/src/components/poll/Proposal.tsx
@@ -4,11 +4,11 @@ import React from "react";
 export default function Proposal() {
   return (
     <div className="w-fit p-2 mx-auto my-2 text-white">
-      <div className="flex items-center gap-2 text-sm ">
-        <p className="bg-black p-2 text-white rounded">DAO Name</p>
-        <p className="bg-neutral-800 text-neutral-500 p-2 rounded">
-          Org type: <span className="text-neutral-50">Community Token DAO</span>
-        </p>
+      <div className="flex items-center gap-2 text-sm mb-2 ">
+        <small className="bg-black p-2 text-white rounded">DAO Name</small>
+        <small className="bg-neutral-800 text-neutral-500 p-2 rounded">
+          Org type: <span className="text-neutral-50 ml-1">Community Token DAO</span>
+        </small>
       </div>
       <h1 className="text-4xl font-bold text-white">
         Let&apos;s create a proposal
