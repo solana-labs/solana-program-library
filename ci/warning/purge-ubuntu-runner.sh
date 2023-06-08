73c8c9fe65a3ad81921f204038724f3962262ed9
Jon Cinque <joncinque@pm.me>
2023-06-08 15:51:21 +0200

    Don't purge in the end

--- ci/warning/purge-ubuntu-runner.sh
+++ ci/warning/purge-ubuntu-runner.sh
@@ -10,33 +10,31 @@ if [[ -n "$CI" ]]; then
   sudo docker rmi $(docker image ls -aq)
   # Clears 12GB
   sudo rm -rf /usr/local/lib/android
-  # Clears ?
-  sudo apt update
-  sudo apt install -y shim-signed shim grub2-common grub-efi-amd64-signed
-  sudo apt purge aria2 \
-    ansible \
-    azure-cli \
-    xorriso \
-    dotnet-sdk-* \
-    firefox \
-    g++-9 \
-    gfortran-9 \
-    google-chrome-stable google-cloud-sdk \
-    ant ant-optional \
-    mercurial \
-    mono-complete \
-    mysql-client libmysqlclient-dev mysql-server \
-    mssql-tools unixodbc-dev libxft-dev \
-    libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev \
-    nginx \
-    shim-signed \
-    php* \
-    libpq-dev postgresql-client \
-    powershell \
-    ruby-full \
-    sphinxsearch \
-    subversion \
-    -yq --allow-remove-essential
-  sudo apt autopurge -y
-  sudo apt autoclean -y
+  # Clears 6GB
+  #sudo apt purge aria2 \
+  #  ansible \
+  #  azure-cli \
+  #  xorriso \
+  #  dotnet-sdk-* \
+  #  firefox \
+  #  g++-9 \
+  #  gfortran-9 \
+  #  google-chrome-stable google-cloud-sdk \
+  #  ant ant-optional \
+  #  mercurial \
+  #  mono-complete \
+  #  mysql-client libmysqlclient-dev mysql-server \
+  #  mssql-tools unixodbc-dev libxft-dev \
+  #  libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev \
+  #  nginx \
+  #  shim-signed \
+  #  php* \
+  #  libpq-dev postgresql-client \
+  #  powershell \
+  #  ruby-full \
+  #  sphinxsearch \
+  #  subversion \
+  #  -yq --allow-remove-essential
+  #sudo apt autopurge -y
+  #sudo apt autoclean -y
 fi
