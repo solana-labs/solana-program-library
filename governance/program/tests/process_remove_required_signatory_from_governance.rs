e5158ce5bdacec92073c3c22395487a068f8a7c7
Ellie High <6687206+Wizzeh@users.noreply.github.com>
2023-06-27 16:09:33 -0700

    Add another test to signatory removal

--- governance/program/tests/process_remove_required_signatory_from_governance.rs
+++ governance/program/tests/process_remove_required_signatory_from_governance.rs
@@ -9,6 +9,7 @@ use solana_program::pubkey::Pubkey;
 
 use crate::cookies::{GovernanceCookie, RealmCookie, TokenOwnerRecordCookie};
 use solana_sdk::signature::{Keypair, Signer};
+use spl_governance_tools::error::GovernanceToolsError;
 
 async fn set_up_governance_with_required_signatory(
     governance_test: &mut GovernanceProgramTest,
@@ -161,4 +162,71 @@ async fn test_remove_required_signatory_from_governance() {
         .await;
 
     assert_eq!(0, governance_account.signatories_count);
+}
+
+
+#[tokio::test]
+async fn test_remove_non_existing_required_signatory_from_governance_err() {
+    // Arrange
+    let mut governance_test = GovernanceProgramTest::start_new().await;
+
+    let (token_owner_record_cookie, mut governance_cookie, realm_cookie, signatory) =
+        set_up_governance_with_required_signatory(&mut governance_test).await;
+
+    let mut proposal_cookie = governance_test
+        .with_proposal(&token_owner_record_cookie, &mut governance_cookie)
+        .await
+        .unwrap();
+
+    let beneficiary = Pubkey::new_unique();
+
+    let proposal_transaction_cookie = governance_test
+        .with_remove_governance_required_signatory_transaction(
+            &mut proposal_cookie,
+            &token_owner_record_cookie,
+            &governance_cookie,
+            &Pubkey::new_unique(),
+            &beneficiary,
+        )
+        .await
+        .unwrap();
+
+    governance_test
+        .with_signatory_record_from_governance(
+            &proposal_cookie,
+            &governance_cookie,
+            &signatory.pubkey(),
+        )
+        .await
+        .unwrap();
+
+    governance_test
+        .do_required_signoff(
+            &realm_cookie,
+            &governance_cookie,
+            &proposal_cookie,
+            &signatory,
+        )
+        .await
+        .unwrap();
+
+    governance_test
+        .with_cast_yes_no_vote(&proposal_cookie, &token_owner_record_cookie, YesNoVote::Yes)
+        .await
+        .unwrap();
+
+    // Advance timestamp past hold_up_time
+    governance_test
+        .advance_clock_by_min_timespan(proposal_transaction_cookie.account.hold_up_time as u64)
+        .await;
+
+    // Act
+    let err = governance_test
+        .execute_proposal_transaction(&proposal_cookie, &proposal_transaction_cookie)
+        .await
+        .err()
+        .unwrap();
+
+    // Assert
+    assert_eq!(err, GovernanceToolsError::AccountDoesNotExist.into());
 }
