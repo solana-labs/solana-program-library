f8c7d04914748eb8b7e5adc194911df6ee35a269
Jon Cinque <joncinque@pm.me>
2023-07-05 15:30:43 +0200

    token-2022: Implement `TokenMetadataInstruction::Initialize`

--- token/program-2022/src/extension/token_metadata/processor.rs
+++ token/program-2022/src/extension/token_metadata/processor.rs
@@ -1,18 +1,86 @@
 //! Token-metadata processor
 
 use {
-    solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, msg, pubkey::Pubkey},
-    spl_token_metadata_interface::instruction::{
-        Emit, Initialize, RemoveKey, TokenMetadataInstruction, UpdateAuthority, UpdateField,
+    crate::{
+        check_program_account,
+        error::TokenError,
+        extension::{
+            alloc_and_serialize, metadata_pointer::MetadataPointer, BaseStateWithExtensions,
+            StateWithExtensions,
+        },
+        state::Mint,
+    },
+    solana_program::{
+        account_info::{next_account_info, AccountInfo},
+        entrypoint::ProgramResult,
+        msg,
+        program_error::ProgramError,
+        program_option::COption,
+        pubkey::Pubkey,
+    },
+    spl_token_metadata_interface::{
+        error::TokenMetadataError,
+        instruction::{
+            Emit, Initialize, RemoveKey, TokenMetadataInstruction, UpdateAuthority, UpdateField,
+        },
+        state::{OptionalNonZeroPubkey, TokenMetadata},
     },
 };
 
 /// Processes a [Initialize](enum.TokenMetadataInstruction.html) instruction.
 pub fn process_initialize(
     _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
-    _data: Initialize,
+    accounts: &[AccountInfo],
+    data: Initialize,
 ) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+
+    let metadata_info = next_account_info(account_info_iter)?;
+    let update_authority_info = next_account_info(account_info_iter)?;
+    let mint_info = next_account_info(account_info_iter)?;
+    let mint_authority_info = next_account_info(account_info_iter)?;
+
+    // check that the mint and metadata accounts are the same, since the metadata
+    // extension should only describe itself
+    if metadata_info.key != mint_info.key {
+        msg!("Metadata for a mint must be initialized in the mint itself.");
+        return Err(TokenError::MintMismatch.into());
+    }
+
+    // scope the mint authority check, since the mint is in the same account!
+    {
+        check_program_account(mint_info.owner)?;
+        let mint_data = mint_info.try_borrow_data()?;
+        let mint = StateWithExtensions::<Mint>::unpack(&mint_data)?;
+
+        if !mint_authority_info.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+        if mint.base.mint_authority.as_ref() != COption::Some(mint_authority_info.key) {
+            return Err(TokenMetadataError::IncorrectMintAuthority.into());
+        }
+
+        if mint.get_extension::<MetadataPointer>().is_err() {
+            msg!("A mint with metadata must have the metadata-pointer extension initialized");
+            return Err(TokenError::InvalidExtensionCombination.into());
+        }
+    }
+
+    // Create the token metadata
+    let update_authority = OptionalNonZeroPubkey::try_from(Some(*update_authority_info.key))?;
+    let token_metadata = TokenMetadata {
+        name: data.name,
+        symbol: data.symbol,
+        uri: data.uri,
+        update_authority,
+        mint: *mint_info.key,
+        ..Default::default()
+    };
+
+    // allocate a TLV entry for the space and write it in, assumes that there's
+    // enough SOL for the new rent-exemption
+    alloc_and_serialize::<Mint, _>(metadata_info, &token_metadata, false)?;
+
     Ok(())
 }
 
