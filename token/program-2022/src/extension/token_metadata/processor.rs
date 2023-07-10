e641ca28ec66719ebddaf0af9f0a3d57be92fd7b
Jon Cinque <me@jonc.dev>
2023-07-11 00:08:04 +0200

    token-2022: Implement `TokenMetadataInstruction::UpdateField`

--- token/program-2022/src/extension/token_metadata/processor.rs
+++ token/program-2022/src/extension/token_metadata/processor.rs
@@ -27,6 +27,21 @@ use {
     },
 };
 
+fn check_update_authority(
+    update_authority_info: &AccountInfo,
+    expected_update_authority: &OptionalNonZeroPubkey,
+) -> Result<(), ProgramError> {
+    if !update_authority_info.is_signer {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+    let update_authority = Option::<Pubkey>::from(expected_update_authority.clone())
+        .ok_or(TokenMetadataError::ImmutableMetadata)?;
+    if update_authority != *update_authority_info.key {
+        return Err(TokenMetadataError::IncorrectUpdateAuthority.into());
+    }
+    Ok(())
+}
+
 /// Processes a [Initialize](enum.TokenMetadataInstruction.html) instruction.
 pub fn process_initialize(
     _program_id: &Pubkey,
@@ -89,9 +104,29 @@ pub fn process_initialize(
 /// Processes an [UpdateField](enum.TokenMetadataInstruction.html) instruction.
 pub fn process_update_field(
     _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
-    _data: UpdateField,
+    accounts: &[AccountInfo],
+    data: UpdateField,
 ) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+    let metadata_info = next_account_info(account_info_iter)?;
+    let update_authority_info = next_account_info(account_info_iter)?;
+
+    // deserialize the metadata, but scope the data borrow since we'll probably
+    // realloc the account
+    let mut token_metadata = {
+        let buffer = metadata_info.try_borrow_data()?;
+        let mint = StateWithExtensions::<Mint>::unpack(&buffer)?;
+        mint.get_variable_len_extension::<TokenMetadata>()?
+    };
+
+    check_update_authority(update_authority_info, &token_metadata.update_authority)?;
+
+    // Update the field
+    token_metadata.update(data.field, data.value);
+
+    // Update / realloc the account
+    alloc_and_serialize::<Mint, _>(metadata_info, &token_metadata, true)?;
+
     Ok(())
 }
 
