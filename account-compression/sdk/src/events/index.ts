b48c537c433043dd42e8c54eb74c198373438336
ngundotra <noah@gundotra.org>
2023-07-26 13:49:07 -0400

    remove anchor dependency, bump pkg version

--- account-compression/sdk/src/events/index.ts
+++ account-compression/sdk/src/events/index.ts
@@ -1,4 +1,3 @@
-import { bs58 } from "@project-serum/anchor/dist/cjs/utils/bytes";
 import BN from "bn.js";
 
 import { ChangeLogEventV1 } from "../types";
@@ -52,49 +51,4 @@ export function deserializeApplicationDataEvent(
     default:
       throw Error("Unable to decode buffer as ApplicationDataEvent");
   }
-}
-
-/**
- * Helper function to extract the all ChangeLogEventV1 emitted in a transaction
- * @param txResponse - Transaction response from `@solana/web3.js`
- * @param noopProgramId - program id of the noop program used (default: `SPL_NOOP_PROGRAM_ID`)
- * @returns
- */
-export function getAllChangeLogEventV1FromTransaction(
-  txResponse: TransactionResponse | VersionedTransactionResponse,
-  noopProgramId: PublicKey = SPL_NOOP_PROGRAM_ID
-): ChangeLogEventV1[] {
-  // ensure a transaction response was provided
-  if (!txResponse) throw Error("No txResponse provided");
-
-  // flatten the array of all account keys (e.g. static, readonly, writable)
-  const accountKeys = txResponse.transaction.message
-    .getAccountKeys()
-    .keySegments()
-    .flat();
-
-  let changeLogEvents: ChangeLogEventV1[] = [];
-
-  // locate and parse noop instruction calls via cpi (aka inner instructions)
-  txResponse!.meta?.innerInstructions?.forEach((compiledIx) => {
-    compiledIx.instructions.forEach((innerIx) => {
-      // only attempt to parse noop instructions
-      if (
-        noopProgramId.toBase58() !==
-        accountKeys[innerIx.programIdIndex].toBase58()
-      )
-        return;
-
-      try {
-        // try to deserialize the cpi data as a changelog event
-        changeLogEvents.push(
-          deserializeChangeLogEventV1(Buffer.from(bs58.decode(innerIx.data)))
-        );
-      } catch (__) {
-        // this noop cpi is not a changelog event. do nothing with it.
-      }
-    });
-  });
-
-  return changeLogEvents;
 }
