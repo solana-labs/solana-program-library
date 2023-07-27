e0a761f0b238186372beda6e48d805458ecb309f
Noah Gundotra <ngundotra@users.noreply.github.com>
2023-07-27 10:15:07 -0400

    Add CI workflows for Account Compression PRs (#4858)
    * add ci workflows for account compression PRs
    
    * Update .github/workflows/pull-request-account-compression.yml
    
    Co-authored-by: Jon Cinque <jon@solana.com>
    
    * add reflective path to workflow trigger
    
    * try building w anchor
    
    * trigger on anchor cli version changes too
    
    * make some changes to trigger workflow
    
    * fix artifact path
    
    * make ci js test sh executable
    
    * Update ci/anchor-cliversion.sh
    
    Co-authored-by: Jon Cinque <jon@solana.com>
    
    * Update ci/anchor-cliversion.sh
    
    Co-authored-by: Jon Cinque <jon@solana.com>
    
    * Update ci/anchor-cliversion.sh
    
    Co-authored-by: Jon Cinque <jon@solana.com>
    
    * generalize ./ci/anchor-build.sh
    
    * rename anchor-cliversion to install-anchor
    
    ---------
    
    Co-authored-by: Jon Cinque <jon@solana.com>

---
+++ ci/install-anchor.sh
@@ -0,0 +1,32 @@
+#
+# This file maintains the solana versions for use by CI.
+#
+# Obtain the environment variables without any automatic updating:
+#   $ source ci/install-anchor.sh
+#
+# Obtain the environment variables and install update:
+#   $ source ci/install-anchor.sh install
+
+# Then to access the anchor version:
+#   $ echo "$anchor_cli_version"
+#
+
+if [[ -n $ANCHOR_CLI_VERSION ]]; then
+  anchor_cli_version="$ANCHOR_CLI_VERSION"
+else
+  anchor_cli_version=v0.28.0
+fi
+
+export anchor_cli_version="$anchor_cli_version"
+
+if [[ -n $1 ]]; then
+  case $1 in
+  install)
+    cargo install --git https://github.com/coral-xyz/anchor --tag $anchor_cli_version anchor-cli --locked
+    anchor --version
+    ;;
+  *)
+    echo "anchor-version.sh: Note: ignoring unknown argument: $1" >&2
+    ;;
+  esac
+fi
