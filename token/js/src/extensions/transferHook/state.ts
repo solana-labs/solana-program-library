80180280141ed450fabbe99a396ff8d32ffb3a15
Wilhelm Thieme <wjthieme@gmail.com>
2023-08-24 14:47:27 +0200

    Added the TransferHookAccount extension type

--- token/js/src/extensions/transferHook/state.ts
+++ token/js/src/extensions/transferHook/state.ts
@@ -2,7 +2,8 @@ import { struct } from '@solana/buffer-layout';
 import type { Mint } from '../../state/mint.js';
 import { ExtensionType, getExtensionData } from '../extensionType.js';
 import type { PublicKey } from '@solana/web3.js';
-import { publicKey } from '@solana/buffer-layout-utils';
+import { bool, publicKey } from '@solana/buffer-layout-utils';
+import type { Account } from '../../state/account.js';
 
 /** TransferHook as stored by the program */
 export interface TransferHook {
@@ -12,7 +13,7 @@ export interface TransferHook {
     programId: PublicKey;
 }
 
-/** Buffer layout for de/serializing a transfer fee config extension */
+/** Buffer layout for de/serializing a transfer hook extension */
 export const TransferHookLayout = struct<TransferHook>([publicKey('authority'), publicKey('programId')]);
 
 export const TRANSFER_HOOK_SIZE = TransferHookLayout.span;
@@ -21,6 +22,29 @@ export function getTransferHook(mint: Mint): TransferHook | null {
     const extensionData = getExtensionData(ExtensionType.TransferHook, mint.tlvData);
     if (extensionData !== null) {
         return TransferHookLayout.decode(extensionData);
+    } else {
+        return null;
+    }
+}
+
+/** TransferHookAccount as stored by the program */
+export interface TransferHookAccount {
+    /**
+     * Whether or not this account is currently tranferring tokens
+     * True during the transfer hook cpi, otherwise false
+     */
+    transferring: boolean;
+}
+
+/** Buffer layout for de/serializing a transfer hook account extension */
+export const TransferHookAccountLayout = struct<TransferHookAccount>([bool('transferring')]);
+
+export const TRANSFER_HOOK_ACCOUNT_SIZE = TransferHookAccountLayout.span;
+
+export function getTransferHookAccount(account: Account): TransferHookAccount | null {
+    const extensionData = getExtensionData(ExtensionType.TransferHookAccount, account.tlvData);
+    if (extensionData !== null) {
+        return TransferHookAccountLayout.decode(extensionData);
     } else {
         return null;
     }
