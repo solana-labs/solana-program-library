96265929d928ada7121f69b34155acaba7caf408
samkim-crypto <skim13@cs.stanford.edu>
2023-09-12 12:07:25 -0700

    add main logic for proof generation in client

--- token/client/src/lib.rs
+++ token/client/src/lib.rs
@@ -3,4 +3,10 @@ pub mod client;
 pub mod output;
 pub mod token;
 
+/// Helper functions to generate split zero-knowledge proofs for confidential transfers.
+///
+/// The logic in this submodule should belong to the `solana-zk-token-sdk` and will be removed with
+/// an upgrade to the Solana program in the future.
+pub mod proof_generation;
+
 pub use spl_token_2022;
