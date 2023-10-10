07dc52e822eb7fa1f77ce90fa99e83314a78637e
hanako mumei <81144685+2501babe@users.noreply.github.com>
2023-10-10 04:36:57 -0700

    single-pool: add reactivate instruction

---
+++ single-pool/program/tests/reactivate.rs
@@ -0,0 +1,149 @@
+#![allow(clippy::integer_arithmetic)]
+#![cfg(feature = "test-sbf")]
+
+mod helpers;
+
+use {
+    bincode::serialize,
+    helpers::*,
+    solana_program_test::*,
+    solana_sdk::{
+        account::AccountSharedData,
+        signature::Signer,
+        stake::state::{Delegation, Stake, StakeState},
+        transaction::Transaction,
+    },
+    spl_single_validator_pool::{error::SinglePoolError, id, instruction},
+    test_case::test_case,
+};
+
+#[tokio::test]
+async fn success() {
+    let mut context = program_test().start_with_context().await;
+    let accounts = SinglePoolAccounts::default();
+    accounts
+        .initialize_for_deposit(&mut context, TEST_STAKE_AMOUNT, None)
+        .await;
+    advance_epoch(&mut context).await;
+
+    // deactivate the pool stake account
+    let (meta, stake, _) =
+        get_stake_account(&mut context.banks_client, &accounts.stake_account).await;
+    let delegation = Delegation {
+        activation_epoch: 0,
+        deactivation_epoch: 0,
+        ..stake.unwrap().delegation
+    };
+    let account_data = serialize(&StakeState::Stake(
+        meta,
+        Stake {
+            delegation,
+            ..stake.unwrap()
+        },
+    ))
+    .unwrap();
+
+    let mut stake_account = get_account(&mut context.banks_client, &accounts.stake_account).await;
+    stake_account.data = account_data;
+    context.set_account(
+        &accounts.stake_account,
+        &AccountSharedData::from(stake_account),
+    );
+
+    // make sure deposit fails
+    let instructions = instruction::deposit(
+        &id(),
+        &accounts.pool,
+        &accounts.alice_stake.pubkey(),
+        &accounts.alice_token,
+        &accounts.alice.pubkey(),
+        &accounts.alice.pubkey(),
+    );
+    let transaction = Transaction::new_signed_with_payer(
+        &instructions,
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &accounts.alice],
+        context.last_blockhash,
+    );
+
+    let e = context
+        .banks_client
+        .process_transaction(transaction)
+        .await
+        .unwrap_err();
+    check_error(e, SinglePoolError::WrongStakeState);
+
+    // reactivate
+    let instruction = instruction::reactivate_pool(&id(), &accounts.vote_account.pubkey());
+    let transaction = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    context
+        .banks_client
+        .process_transaction(transaction)
+        .await
+        .unwrap();
+
+    advance_epoch(&mut context).await;
+
+    // deposit works again
+    let instructions = instruction::deposit(
+        &id(),
+        &accounts.pool,
+        &accounts.alice_stake.pubkey(),
+        &accounts.alice_token,
+        &accounts.alice.pubkey(),
+        &accounts.alice.pubkey(),
+    );
+    let transaction = Transaction::new_signed_with_payer(
+        &instructions,
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &accounts.alice],
+        context.last_blockhash,
+    );
+
+    context
+        .banks_client
+        .process_transaction(transaction)
+        .await
+        .unwrap();
+
+    assert!(context
+        .banks_client
+        .get_account(accounts.alice_stake.pubkey())
+        .await
+        .expect("get_account")
+        .is_none());
+}
+
+#[test_case(true; "activated")]
+#[test_case(false; "activating")]
+#[tokio::test]
+async fn fail_not_deactivated(activate: bool) {
+    let mut context = program_test().start_with_context().await;
+    let accounts = SinglePoolAccounts::default();
+    accounts.initialize(&mut context).await;
+
+    if activate {
+        advance_epoch(&mut context).await;
+    }
+
+    let instruction = instruction::reactivate_pool(&id(), &accounts.vote_account.pubkey());
+    let transaction = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    let e = context
+        .banks_client
+        .process_transaction(transaction)
+        .await
+        .unwrap_err();
+    check_error(e, SinglePoolError::WrongStakeState);
+}
