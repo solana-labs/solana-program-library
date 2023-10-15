e1481be23e93c4c59aa19fe889d8b0f9641d3699
Joe <jcaulfield135@gmail.com>
2023-10-15 19:32:57 +0200

    feature gate: add activate instruction

--- feature-gate/program/src/instruction.rs
+++ feature-gate/program/src/instruction.rs
@@ -3,9 +3,12 @@
 use {
     num_enum::{IntoPrimitive, TryFromPrimitive},
     solana_program::{
+        feature::Feature,
         instruction::{AccountMeta, Instruction},
         program_error::ProgramError,
         pubkey::Pubkey,
+        rent::Rent,
+        system_instruction, system_program,
     },
 };
 
@@ -13,6 +16,19 @@ use {
 #[derive(Clone, Debug, PartialEq, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
 pub enum FeatureGateInstruction {
+    /// Submit a feature for activation.
+    ///
+    /// Note: This instruction expects the account to exist and be owned by the
+    /// system program. The account should also have enough rent-exempt lamports
+    /// to cover the cost of the account creation for a
+    /// `solana_program::feature::Feature` state prior to invoking this
+    /// instruction.
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    ///   0. `[w+s]`    Feature account (must be a system account)
+    ///   1. `[]`       System program
+    ActivateFeature,
     /// Revoke a pending feature activation.
     ///
     /// A "pending" feature activation is a feature account that has been
@@ -44,6 +60,36 @@ impl FeatureGateInstruction {
     }
 }
 
+/// Creates an 'ActivateFeature' instruction.
+pub fn activate_feature(feature_id: &Pubkey) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(*feature_id, true),
+        AccountMeta::new_readonly(system_program::id(), false),
+    ];
+
+    let data = FeatureGateInstruction::ActivateFeature.pack();
+
+    Instruction {
+        program_id: crate::id(),
+        accounts,
+        data,
+    }
+}
+
+/// Creates a set of two instructions:
+///   * One to fund the feature account with rent-exempt lamports
+///   * Another is the Feature Gate Program's 'ActivateFeature' instruction
+pub fn activate_feature_with_rent_transfer(
+    feature_id: &Pubkey,
+    payer: &Pubkey,
+) -> [Instruction; 2] {
+    let lamports = Rent::default().minimum_balance(Feature::size_of());
+    [
+        system_instruction::transfer(payer, feature_id, lamports),
+        activate_feature(feature_id),
+    ]
+}
+
 /// Creates a 'RevokePendingActivation' instruction.
 pub fn revoke_pending_activation(feature_id: &Pubkey, destination: &Pubkey) -> Instruction {
     let accounts = vec![
@@ -68,6 +114,11 @@ mod test {
         let packed = instruction.pack();
         let unpacked = FeatureGateInstruction::unpack(&packed).unwrap();
         assert_eq!(instruction, &unpacked);
+    }
+
+    #[test]
+    fn test_pack_unpack_activate_feature() {
+        test_pack_unpack(&FeatureGateInstruction::ActivateFeature);
     }
 
     #[test]
