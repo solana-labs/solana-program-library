d1f15b65063becd2b77b43a020b4e226097143f7
Jon C <me@jonc.dev>
2023-11-08 21:56:04 +0100

    Move token-swap tests to mocha

---
+++ token-swap/js/test/main.test.ts
@@ -0,0 +1,37 @@
+import {
+  createAccountAndSwapAtomic,
+  createTokenSwap,
+  swap,
+  depositAllTokenTypes,
+  withdrawAllTokenTypes,
+  depositSingleTokenTypeExactAmountIn,
+  withdrawSingleTokenTypeExactAmountOut,
+} from './token-swap-test';
+import {CurveType} from '../src';
+
+describe('spl-token-swap instructions', () => {
+  it('executes properly', async () => {
+    // These test cases are designed to run sequentially and in the following order
+    console.log('Run test: createTokenSwap (constant price)');
+    const constantPrice = new Uint8Array(8);
+    constantPrice[0] = 1;
+    await createTokenSwap(CurveType.ConstantPrice, constantPrice);
+    console.log(
+      'Run test: createTokenSwap (constant product, used further in tests)',
+    );
+    await createTokenSwap(CurveType.ConstantProduct);
+    console.log('Run test: deposit all token types');
+    await depositAllTokenTypes();
+    console.log('Run test: withdraw all token types');
+    await withdrawAllTokenTypes();
+    console.log('Run test: swap');
+    await swap();
+    console.log('Run test: create account, approve, swap all at once');
+    await createAccountAndSwapAtomic();
+    console.log('Run test: deposit one exact amount in');
+    await depositSingleTokenTypeExactAmountIn();
+    console.log('Run test: withrdaw one exact amount out');
+    await withdrawSingleTokenTypeExactAmountOut();
+    console.log('Success\n');
+  });
+});
