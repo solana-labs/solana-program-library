3410189ec2d500f14125fe439e2ea64813be0c06
Jon C <me@jonc.dev>
2023-11-08 14:20:03 +0100

    stake-pool: Update to pnpm

--- ci/js-test-stake-pool.sh
+++ ci/js-test-stake-pool.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/.."
 source ./ci/solana-version.sh install
 
 cd stake-pool/js
-npm install
-npm run lint
-npm run build
-npm run test
+pnpm install
+pnpm lint
+pnpm build
+pnpm test
