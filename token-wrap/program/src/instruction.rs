247d65d8df1c407a8dd50de256130de64a2fc20d
stacc <jarettrsdunn+git@gmail.com>
2023-11-11 15:34:16 +0000

    untested, commiting to discuss with cinque

--- token-wrap/program/src/instruction.rs
+++ token-wrap/program/src/instruction.rs
@@ -1,7 +1,12 @@
 //! Program instructions
-
 use num_enum::{IntoPrimitive, TryFromPrimitive};
-
+use solana_program::program_error::ProgramError;
+use std::convert::{TryFrom, TryInto};
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+};
+use borsh::{BorshSerialize, BorshDeserialize};
 /// Instructions supported by the Token Wrap program
 #[derive(Clone, Debug, PartialEq, TryFromPrimitive, IntoPrimitive)]
 #[repr(u8)]
@@ -78,4 +83,292 @@ pub enum TokenWrapInstruction {
     /// Data expected by this instruction:
     ///   * little-endian u64 representing the amount to unwrap
     Unwrap,
+}
+
+/// Creates a 'transfer' instruction.
+pub fn transfer(
+    program_id: &Pubkey,
+    from_pubkey: &Pubkey,
+    to_pubkey: &Pubkey,
+    authority_pubkey: &Pubkey,
+    amount: u64,
+) -> Result<Instruction, ProgramError> {
+    let data = TransferData { amount }.amount.try_to_vec()?;
+
+    let accounts = vec![
+        AccountMeta::new(*from_pubkey, false),
+        AccountMeta::new(*to_pubkey, false),
+        AccountMeta::new_readonly(*authority_pubkey, true),
+    ];
+
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Transfer instruction data
+#[repr(C)]
+pub struct TransferData {
+    /// Amount of the token to transfer
+    pub amount: u64,
+}
+
+/// Creates a 'mint_to' instruction.
+pub fn mint_to(
+    program_id: &Pubkey,
+    mint_pubkey: &Pubkey,
+    recipient_pubkey: &Pubkey,
+    mint_authority_pubkey: &Pubkey,
+    amount: u64,
+) -> Result<Instruction, ProgramError> {
+    let data = MintToData { amount }.amount.try_to_vec()?;
+
+    let accounts = vec![
+        AccountMeta::new(*mint_pubkey, false),
+        AccountMeta::new(*recipient_pubkey, false),
+        AccountMeta::new_readonly(*mint_authority_pubkey, true),
+    ];
+
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// MintTo instruction data
+#[repr(C)]
+pub struct MintToData {
+    /// Amount of the token to mint
+    pub amount: u64,
+}
+/// Creates a 'burn' instruction.
+pub fn burn(
+    program_id: &Pubkey,
+    burn_account_pubkey: &Pubkey,
+    mint_pubkey: &Pubkey,
+    burn_authority_pubkey: &Pubkey,
+    amount: u64,
+) -> Result<Instruction, ProgramError> {
+    let data = BurnData { amount }.amount.try_to_vec()?;
+
+    let accounts = vec![
+        AccountMeta::new(*burn_account_pubkey, false),
+        AccountMeta::new(*mint_pubkey, false),
+        AccountMeta::new_readonly(*burn_authority_pubkey, true),
+    ];
+
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Burn instruction data
+#[repr(C)]
+pub struct BurnData {
+    /// Amount of the token to burn
+    pub amount: u64,
+}
+
+
+/// Creates a 'create_mint' instruction.
+pub fn create_mint(
+    program_id: &Pubkey,
+    funder_pubkey: &Pubkey,
+    wrapped_mint_pubkey: &Pubkey,
+    backpointer_pubkey: &Pubkey,
+    unwrapped_mint_pubkey: &Pubkey,
+    system_program_id: &Pubkey,
+    wrapped_token_program_id: &Pubkey,
+    idempotent: bool,
+) -> Result<Instruction, ProgramError> {
+    let data = CreateMintData { idempotent }.try_to_vec()?;
+
+    let accounts = vec![
+        AccountMeta::new(*funder_pubkey, true),
+        AccountMeta::new(*wrapped_mint_pubkey, false),
+        AccountMeta::new(*backpointer_pubkey, false),
+        AccountMeta::new_readonly(*unwrapped_mint_pubkey, false),
+        AccountMeta::new_readonly(*system_program_id, false),
+        AccountMeta::new_readonly(*wrapped_token_program_id, false),
+    ];
+
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Creates a 'wrap' instruction.
+pub fn wrap(
+    program_id: &Pubkey,
+    unwrapped_token_account_pubkey: &Pubkey,
+    escrow_pubkey: &Pubkey,
+    unwrapped_mint_pubkey: &Pubkey,
+    wrapped_mint_pubkey: &Pubkey,
+    recipient_pubkey: &Pubkey,
+    escrow_authority_pubkey: &Pubkey,
+    wrapped_token_program_id: &Pubkey,
+    transfer_authority_pubkey: &Pubkey,
+    amount: u64,
+) -> Result<Instruction, ProgramError> {
+    let data = WrapData { amount }.try_to_vec()?;
+
+    let accounts = vec![
+        AccountMeta::new(*unwrapped_token_account_pubkey, false),
+        AccountMeta::new(*escrow_pubkey, false),
+        AccountMeta::new_readonly(*unwrapped_mint_pubkey, false),
+        AccountMeta::new(*wrapped_mint_pubkey, false),
+        AccountMeta::new(*recipient_pubkey, false),
+        AccountMeta::new_readonly(*escrow_authority_pubkey, false),
+        AccountMeta::new_readonly(*wrapped_token_program_id, false),
+        AccountMeta::new_readonly(*transfer_authority_pubkey, true),
+    ];
+    Ok( Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })  
+}
+
+/// Creates an 'unwrap' instruction.
+pub fn unwrap(
+    program_id: &Pubkey,
+    wrapped_token_account_pubkey: &Pubkey,
+    wrapped_mint_pubkey: &Pubkey,
+    escrow_pubkey: &Pubkey,
+    recipient_pubkey: &Pubkey,
+    unwrapped_mint_pubkey: &Pubkey,
+    escrow_authority_pubkey: &Pubkey,
+    wrapped_token_program_id: &Pubkey,
+    transfer_authority_pubkey: &Pubkey,
+    amount: u64,
+) -> Result<Instruction, ProgramError> {
+    let data = UnwrapData { amount }.try_to_vec()?;
+
+    let accounts = vec![
+        AccountMeta::new(*wrapped_token_account_pubkey, false),
+        AccountMeta::new(*wrapped_mint_pubkey, false),
+        AccountMeta::new(*escrow_pubkey, false),
+        AccountMeta::new(*recipient_pubkey, false),
+        AccountMeta::new_readonly(*unwrapped_mint_pubkey, false),
+        AccountMeta::new_readonly(*escrow_authority_pubkey, false),
+        AccountMeta::new_readonly(*wrapped_token_program_id, false),
+        AccountMeta::new_readonly(*transfer_authority_pubkey, true),
+    ];
+    burn(wrapped_token_program_id, wrapped_token_account_pubkey, wrapped_mint_pubkey, transfer_authority_pubkey, amount)?;
+    transfer(program_id, escrow_pubkey, recipient_pubkey, transfer_authority_pubkey, amount)?;
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create Mint instruction data
+#[repr(C)]
+pub struct CreateMintData {
+    /// Whether the creation is idempotent
+    pub idempotent: bool,
+}
+impl BorshSerialize for CreateMintData {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        self.idempotent.serialize(writer)
+    }
+}
+impl BorshDeserialize for CreateMintData {
+    fn deserialize(buf: &mut &[u8]) -> std::io::Result<Self> {
+        let idempotent = bool::deserialize(buf)?;
+        Ok(Self { idempotent })
+    }
+}
+/// Wrap instruction data
+#[repr(C)]
+pub struct WrapData {
+    /// Amount of the token to wrap
+    pub amount: u64,
+}
+
+
+impl BorshSerialize for WrapData {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        self.amount.serialize(writer)
+    }
+}
+
+impl BorshDeserialize for WrapData {
+    fn deserialize(buf: &mut &[u8]) -> std::io::Result<Self> {
+        let amount = u64::deserialize(buf)?;
+        Ok(Self { amount })
+    }
+}
+
+/// Unwrap instruction data
+#[repr(C)]
+pub struct UnwrapData {
+    /// Amount of the token to unwrap
+    pub amount: u64,
+}
+
+impl BorshSerialize for UnwrapData {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        self.amount.serialize(writer)
+    }
+}
+impl BorshDeserialize for UnwrapData {
+    fn deserialize(buf: &mut &[u8]) -> std::io::Result<Self> {
+        let amount = u64::deserialize(buf)?;
+        Ok(Self { amount })
+    }
+}
+
+impl TryFrom<&[u8]> for CreateMintData {
+    type Error = ProgramError;
+
+    fn try_from(slice: &[u8]) -> Result<Self, Self::Error> {
+        if slice.len() != 1 {
+            return Err(ProgramError::InvalidInstructionData);
+        }
+        let idempotent = match slice[0] {
+            0 => false,
+            1 => true,
+            _ => return Err(ProgramError::InvalidInstructionData),
+        };
+        Ok(CreateMintData { idempotent })
+    }
+}
+
+impl TryFrom<&[u8]> for WrapData {
+    type Error = ProgramError;
+
+    fn try_from(slice: &[u8]) -> Result<Self, Self::Error> {
+        if slice.len() != 8 {
+            return Err(ProgramError::InvalidInstructionData);
+        }
+        let amount = slice
+            .try_into()
+            .map(u64::from_le_bytes)
+            .map_err(|_| ProgramError::InvalidInstructionData)?;
+        Ok(WrapData { amount })
+    }
+}
+
+impl TryFrom<&[u8]> for UnwrapData {
+    type Error = ProgramError;
+
+    fn try_from(slice: &[u8]) -> Result<Self, Self::Error> {
+        if slice.len() != 8 {
+            return Err(ProgramError::InvalidInstructionData);
+        }
+        let amount = slice
+            .try_into()
+            .map(u64::from_le_bytes)
+            .map_err(|_| ProgramError::InvalidInstructionData)?;
+        Ok(UnwrapData { amount })
+    }
 }
--- token-wrap/program/src/processor.rs
+++ token-wrap/program/src/processor.rs
@@ -1,5 +1,9 @@
 //! Program state processor
 
+use solana_program::program::{invoke, invoke_signed};
+
+use crate::{instruction::{wrap, transfer, mint_to, create_mint, burn}, get_wrapped_mint_backpointer_address, get_wrapped_mint_authority_with_seed, get_wrapped_mint_address};
+
 use {
     crate::instruction::TokenWrapInstruction,
     solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey},
@@ -8,19 +12,208 @@ use {
 
 /// Instruction processor
 pub fn process_instruction(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
+    _program_account: &Pubkey,
+    accounts: &[AccountInfo],
     input: &[u8],
 ) -> ProgramResult {
     match decode_instruction_type(input)? {
         TokenWrapInstruction::CreateMint => {
-            unimplemented!();
+            let program_account = &accounts[0];
+            let mint_account = &accounts[1];
+            let mint_authority_account = &accounts[2];
+            let freeze_authority_account = &accounts[3];
+            let token_program_account = &accounts[4];
+            let wrapped_mint_account = &accounts[5];
+            let unwrapped_mint_account = &accounts[6];
+            let system_program_account = &accounts[7];
+            let wrapped_token_program_account = &accounts[8];
+            let backpointer_pubkey = get_wrapped_mint_backpointer_address(wrapped_mint_account.key);
+
+            assert!(mint_account.key == &get_wrapped_mint_address(unwrapped_mint_account.key,
+                wrapped_token_program_account.key));
+
+            
+
+            let idempotent: bool = input[1] == 1;
+            //program_id, funder_pubkey, wrapped_mint_pubkey, backpointer_pubkey, unwrapped_mint_pubkey, system_program_id, wrapped_token_program_id, idempotent)
+            let ix = create_mint(
+                program_account.key,
+                program_account.key,
+                mint_account.key,
+                &backpointer_pubkey,
+                unwrapped_mint_account.key,
+                system_program_account.key,
+                wrapped_token_program_account.key,
+                idempotent,
+            )?;
+            invoke(&ix, &[
+                mint_account.clone(),
+                mint_authority_account.clone(),
+                freeze_authority_account.clone(),
+                token_program_account.clone(),
+            ])?;
+            Ok(())
         }
         TokenWrapInstruction::Wrap => {
-            unimplemented!();
+            let program_account = &accounts[0];
+            let unwrapped_token_account_account = &accounts[1];
+            let escrow_account = &accounts[2];
+            let unwrapped_mint_account = &accounts[3];
+            let wrapped_mint_account = &accounts[4];
+            let recipient_account = &accounts[5];
+            let escrow_authority_account = &accounts[6];
+            let wrapped_token_program_account = &accounts[7];
+            let transfer_authority_account = &accounts[8];
+            let unwrapped_token_program_account: &AccountInfo<'_> = &accounts[9];
+            let token_program_account = &accounts[10];
+            
+            
+            assert!(wrapped_mint_account.key == &get_wrapped_mint_address(unwrapped_mint_account.key,
+                wrapped_token_program_account.key));
+            assert!(wrapped_mint_account.owner == wrapped_token_program_account.key);
+            assert!(escrow_account.owner == &get_wrapped_mint_authority_with_seed(wrapped_mint_account.key).0);
+            
+            // ? breaking: fix this
+            let amount: u64 = (&input[1..9]).into_iter().rev().fold(0, |acc, &x| (acc << 8) + x as u64);
+            let ix1 = transfer(unwrapped_token_program_account.key,
+                 unwrapped_token_account_account.key,
+                  escrow_account.key,
+                   transfer_authority_account.key,
+                    amount)?;
+            let ix2 = mint_to(wrapped_token_program_account.key, 
+                wrapped_mint_account.key, 
+                recipient_account.key, 
+                transfer_authority_account.key, 
+                amount)?;
+            invoke(&ix1, 
+                &[
+                    unwrapped_token_account_account.clone(),
+                    escrow_account.clone(),
+                    transfer_authority_account.clone(),
+                    unwrapped_token_program_account.clone(),
+                
+                ]
+            )?;
+            invoke(&ix2, 
+                &[
+                    wrapped_mint_account.clone(),
+                    recipient_account.clone(),
+                    transfer_authority_account.clone(),
+                    token_program_account.clone(),
+                
+                ]
+            )?;
+            let ix3 = wrap(
+                program_account.key,
+                unwrapped_token_program_account.key,
+                escrow_account.key,
+                unwrapped_mint_account.key,
+                wrapped_mint_account.key,
+                recipient_account.key,
+                escrow_authority_account.key,
+                wrapped_token_program_account.key,
+                transfer_authority_account.key,
+                amount
+            )?;
+            invoke(&ix3, 
+                &[
+                    program_account.clone(),
+                    unwrapped_token_account_account.clone(),
+                    escrow_account.clone(),
+                    unwrapped_mint_account.clone(),
+                    wrapped_mint_account.clone(),
+                    recipient_account.clone(),
+                    escrow_authority_account.clone(),
+                    wrapped_token_program_account.clone(),
+                    transfer_authority_account.clone()
+                
+                ]
+            )?;
+            Ok(())
         }
         TokenWrapInstruction::Unwrap => {
-            unimplemented!();
+                    
+            let program_account = &accounts[0];
+            let unwrapped_token_account_account = &accounts[1];
+            let escrow_account = &accounts[2];
+            let unwrapped_mint_account = &accounts[3];
+            let wrapped_mint_account = &accounts[4];
+            let recipient_account = &accounts[5];
+            let escrow_authority_account = &accounts[6];
+            let wrapped_token_program_account = &accounts[7];
+            let transfer_authority_account = &accounts[8];
+            let wrapped_token_account_account = &accounts[9];
+            let token_program_account = &accounts[10];
+            let unwrapped_token_program_account: &AccountInfo<'_> = &accounts[11];
+
+            assert!(wrapped_mint_account.key == &get_wrapped_mint_address(unwrapped_mint_account.key,
+                wrapped_token_program_account.key));
+            assert!(wrapped_mint_account.owner == wrapped_token_program_account.key);
+            assert!(escrow_account.owner == &get_wrapped_mint_authority_with_seed(wrapped_mint_account.key).0);
+
+            // ? breaking: fix this
+            let amount: u64 = (&input[1..9]).into_iter().rev().fold(0, |acc, &x| (acc << 8) + x as u64);
+            //program_id, burn_account_pubkey, mint_pubkey, burn_authority_pubkey, amount)
+            let ix1 = burn(
+                wrapped_token_program_account.key,
+                wrapped_token_account_account.key,
+                wrapped_mint_account.key,
+                escrow_authority_account.key,
+                amount
+            )?;
+            let ix2 = transfer(
+                unwrapped_token_program_account.key,
+                escrow_account.key,
+                unwrapped_token_account_account.key,
+                escrow_authority_account.key,
+                amount
+            )?;
+            let signer_seeds = get_wrapped_mint_authority_with_seed(wrapped_mint_account.key);
+            invoke_signed(&ix1, 
+                &[
+                    wrapped_token_account_account.clone(),
+                    wrapped_mint_account.clone(),
+                    transfer_authority_account.clone(),
+                    token_program_account.clone(),
+                ],
+                &[&[&[signer_seeds.1]]]
+            )?;
+            invoke_signed(&ix2, 
+                &[
+                    unwrapped_mint_account.clone(),
+                    recipient_account.clone(),
+                    transfer_authority_account.clone(),
+                    token_program_account.clone(),
+                ],
+                &[&[&[signer_seeds.1]]]
+            )?;
+            let ix3 = wrap(
+                program_account.key,
+                unwrapped_token_program_account.key,
+                escrow_account.key,
+                unwrapped_mint_account.key,
+                wrapped_mint_account.key,
+                recipient_account.key,
+                escrow_authority_account.key,
+                wrapped_token_program_account.key,
+                transfer_authority_account.key,
+                amount
+            )?;
+            invoke(&ix3, 
+                &[
+                    program_account.clone(),
+                    unwrapped_token_account_account.clone(),
+                    escrow_account.clone(),
+                    unwrapped_mint_account.clone(),
+                    wrapped_mint_account.clone(),
+                    recipient_account.clone(),
+                    escrow_authority_account.clone(),
+                    wrapped_token_program_account.clone(),
+                    transfer_authority_account.clone()
+                
+                ]
+            )?;
+            Ok(())
         }
     }
 }
