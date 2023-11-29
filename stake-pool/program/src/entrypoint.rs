dc754aa4d0cb0044d265888c26dffcef2ea9ac6c
Jon Cinque <me@jonc.dev>
2023-11-29 22:54:01 +0100

    stake-pool: Add security.txt (#5929)
    * stake-pool: Add security.txt
    
    * Update stake-pool/program/src/entrypoint.rs
    
    Co-authored-by: Trent Nelson <trent.a.b.nelson@gmail.com>
    
    ---------
    
    Co-authored-by: Trent Nelson <trent.a.b.nelson@gmail.com>

--- stake-pool/program/src/entrypoint.rs
+++ stake-pool/program/src/entrypoint.rs
@@ -8,6 +8,7 @@ use {
         account_info::AccountInfo, entrypoint::ProgramResult, program_error::PrintProgramError,
         pubkey::Pubkey,
     },
+    solana_security_txt::security_txt,
 };
 
 solana_program::entrypoint!(process_instruction);
@@ -23,4 +24,19 @@ fn process_instruction(
     } else {
         Ok(())
     }
+}
+
+security_txt! {
+    // Required fields
+    name: "SPL Stake Pool",
+    project_url: "https://spl.solana.com/stake-pool",
+    contacts: "link:https://github.com/solana-labs/solana-program-library/security/advisories/new,mailto:security@solana.com,discord:https://solana.com/discord",
+    policy: "https://github.com/solana-labs/solana-program-library/blob/master/SECURITY.md",
+
+    // Optional Fields
+    preferred_languages: "en",
+    source_code: "https://github.com/solana-labs/solana-program-library/tree/master/stake-pool/program",
+    source_revision: "58c1226a513d3d8bb2de8ec67586a679be7fd2d4",
+    source_release: "stake-pool-v0.6.4",
+    auditors: "https://github.com/solana-labs/security-audits#stake-pool"
 }
