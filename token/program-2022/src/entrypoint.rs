cc47a99acb3269d19a4e0a5a3b46b3c8ea99bbe0
Jon C <me@jonc.dev>
2023-11-29 18:42:03 +0100

    token-2022: Add security.txt support

--- token/program-2022/src/entrypoint.rs
+++ token/program-2022/src/entrypoint.rs
@@ -6,6 +6,7 @@ use {
         account_info::AccountInfo, entrypoint::ProgramResult, program_error::PrintProgramError,
         pubkey::Pubkey,
     },
+    solana_security_txt::security_txt,
 };
 
 solana_program::entrypoint!(process_instruction);
@@ -20,4 +21,19 @@ fn process_instruction(
         return Err(error);
     }
     Ok(())
+}
+
+security_txt! {
+    // Required fields
+    name: "SPL Token-2022",
+    project_url: "https://spl.solana.com/token-2022",
+    contacts: "link:https://github.com/solana-labs/solana-program-library/security/advisories/new,mailto:security@solana.com,discord:https://discord.gg/solana",
+    policy: "https://github.com/solana-labs/solana-program-library/blob/master/SECURITY.md",
+
+    // Optional Fields
+    preferred_languages: "en",
+    source_code: "https://github.com/solana-labs/solana-program-library/tree/master/token/program-2022",
+    source_revision: "79a575fb7af56d26deeda94fef8f55bde7a90df3",
+    source_release: "token-2022-v0.9.0",
+    auditors: "https://github.com/solana-labs/security-audits#token-2022"
 }
