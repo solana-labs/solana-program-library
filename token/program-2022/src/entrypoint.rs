7ff12ef63e62bc3533c9193ce8a1fc1667a76306
Jon Cinque <me@jonc.dev>
2023-12-13 16:56:53 +0100

    token-2022: Update audit and release info (#5995)

--- token/program-2022/src/entrypoint.rs
+++ token/program-2022/src/entrypoint.rs
@@ -33,7 +33,7 @@ security_txt! {
     // Optional Fields
     preferred_languages: "en",
     source_code: "https://github.com/solana-labs/solana-program-library/tree/master/token/program-2022",
-    source_revision: "", // Add after PR is merged
+    source_revision: "15ebdb6440a4585a908ee3d91429561d64afebf6",
     source_release: "token-2022-v1.0.0",
     auditors: "https://github.com/solana-labs/security-audits#token-2022"
 }
