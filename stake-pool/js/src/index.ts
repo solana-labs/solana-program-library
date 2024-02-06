f9442e14d0f9d333f2654d42f28ac4f49a313e70
kagren <kristofer.agren@gmail.com>
2024-02-06 16:02:19 +0100

    Bugfix JS client stake pool code increaseValidatorStake and decreaseValidatorStake (#6224)
    * when ephemeralStakeSeed is supplied, transient stake seed needs to be unchanged
    
    * eslint, prettier fix

--- stake-pool/js/src/index.ts
+++ stake-pool/js/src/index.ts
@@ -679,7 +679,11 @@ export async function increaseValidatorStake(
     stakePoolAddress,
   );
 
-  const transientStakeSeed = validatorInfo.transientSeedSuffixStart.addn(1); // bump up by one to avoid reuse
+  // Bump transient seed suffix by one to avoid reuse when not using the increaseAdditionalStake instruction
+  const transientStakeSeed =
+    ephemeralStakeSeed == undefined
+      ? validatorInfo.transientSeedSuffixStart.addn(1)
+      : validatorInfo.transientSeedSuffixStart;
 
   const transientStake = await findTransientStakeProgramAddress(
     STAKE_POOL_PROGRAM_ID,
@@ -775,7 +779,11 @@ export async function decreaseValidatorStake(
     stakePoolAddress,
   );
 
-  const transientStakeSeed = validatorInfo.transientSeedSuffixStart.addn(1); // bump up by one to avoid reuse
+  // Bump transient seed suffix by one to avoid reuse when not using the decreaseAdditionalStake instruction
+  const transientStakeSeed =
+    ephemeralStakeSeed == undefined
+      ? validatorInfo.transientSeedSuffixStart.addn(1)
+      : validatorInfo.transientSeedSuffixStart;
 
   const transientStake = await findTransientStakeProgramAddress(
     STAKE_POOL_PROGRAM_ID,
