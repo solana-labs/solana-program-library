bb18b5337cb7d800f0042d7032f860bd6a5e0359
samkim-crypto <skim13@cs.stanford.edu>
2024-03-09 11:17:59 +0900

    remove non-functioning `owner` alias in some subcommands

--- token/cli/src/clap_app.rs
+++ token/cli/src/clap_app.rs
@@ -1640,7 +1640,6 @@ pub fn app<'a>(
                 )
                 .arg(
                     Arg::with_name("wallet_keypair")
-                        .alias("owner")
                         .value_name("KEYPAIR")
                         .validator(|s| is_valid_signer(s))
                         .takes_value(true)
@@ -1738,7 +1737,6 @@ pub fn app<'a>(
                 .arg(
                     Arg::with_name("close_authority")
                         .long("close-authority")
-                        .alias("owner")
                         .value_name("KEYPAIR")
                         .validator(|s| is_valid_signer(s))
                         .takes_value(true)
@@ -2264,7 +2262,6 @@ pub fn app<'a>(
                 .arg(
                     Arg::with_name("withdraw_withheld_authority")
                         .long("withdraw-withheld-authority")
-                        .alias("owner")
                         .value_name("KEYPAIR")
                         .validator(|s| is_valid_signer(s))
                         .takes_value(true)
