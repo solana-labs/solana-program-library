736759549eb85ad99c253b3ecaf2149483eb1309
samkim-crypto <skim13@cs.stanford.edu>
2024-03-12 07:18:24 +0900

    remove duplicate addition of args in invalid config test

--- token/cli/tests/config.rs
+++ token/cli/tests/config.rs
@@ -3,9 +3,8 @@ use assert_cmd::cmd::Command;
 #[test]
 fn invalid_config_will_cause_commands_to_fail() {
     let mut cmd = Command::cargo_bin("spl-token").unwrap();
-    let args = &["address", "--config", "~/nonexistent/config.yml"];
-    cmd.args(args)
-        .assert()
+    cmd.args(["address", "--config", "~/nonexistent/config.yml"]);
+    cmd.assert()
         .stderr("error: Could not find config file `~/nonexistent/config.yml`\n");
-    cmd.args(args).assert().code(1).failure();
+    cmd.assert().code(1).failure();
 }
