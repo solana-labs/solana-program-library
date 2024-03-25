88661a04fdc5ad8dcc760a6bc90e69afb051e849
samkim-crypto <skim13@cs.stanford.edu>
2024-03-25 09:50:35 +0900

    fix confidential transfer extensions account length

--- token/js/src/extensions/extensionType.ts
+++ token/js/src/extensions/extensionType.ts
@@ -77,9 +77,9 @@ export function getTypeLen(e: ExtensionType): number {
         case ExtensionType.MintCloseAuthority:
             return MINT_CLOSE_AUTHORITY_SIZE;
         case ExtensionType.ConfidentialTransferMint:
-            return 97;
+            return 65;
         case ExtensionType.ConfidentialTransferAccount:
-            return 286;
+            return 295;
         case ExtensionType.CpiGuard:
             return CPI_GUARD_SIZE;
         case ExtensionType.DefaultAccountState:
