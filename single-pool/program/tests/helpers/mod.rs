68827b8c4b1bc7039fb8172ffff41b2cf35b07f1
Ashwin Sekar <ashwin@solana.com>
2024-04-04 13:51:33 -0700

    Remove usage of VoteStateVersions::vote_state_size_of (#6549)

--- single-pool/program/tests/helpers/mod.rs
+++ single-pool/program/tests/helpers/mod.rs
@@ -15,7 +15,7 @@ use {
     },
     solana_vote_program::{
         self, vote_instruction,
-        vote_state::{VoteInit, VoteState, VoteStateVersions},
+        vote_state::{VoteInit, VoteState},
     },
     spl_associated_token_account as atoken,
     spl_single_pool::{
@@ -380,7 +380,7 @@ pub async fn create_vote(
         },
         rent_voter,
         vote_instruction::CreateVoteAccountConfig {
-            space: VoteStateVersions::vote_state_size_of(true) as u64,
+            space: VoteState::size_of() as u64,
             ..Default::default()
         },
     ));
