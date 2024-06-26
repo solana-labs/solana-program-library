af13f8a7216fb719e4d4996b006865520e581d96
Jon C <me@jonc.dev>
2024-06-27 00:55:57 +0200

    Fixup types

--- single-pool/js/packages/classic/src/addresses.ts
+++ single-pool/js/packages/classic/src/addresses.ts
@@ -1,4 +1,6 @@
+import type { Base58EncodedAddress } from '@solana/addresses';
 import { PublicKey } from '@solana/web3.js';
+import type { PoolAddress, VoteAccountAddress } from '@solana/spl-single-pool';
 import {
   findPoolAddress as findPoolModern,
   findPoolStakeAddress as findStakeModern,
@@ -10,29 +12,57 @@ import {
 } from '@solana/spl-single-pool';
 
 export async function findPoolAddress(programId: PublicKey, voteAccountAddress: PublicKey) {
-  return new PublicKey(await findPoolModern(programId.toBase58(), voteAccountAddress.toBase58()));
+  return new PublicKey(
+    await findPoolModern(
+      programId.toBase58() as Base58EncodedAddress,
+      voteAccountAddress.toBase58() as VoteAccountAddress,
+    ),
+  );
 }
 
 export async function findPoolStakeAddress(programId: PublicKey, poolAddress: PublicKey) {
-  return new PublicKey(await findStakeModern(programId.toBase58(), poolAddress.toBase58()));
+  return new PublicKey(
+    await findStakeModern(
+      programId.toBase58() as Base58EncodedAddress,
+      poolAddress.toBase58() as PoolAddress,
+    ),
+  );
 }
 
 export async function findPoolMintAddress(programId: PublicKey, poolAddress: PublicKey) {
-  return new PublicKey(await findMintModern(programId.toBase58(), poolAddress.toBase58()));
+  return new PublicKey(
+    await findMintModern(
+      programId.toBase58() as Base58EncodedAddress,
+      poolAddress.toBase58() as PoolAddress,
+    ),
+  );
 }
 
 export async function findPoolStakeAuthorityAddress(programId: PublicKey, poolAddress: PublicKey) {
   return new PublicKey(
-    await findStakeAuthorityModern(programId.toBase58(), poolAddress.toBase58()),
+    await findStakeAuthorityModern(
+      programId.toBase58() as Base58EncodedAddress,
+      poolAddress.toBase58() as PoolAddress,
+    ),
   );
 }
 
 export async function findPoolMintAuthorityAddress(programId: PublicKey, poolAddress: PublicKey) {
-  return new PublicKey(await findMintAuthorityModern(programId.toBase58(), poolAddress.toBase58()));
+  return new PublicKey(
+    await findMintAuthorityModern(
+      programId.toBase58() as Base58EncodedAddress,
+      poolAddress.toBase58() as PoolAddress,
+    ),
+  );
 }
 
 export async function findPoolMplAuthorityAddress(programId: PublicKey, poolAddress: PublicKey) {
-  return new PublicKey(await findMplAuthorityModern(programId.toBase58(), poolAddress.toBase58()));
+  return new PublicKey(
+    await findMplAuthorityModern(
+      programId.toBase58() as Base58EncodedAddress,
+      poolAddress.toBase58() as PoolAddress,
+    ),
+  );
 }
 
 export async function findDefaultDepositAccountAddress(
@@ -40,6 +70,9 @@ export async function findDefaultDepositAccountAddress(
   userWallet: PublicKey,
 ) {
   return new PublicKey(
-    await findDefaultDepositModern(poolAddress.toBase58(), userWallet.toBase58()),
+    await findDefaultDepositModern(
+      poolAddress.toBase58() as PoolAddress,
+      userWallet.toBase58() as Base58EncodedAddress,
+    ),
   );
 }
