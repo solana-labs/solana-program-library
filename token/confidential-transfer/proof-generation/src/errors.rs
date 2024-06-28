09195d94668384edf5c2ea1166fd411d05d3ccca
samkim-crypto <skim13@cs.stanford.edu>
2024-06-28 17:53:05 +0900

    add transfer with fee proof generation

--- token/confidential-transfer/proof-generation/src/errors.rs
+++ token/confidential-transfer/proof-generation/src/errors.rs
@@ -8,4 +8,6 @@ pub enum TokenProofGenerationError {
     NotEnoughFunds,
     #[error("illegal amount bit length")]
     IllegalAmountBitLength,
+    #[error("fee calculation failed")]
+    FeeCalculation,
 }
