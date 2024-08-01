702e2cee8b9b64c61473074ce98605a506c04c36
Jon C <me@jonc.dev>
2024-08-01 15:05:16 +0200

    single-pool-js: Update to web3.js v2 rc

--- single-pool/js/packages/modern/src/addresses.ts
+++ single-pool/js/packages/modern/src/addresses.ts
@@ -1,99 +1,91 @@
 import {
   address,
   getAddressCodec,
-  Base58EncodedAddress,
   getProgramDerivedAddress,
   createAddressWithSeed,
-} from '@solana/web3.js';
+  Address,
+} from '@solana/addresses';
 
 import { MPL_METADATA_PROGRAM_ID } from './internal.js';
 import { STAKE_PROGRAM_ID } from './quarantine.js';
 
 export const SINGLE_POOL_PROGRAM_ID = address('SVSPxpvHdN29nkVg9rPapPNDddN5DipNLRUFhyjFThE');
 
-export type VoteAccountAddress<TAddress extends string = string> =
-  Base58EncodedAddress<TAddress> & {
-    readonly __voteAccountAddress: unique symbol;
-  };
+export type VoteAccountAddress<TAddress extends string = string> = Address<TAddress> & {
+  readonly __voteAccountAddress: unique symbol;
+};
 
-export type PoolAddress<TAddress extends string = string> = Base58EncodedAddress<TAddress> & {
+export type PoolAddress<TAddress extends string = string> = Address<TAddress> & {
   readonly __poolAddress: unique symbol;
 };
 
-export type PoolStakeAddress<TAddress extends string = string> = Base58EncodedAddress<TAddress> & {
+export type PoolStakeAddress<TAddress extends string = string> = Address<TAddress> & {
   readonly __poolStakeAddress: unique symbol;
 };
 
-export type PoolMintAddress<TAddress extends string = string> = Base58EncodedAddress<TAddress> & {
+export type PoolMintAddress<TAddress extends string = string> = Address<TAddress> & {
   readonly __poolMintAddress: unique symbol;
 };
 
-export type PoolStakeAuthorityAddress<TAddress extends string = string> =
-  Base58EncodedAddress<TAddress> & {
-    readonly __poolStakeAuthorityAddress: unique symbol;
-  };
+export type PoolStakeAuthorityAddress<TAddress extends string = string> = Address<TAddress> & {
+  readonly __poolStakeAuthorityAddress: unique symbol;
+};
 
-export type PoolMintAuthorityAddress<TAddress extends string = string> =
-  Base58EncodedAddress<TAddress> & {
-    readonly __poolMintAuthorityAddress: unique symbol;
-  };
+export type PoolMintAuthorityAddress<TAddress extends string = string> = Address<TAddress> & {
+  readonly __poolMintAuthorityAddress: unique symbol;
+};
 
-export type PoolMplAuthorityAddress<TAddress extends string = string> =
-  Base58EncodedAddress<TAddress> & {
-    readonly __poolMplAuthorityAddress: unique symbol;
-  };
+export type PoolMplAuthorityAddress<TAddress extends string = string> = Address<TAddress> & {
+  readonly __poolMplAuthorityAddress: unique symbol;
+};
 
 export async function findPoolAddress(
-  programId: Base58EncodedAddress,
+  programId: Address,
   voteAccountAddress: VoteAccountAddress,
 ): Promise<PoolAddress> {
   return (await findPda(programId, voteAccountAddress, 'pool')) as PoolAddress;
 }
 
 export async function findPoolStakeAddress(
-  programId: Base58EncodedAddress,
+  programId: Address,
   poolAddress: PoolAddress,
 ): Promise<PoolStakeAddress> {
   return (await findPda(programId, poolAddress, 'stake')) as PoolStakeAddress;
 }
 
 export async function findPoolMintAddress(
-  programId: Base58EncodedAddress,
+  programId: Address,
   poolAddress: PoolAddress,
 ): Promise<PoolMintAddress> {
   return (await findPda(programId, poolAddress, 'mint')) as PoolMintAddress;
 }
 
 export async function findPoolStakeAuthorityAddress(
-  programId: Base58EncodedAddress,
+  programId: Address,
   poolAddress: PoolAddress,
 ): Promise<PoolStakeAuthorityAddress> {
   return (await findPda(programId, poolAddress, 'stake_authority')) as PoolStakeAuthorityAddress;
 }
 
 export async function findPoolMintAuthorityAddress(
-  programId: Base58EncodedAddress,
+  programId: Address,
   poolAddress: PoolAddress,
 ): Promise<PoolMintAuthorityAddress> {
   return (await findPda(programId, poolAddress, 'mint_authority')) as PoolMintAuthorityAddress;
 }
 
 export async function findPoolMplAuthorityAddress(
-  programId: Base58EncodedAddress,
+  programId: Address,
   poolAddress: PoolAddress,
 ): Promise<PoolMplAuthorityAddress> {
   return (await findPda(programId, poolAddress, 'mpl_authority')) as PoolMplAuthorityAddress;
 }
 
-async function findPda(
-  programId: Base58EncodedAddress,
-  baseAddress: Base58EncodedAddress,
-  prefix: string,
-) {
-  const { serialize } = getAddressCodec();
+async function findPda(programId: Address, baseAddress: Address, prefix: string) {
+  const { encode } = getAddressCodec();
   const [pda] = await getProgramDerivedAddress({
     programAddress: programId,
-    seeds: [prefix, serialize(baseAddress)],
+    seeds: [prefix, encode(baseAddress)],
   });
 
   return pda;
@@ -101,7 +93,7 @@ async function findPda(
 
 export async function findDefaultDepositAccountAddress(
   poolAddress: PoolAddress,
-  userWallet: Base58EncodedAddress,
+  userWallet: Address,
 ) {
   return createAddressWithSeed({
     baseAddress: userWallet,
@@ -115,10 +107,10 @@ export function defaultDepositAccountSeed(poolAddress: PoolAddress): string {
 }
 
 export async function findMplMetadataAddress(poolMintAddress: PoolMintAddress) {
-  const { serialize } = getAddressCodec();
+  const { encode } = getAddressCodec();
   const [pda] = await getProgramDerivedAddress({
     programAddress: MPL_METADATA_PROGRAM_ID,
-    seeds: ['metadata', serialize(MPL_METADATA_PROGRAM_ID), serialize(poolMintAddress)],
+    seeds: ['metadata', encode(MPL_METADATA_PROGRAM_ID), encode(poolMintAddress)],
   });
 
   return pda;
