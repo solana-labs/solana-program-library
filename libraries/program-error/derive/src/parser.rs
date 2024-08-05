c27aff51a3438896dd2cf850cea35d869f02542f
Jon C <me@jonc.dev>
2024-08-05 20:50:15 +0200

    program-error: Add option to specify solana_program crate
    Problem
    
    There are going to be a lot of issues with people seeing errors on
    spl_program_error derivation. When a build pulls in two version of
    solana_program, the macro uses `::solana_program`, which now becomes
    ambiguous.
    
    Summary of changes
    
    I'm not sure if this is a good idea, but this PR gives the ability
    to specify which version of `solana_program` to use when deriving the
    various traits on your error type.
    
    Borsh has this same functionality, and it's saved us when pulling in
    multiple versions of borsh in the SDK.
    
    Note: this PR defaults to `solana_program` instead of
    `::solana_program`, which might cause downstream issues.

--- libraries/program-error/derive/src/parser.rs
+++ libraries/program-error/derive/src/parser.rs
@@ -1,11 +1,11 @@
 //! Token parsing
 
 use {
-    proc_macro2::Ident,
+    proc_macro2::{Ident, Span},
     syn::{
         parse::{Parse, ParseStream},
         token::Comma,
-        LitInt, Token,
+        LitInt, LitStr, Path, Token,
     },
 };
 
@@ -14,20 +14,31 @@ pub struct SplProgramErrorArgs {
     /// Whether to hash the error codes using `solana_program::hash`
     /// or to use the default error code assigned by `num_traits`.
     pub hash_error_code_start: Option<u32>,
+    /// Crate to use for solana_program
+    pub solana_program_crate: Path,
 }
 
 impl Parse for SplProgramErrorArgs {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        if input.is_empty() {
-            return Ok(Self {
-                hash_error_code_start: None,
-            });
+        let default_solana_program_crate = Ident::new("solana_program", Span::call_site());
+        let mut hash_error_code_start = None;
+        let mut solana_program_crate = None;
+        while !input.is_empty() {
+            match SplProgramErrorArgParser::parse(input)? {
+                SplProgramErrorArgParser::HashErrorCodes { value, .. } => {
+                    hash_error_code_start = Some(value.base10_parse::<u32>()?);
+                }
+                SplProgramErrorArgParser::SolanaProgramCrate { value, .. } => {
+                    solana_program_crate = value.parse()?;
+                }
+            }
         }
-        match SplProgramErrorArgParser::parse(input)? {
-            SplProgramErrorArgParser::HashErrorCodes { value, .. } => Ok(Self {
-                hash_error_code_start: Some(value.base10_parse::<u32>()?),
-            }),
-        }
+        Ok(Self {
+            hash_error_code_start,
+            solana_program_crate: solana_program_crate
+                .unwrap_or(default_solana_program_crate)
+                .into(),
+        })
     }
 }
 
@@ -35,30 +46,45 @@ impl Parse for SplProgramErrorArgs {
 /// ie. `#[spl_program_error(hash_error_code_start = 1275525928)]`
 enum SplProgramErrorArgParser {
     HashErrorCodes {
-        _ident: Ident,
         _equals_sign: Token![=],
         value: LitInt,
+        _comma: Option<Comma>,
+    },
+    SolanaProgramCrate {
+        _equals_sign: Token![=],
+        value: LitStr,
         _comma: Option<Comma>,
     },
 }
 
 impl Parse for SplProgramErrorArgParser {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let _ident = {
-            let ident = input.parse::<Ident>()?;
-            if ident != "hash_error_code_start" {
-                return Err(input.error("Expected argument 'hash_error_code_start'"));
+        let ident = input.parse::<Ident>()?;
+        match ident.to_string().as_str() {
+            "hash_error_code_start" => {
+                let _equals_sign = input.parse::<Token![=]>()?;
+                let value = input.parse::<LitInt>()?;
+                let _comma: Option<Comma> = input.parse().unwrap_or(None);
+                Ok(Self::HashErrorCodes {
+                    _equals_sign,
+                    value,
+                    _comma,
+                })
             }
-            ident
-        };
-        let _equals_sign = input.parse::<Token![=]>()?;
-        let value = input.parse::<LitInt>()?;
-        let _comma: Option<Comma> = input.parse().unwrap_or(None);
-        Ok(Self::HashErrorCodes {
-            _ident,
-            _equals_sign,
-            value,
-            _comma,
-        })
+            "solana_program_crate" => {
+                let _equals_sign = input.parse::<Token![=]>()?;
+                let value = input.parse::<LitStr>()?;
+                let _comma: Option<Comma> = input.parse().unwrap_or(None);
+                Ok(Self::SolanaProgramCrate {
+                    _equals_sign,
+                    value,
+                    _comma,
+                })
+            }
+            _ => {
+                Err(input
+                    .error("Expected argument 'hash_error_code_start' or 'solana_program_crate'"))
+            }
+        }
     }
 }
