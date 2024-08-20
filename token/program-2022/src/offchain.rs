2ec0b9b5b497848616ce491b54b8668bcbc4e2c1
tonton-sol <19677766+tonton-sol@users.noreply.github.com>
2024-08-20 14:28:30 +0900

    added create_transfer_checked_with_fee_instruction_with_extra_metas

--- token/program-2022/src/offchain.rs
+++ token/program-2022/src/offchain.rs
@@ -74,6 +74,72 @@ where
     Ok(transfer_instruction)
 }
 
+/// Offchain helper to create a `TransferCheckedWithFee` instruction with all
+/// additional required account metas for a transfer, including the ones
+/// required by the transfer hook.
+///
+/// To be client-agnostic and to avoid pulling in the full solana-sdk, this
+/// simply takes a function that will return its data as `Future<Vec<u8>>` for
+/// the given address. Can be called in the following way:
+///
+/// ```rust,ignore
+/// let instruction = create_transfer_checked_with_fee_instruction_with_extra_metas(
+///     &spl_token_2022::id(),
+///     &source,
+///     &mint,
+///     &destination,
+///     &authority,
+///     &[],
+///     amount,
+///     decimals,
+///     fee,
+///     |address| self.client.get_account(&address).map_ok(|opt| opt.map(|acc| acc.data)),
+/// )
+/// .await?
+/// ```
+#[allow(clippy::too_many_arguments)]
+pub async fn create_transfer_checked_with_fee_instruction_with_extra_metas<F, Fut>(
+    token_program_id: &Pubkey,
+    source_pubkey: &Pubkey,
+    mint_pubkey: &Pubkey,
+    destination_pubkey: &Pubkey,
+    authority_pubkey: &Pubkey,
+    signer_pubkeys: &[&Pubkey],
+    amount: u64,
+    decimals: u8,
+    fee: u64,
+    fetch_account_data_fn: F,
+) -> Result<Instruction, AccountFetchError>
+where
+    F: Fn(Pubkey) -> Fut,
+    Fut: Future<Output = AccountDataResult>,
+{
+    let mut transfer_instruction = transfer_fee::instruction::transfer_checked_with_fee(
+        token_program_id,
+        source_pubkey,
+        mint_pubkey,
+        destination_pubkey,
+        authority_pubkey,
+        signer_pubkeys,
+        amount,
+        decimals,
+        fee,
+    )?;
+
+    add_extra_account_metas(
+        &mut transfer_instruction,
+        source_pubkey,
+        mint_pubkey,
+        destination_pubkey,
+        authority_pubkey,
+        amount,
+        fetch_account_data_fn,
+    )
+    .await?;
+
+    Ok(transfer_instruction)
+}
+
 /// Offchain helper to add required account metas to an instruction, including
 /// the ones required by the transfer hook.
 ///
