404466d0157b3979b2963fe401dc304be778a91a
Noah Gundotra <noah@gundotra.org>
2024-10-17 15:31:40 -0400

    clean up CI

--- ci/js-test-account-compression.sh
+++ ci/js-test-account-compression.sh
@@ -3,11 +3,12 @@
 set -e
 cd "$(dirname "$0")/.."
 source ./ci/solana-version.sh install
+cargo install solana-verify
 
 set -x
+solana-verify build --library-name spl_account_compression && solana-verify build --library-name spl_noop
 cd account-compression/sdk
 pnpm install
 pnpm build
-pnpm build:program
 pnpm lint
 pnpm test
