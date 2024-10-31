719200db05a87146b85210fed5828c6e1ac61cd7
kevinheavey <heaveykevin2@gmail.com>
2024-10-31 18:21:04 +0400

    add back repr(32)

--- libraries/pod/src/error.rs
+++ libraries/pod/src/error.rs
@@ -6,6 +6,7 @@ use {
 };
 
 /// Errors that may be returned by the spl-pod library.
+#[repr(32)]
 #[derive(Debug, Clone, PartialEq, Eq, thiserror::Error, num_derive::FromPrimitive)]
 pub enum PodSliceError {
     /// Error in checked math operation
