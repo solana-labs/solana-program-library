171e50f8049e76f8620a90b1c199b2853cb7bbef
Jon C <me@jonc.dev>
2024-11-01 01:52:10 +0100

    instruction-padding: Remove solana-program dependency
    #### Problem
    
    The instruction-padding program still has an explicit dependency on
    solana-program, but it isn't needed.
    
    #### Summary of changes
    
    Use the component crates instead. Some of the syscall consts aren't
    available yet outside of solana-program, but it was easy to add a static
    assertion for those.

--- instruction-padding/program/src/lib.rs
+++ instruction-padding/program/src/lib.rs
@@ -2,5 +2,4 @@ mod entrypoint;
 pub mod instruction;
 pub mod processor;
 
-pub use solana_program;
-solana_program::declare_id!("iXpADd6AW1k5FaaXum5qHbSqyd7TtoN6AD7suVa83MF");
+solana_pubkey::declare_id!("iXpADd6AW1k5FaaXum5qHbSqyd7TtoN6AD7suVa83MF");
