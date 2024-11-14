afb9652bdb90b6e7f44d2c1d360e76748df72142
samkim-crypto <skim13@cs.stanford.edu>
2024-11-14 16:07:35 +0900

    update token-cli using the updated syntax

--- token/cli/src/command.rs
+++ token/cli/src/command.rs
@@ -57,19 +57,19 @@ use {
             transfer_hook::TransferHook,
             BaseStateWithExtensions, ExtensionType, StateWithExtensionsOwned,
         },
-        solana_zk_sdk::{
-            encryption::{
-                auth_encryption::AeKey,
-                elgamal::{self, ElGamalKeypair},
-                pod::elgamal::PodElGamalPubkey,
-            },
-            zk_elgamal_proof_program::proof_data::ZkProofData,
+        solana_zk_sdk::encryption::{
+            auth_encryption::AeKey,
+            elgamal::{self, ElGamalKeypair},
+            pod::elgamal::PodElGamalPubkey,
         },
         state::{Account, AccountState, Mint},
     },
     spl_token_client::{
         client::{ProgramRpcClientSendTransaction, RpcClientResponse},
-        token::{ComputeUnitLimit, ExtensionInitializationParams, ProofAccount, Token},
+        token::{
+            ComputeUnitLimit, ExtensionInitializationParams, ProofAccount,
+            ProofAccountWithCiphertext, Token,
+        },
     },
     spl_token_confidential_transfer_proof_generation::{
         transfer::TransferProofData, withdraw::WithdrawProofData,
@@ -1614,7 +1614,7 @@ async fn command_transfer(
 
             let TransferProofData {
                 equality_proof_data,
-                ciphertext_validity_proof_data,
+                ciphertext_validity_proof_data_with_ciphertext,
                 range_proof_data,
             } = transfer_account_info
                 .generate_split_transfer_proof_data(
@@ -1626,16 +1626,10 @@ async fn command_transfer(
                 )
                 .unwrap();
 
-            let transfer_amount_auditor_ciphertext_lo = ciphertext_validity_proof_data
-                .context_data()
-                .grouped_ciphertext_lo
-                .try_extract_ciphertext(2)
-                .unwrap();
-            let transfer_amount_auditor_ciphertext_hi = ciphertext_validity_proof_data
-                .context_data()
-                .grouped_ciphertext_hi
-                .try_extract_ciphertext(2)
-                .unwrap();
+            let transfer_amount_auditor_ciphertext_lo =
+                ciphertext_validity_proof_data_with_ciphertext.ciphertext_lo;
+            let transfer_amount_auditor_ciphertext_hi =
+                ciphertext_validity_proof_data_with_ciphertext.ciphertext_hi;
 
             // setup proofs
             let create_range_proof_context_signer = &[&range_proof_context_state_account];
@@ -1661,7 +1655,7 @@ async fn command_transfer(
                 token.confidential_transfer_create_context_state_account(
                     &ciphertext_validity_proof_pubkey,
                     &context_state_authority_pubkey,
-                    &ciphertext_validity_proof_data,
+                    &ciphertext_validity_proof_data_with_ciphertext.proof_data,
                     false,
                     create_ciphertext_validity_proof_context_signer
                 )
@@ -1675,17 +1669,21 @@ async fn command_transfer(
             let range_proof_context_proof_account =
                 ProofAccount::ContextAccount(range_proof_pubkey);
 
+            let ciphertext_validity_proof_account_with_ciphertext = ProofAccountWithCiphertext {
+                proof_account: ciphertext_validity_proof_context_proof_account,
+                ciphertext_lo: transfer_amount_auditor_ciphertext_lo,
+                ciphertext_hi: transfer_amount_auditor_ciphertext_hi,
+            };
+
             let transfer_result = token
                 .confidential_transfer_transfer(
                     &sender,
                     &recipient_token_account,
                     &sender_owner,
                     Some(&equality_proof_context_proof_account),
-                    Some(&ciphertext_validity_proof_context_proof_account),
+                    Some(&ciphertext_validity_proof_account_with_ciphertext),
                     Some(&range_proof_context_proof_account),
                     transfer_balance,
-                    Some(&transfer_amount_auditor_ciphertext_lo),
-                    Some(&transfer_amount_auditor_ciphertext_hi),
                     Some(transfer_account_info),
                     &args.sender_elgamal_keypair,
                     &args.sender_aes_key,
