c16944f3d308ae47ada8da1334051028ade19b60
Ilan Gitter <8359193+gitteri@users.noreply.github.com>
2024-12-04 12:06:59 -0700

    address comments

--- token/js/src/actions/amountToUiAmount.ts
+++ token/js/src/actions/amountToUiAmount.ts
@@ -38,14 +38,14 @@ export async function amountToUiAmount(
  * @param r - The interest rate in basis points.
  * @returns The calculated exponent.
  */
-const calculateExponentForTimesAndRate = (t1: number, t2: number, r: number) => {
+function calculateExponentForTimesAndRate(t1: number, t2: number, r: number) {
     const ONE_IN_BASIS_POINTS = 10000;
     const SECONDS_PER_YEAR = 60 * 60 * 24 * 365.24;
     const timespan = t2 - t1;
     const numerator = r * timespan;
     const exponent = numerator / (SECONDS_PER_YEAR * ONE_IN_BASIS_POINTS);
     return Math.exp(exponent);
-};
+}
 
 /**
  * Retrieves the current timestamp from the Solana clock sysvar.
@@ -53,7 +53,7 @@ const calculateExponentForTimesAndRate = (t1: number, t2: number, r: number) => 
  * @returns A promise that resolves to the current timestamp in seconds.
  * @throws An error if the sysvar clock cannot be fetched or parsed.
  */
-const getSysvarClockTimestamp = async (connection: Connection): Promise<number> => {
+async function getSysvarClockTimestamp(connection: Connection): Promise<number> {
     const info = await connection.getParsedAccountInfo(new PublicKey('SysvarC1ock11111111111111111111111111111111'));
     if (!info) {
         throw new Error('Failed to fetch sysvar clock');
@@ -62,7 +62,7 @@ const getSysvarClockTimestamp = async (connection: Connection): Promise<number> 
         return info.value.data.parsed.info.unixTimestamp;
     }
     throw new Error('Failed to parse sysvar clock');
-};
+}
 
 /**
  * Convert amount to UiAmount for a mint with interest bearing extension without simulating a transaction
@@ -109,7 +109,7 @@ export function amountToUiAmountWithoutSimulation(
 
     // Calculate post-update exponent
     // e^(currentRate * (currentTimestamp - lastUpdateTimestamp) / (SECONDS_PER_YEAR * ONE_IN_BASIS_POINTS))
-    const postUpdateExp = calculateExponentForTimesAndRate(lastUpdateTimestamp, Number(currentTimestamp), currentRate);
+    const postUpdateExp = calculateExponentForTimesAndRate(lastUpdateTimestamp, currentTimestamp, currentRate);
 
     // Calculate total scale
     const totalScale = preUpdateExp * postUpdateExp;
@@ -134,7 +134,6 @@ export function amountToUiAmountWithoutSimulation(
  * @param connection     Connection to use
  * @param mint           Mint to use for calculations
  * @param amount         Amount of tokens to be converted to Ui Amount
- * @param programId      SPL Token program account (default: TOKEN_PROGRAM_ID)
  *
  * @return Ui Amount generated
  */
@@ -175,7 +174,7 @@ export async function amountToUiAmountForMintWithoutSimulation(
  * Convert an amount with interest back to the original amount without interest
  * This implements the same logic as the CPI instruction available in /token/program-2022/src/extension/interest_bearing_mint/mod.rs
  *
- * @param uiAmount                  UI Amount (principle plus continuously compounding interest) to be converted back to original principle
+ * @param uiAmount                  UI Amount (principal plus continuously compounding interest) to be converted back to original principal
  * @param decimals                  Number of decimals for the mint
  * @param currentTimestamp          Current timestamp in seconds
  * @param lastUpdateTimestamp       Last time the interest rate was updated in seconds
@@ -183,21 +182,21 @@ export async function amountToUiAmountForMintWithoutSimulation(
  * @param preUpdateAverageRate      Interest rate in basis points (hundredths of a percent) before the last update
  * @param currentRate              Current interest rate in basis points
  *
- * In general to calculate the principle from the UI amount, the formula is:
+ * In general to calculate the principal from the UI amount, the formula is:
  * P = A / (e^(r * t)) where
- * P = principle
+ * P = principal
  * A = UI amount
  * r = annual interest rate (as a decimal, e.g., 5% = 0.05)
  * t = time in years
  *
- * In this case, we are calculating the principle by dividing the UI amount by the total scale factor which is the product of two exponential functions:
+ * In this case, we are calculating the principal by dividing the UI amount by the total scale factor which is the product of two exponential functions:
  * totalScale = e^(r1 * t1) * e^(r2 * t2)
  * where r1 is the pre-update average rate, r2 is the current rate, t1 is the time in years between the initialization timestamp and the last update timestamp,
  * and t2 is the time in years between the last update timestamp and the current timestamp.
- * then to calculate the principle, we divide the UI amount by the total scale factor:
+ * then to calculate the principal, we divide the UI amount by the total scale factor:
  * P = A / totalScale
  *
- * @return Original amount (principle) without interest
+ * @return Original amount (principal) without interest
  */
 export function uiAmountToAmountWithoutSimulation(
     uiAmount: string,
@@ -225,9 +224,9 @@ export function uiAmountToAmountWithoutSimulation(
     // Calculate total scale
     const totalScale = preUpdateExp * postUpdateExp;
 
-    // Calculate original principle by dividing the UI amount (principle + interest) by the total scale
-    const originalPrinciple = uiAmountScaled / totalScale;
-    return BigInt(Math.floor(originalPrinciple));
+    // Calculate original principal by dividing the UI amount (principal + interest) by the total scale
+    const originalprincipal = uiAmountScaled / totalScale;
+    return BigInt(Math.trunc(originalprincipal));
 }
 
 /**
@@ -236,7 +235,6 @@ export function uiAmountToAmountWithoutSimulation(
  * @param connection     Connection to use
  * @param mint           Mint to use for calculations
  * @param uiAmount       UI Amount to be converted back to raw amount
- * @param programId      SPL Token program account (default: TOKEN_PROGRAM_ID)
  *
  *
  * @return Raw amount
@@ -252,12 +250,11 @@ export async function uiAmountToAmountForMintWithoutSimulation(
         throw new Error('Invalid program ID');
     }
 
-    const mintInfo = await getMint(connection, mint, 'confirmed', programId);
-
+    const mintInfo = unpackMint(mint, accountInfo, programId);
     const interestBearingMintConfigState = getInterestBearingMintConfigState(mintInfo);
     if (!interestBearingMintConfigState) {
         const uiAmountScaled = parseFloat(uiAmount) * Math.pow(10, mintInfo.decimals);
-        return BigInt(Math.floor(uiAmountScaled));
+        return BigInt(Math.trunc(uiAmountScaled));
     }
 
     const timestamp = await getSysvarClockTimestamp(connection);
