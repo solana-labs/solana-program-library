b76e15670f854854136e54f94a385a266f018c85
Jon C <me@jonc.dev>
2025-01-10 17:54:28 +0100

    libraries: Remove ported libraries

--- libraries/type-length-value/js/src/splDiscriminate.ts
+++
@@ -1,8 +0,0 @@
-import { assertDigestCapabilityIsAvailable } from '@solana/assertions';
-
-export async function splDiscriminate(discriminator: string, length = 8): Promise<Uint8Array> {
-    assertDigestCapabilityIsAvailable();
-    const bytes = new TextEncoder().encode(discriminator);
-    const digest = await crypto.subtle.digest('SHA-256', bytes);
-    return new Uint8Array(digest).subarray(0, length);
-}
