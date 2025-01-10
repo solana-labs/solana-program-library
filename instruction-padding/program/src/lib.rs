623d210271d9e5a8858a680b860cfd09b61a790b
Jon C <me@jonc.dev>
2025-01-10 17:51:33 +0100

    instruction-padding: Remove program

--- instruction-padding/program/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod entrypoint;
-pub mod instruction;
-pub mod processor;
-
-pub use {
-    solana_account_info, solana_cpi, solana_instruction, solana_program_entrypoint,
-    solana_program_error, solana_pubkey,
-};
-solana_pubkey::declare_id!("iXpADd6AW1k5FaaXum5qHbSqyd7TtoN6AD7suVa83MF");
