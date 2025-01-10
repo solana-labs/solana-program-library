fb56d8df83b68b8db3f582325c9bd0c7946ca8b3
Jon C <me@jonc.dev>
2025-01-10 17:58:24 +0100

    stake-pool: Remove program and clients

--- stake-pool/py/vote/constants.py
+++
@@ -1,8 +0,0 @@
-from solders.pubkey import Pubkey
-
-
-VOTE_PROGRAM_ID = Pubkey.from_string("Vote111111111111111111111111111111111111111")
-"""Program id for the native vote program."""
-
-VOTE_STATE_LEN: int = 3762
-"""Size of vote account."""
